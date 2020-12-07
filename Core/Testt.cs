919945a2ca2f31e9258a55b20e7eafc06ea2679c
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2020-12-07 12:06:54 +0000

    Add simple struct project

---
+++ Core/Testt.cs
@@ -0,0 +1,13 @@
+﻿using Alis.Store;
+using System;
+
+namespace Alis.Core
+{
+    public class Testt
+    {
+        public static void Test() 
+        {
+            LocalStore.Test();
+        }
+    }
+}
---
+++ Store/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Alis.Store
+{
+    public class LocalStore
+    {
+        public static void Test()
+        {
+            Console.Write("Hola");
+        }
+    }
+}
