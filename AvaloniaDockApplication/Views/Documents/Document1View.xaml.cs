6f7749265f25cf167117495e8b1beb19db598925
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2020-12-10 13:33:14 +0000

    Add dock

--- AvaloniaDockApplication/Views/Documents/Document1View.xaml.cs
+++ AvaloniaDockApplication/Views/Documents/Document1View.xaml.cs
@@ -1,18 +1,19 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using System;
+using System.Threading;
 
 namespace AvaloniaDockApplication.Views.Documents
 {
     public class Document1View : UserControl
     {
-        public Document1View()
-        {
-            this.InitializeComponent();
-        }
 
-        private void InitializeComponent()
+        public Document1View() 
         {
             AvaloniaXamlLoader.Load(this);
+
+            
         }
     }
 }
