4f5cb2df414c29d6fb13392abf57a8cc813223d5
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2020-12-10 11:29:20 +0000

    Add dock system

---
+++ AvaloniaDockApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+
+namespace AvaloniaDockApplication
+{
+    class Program
+    {
+        public static void Main(string[] args) => BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToDebug();
+    }
+}
---
+++ AvaloniaDockApplication/ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Dock.Model;
+using Dock.Model.Controls;
+
+namespace AvaloniaDockApplication.ViewModels
+{
+    public class MainViewModel : RootDock
+    {
+        public override IDockable Clone()
+        {
+            var mainViewModel = new MainViewModel();
+
+            CloneHelper.CloneDockProperties(this, mainViewModel);
+            CloneHelper.CloneRootDockProperties(this, mainViewModel);
+
+            return mainViewModel;
+        }
+    }
+}
