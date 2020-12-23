f47e7d070bb38da697afa8a4507a3d24e238f160
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2020-12-23 10:40:04 +0000

    Add apiDoc, github page & create simple web.

--- Documenter/Help/_Footer.md
+++
@@ -1,6 +0,0 @@
-﻿Documentation Alis Framework
-
-
-
-
-Send comments on this topic to [](mailto:?Subject=Documentation%20Alis%20Framework)
--- Editor/RECICLE/Class1.cs
+++ Editor/RECICLE/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Editor
+namespace Alis.Editor
 {
     class Class1
     {
