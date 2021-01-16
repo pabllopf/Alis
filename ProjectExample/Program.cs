710f5b2983e4480e27cf82d66c09f8650f096620
pabllopf <pabloperdomofalcon@gmail.com>
2021-01-16 19:55:43 +0000

    Upgrade zipper tool.

--- ProjectExample/Program.cs
+++ ProjectExample/Program.cs
@@ -7,45 +7,9 @@ namespace ProjectExample
 {
     class Program
     {
-        public static string example = "Pirate";
-
         static void Main(string[] args)
         {
-            
-            Language.Change += Language_Change;
-
-            Language.TranslateTo(Idiom.English);
-            
-            Language.TranslateTo(Idiom.Spanish);
-            
-            Language.TranslateTo(Idiom.French);
-            
-            Language.TranslateTo(Idiom.Italian);
-
-            Console.ReadKey();
-        }
-
-        private static void Language_Change(object sender, Idiom idiom)
-        {
-            Console.WriteLine("Change to: " + idiom.ToString());
-            example = Language.GetSentence(idiom, "CONTEXT_CLASS_PIRATE");
-            Console.WriteLine("string: " + example + "\n");
+            Zipper.Zip("", );
         }
     }
-
-    public class Login 
-    {
-        private string user;
-        private string passwd;
-
-        public Login(string user, string passwd)
-        {
-            this.user = user;
-            this.passwd = passwd;
-        }
-
-        public string User { get => user; set => user = value; }
-        public string Passwd { get => passwd; set => passwd = value; }
-    }
-
 }
--- Tools/Zipper.cs
+++ Tools/Zipper.cs
@@ -1,10 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Tools
+﻿//-------------------------------------------------------------------------------------------------
+// <author>Pablo Perdomo Falcón</author>
+// <copyright file="Zipper.cs" company="Pabllopf">GNU General Public License v3.0</copyright>
+//-------------------------------------------------------------------------------------------------
+namespace Alis.Tools
 {
-    class Zipper
+    using System.IO;
+    using Ionic.Zip;
+
+    /// <summary>Manage zip files. </summary>
+    public class Zipper
     {
+        /// <summary>Unzips the specified zip file path.</summary>
+        /// <param name="zipFilePath">The zip file path.</param>
+        /// <param name="location">The location.</param>
+        public static void Unzip(string zipFilePath, string location)
+        {
+            Directory.CreateDirectory(location);
+
+            using (ZipFile zip = ZipFile.Read(zipFilePath))
+            {
+                zip.ExtractAll(location, ExtractExistingFileAction.OverwriteSilently);
+            }
+        }
+
+        /// <summary>Zips the specified zip file name.</summary>
+        /// <param name="zipFileName">Name of the zip file.</param>
+        /// <param name="files">The files.</param>
+        public static void Zip(string zipFileName, params string[] files)
+        {
+            string path = Path.GetDirectoryName(zipFileName);
+            Directory.CreateDirectory(path);
+
+            using (ZipFile zip = new ZipFile())
+            {
+                foreach (string file in files)
+                {
+                    zip.AddFile(file, string.Empty);
+                }
+
+                zip.Save(zipFileName);
+            }
+        }
     }
 }
