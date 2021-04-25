49d448dc21099493bb4462e1935ad782cead6cf7
pabllopf <pabloperdomofalcon@gmail.com>
2021-04-25 12:33:16 +0100

    Upgrade the asset tool

--- Tools/Asset.cs
+++ Tools/Asset.cs
@@ -12,9 +12,13 @@ namespace Alis.Tools
     /// <summary>Find any asset of the videogame.</summary>
     public static class Asset
     {
+        /// <summary>The files</summary>
+        [AllowNull]
+        private static Dictionary<string, string> files;
+
         /// <summary>The work path</summary>
         [AllowNull]
-        private static string workPath = Environment.CurrentDirectory;
+        private static string workPath;
 
         /// <summary>Gets or sets the work path.</summary>
         /// <value>The work path.</value>
@@ -26,12 +30,32 @@ namespace Alis.Tools
         [return: MaybeNull]
         public static string Load(string file)
         {
+            if (workPath == null) 
+            {
+                workPath = Environment.CurrentDirectory;
+            }
+
+            if (files == null) 
+            {
+                files = new Dictionary<string, string>();
+            }
+
+            if (files.ContainsKey(file))
+            {
+                return files[file];
+            }
+
             if (Directory.Exists(workPath))
             {
                 foreach (string path in Directory.GetFiles(workPath, "*", SearchOption.AllDirectories))
                 {
                     if (Path.GetFileName(path).Equals(file))
                     {
+                        if (!files.ContainsKey(file)) 
+                        {
+                            files.Add(file, path);
+                        }
+
                         return path;
                     }
                 }
