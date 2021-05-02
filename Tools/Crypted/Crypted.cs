a3e118bb5cc37a5af28b8a81f9a25e553c1720c4
pabllopf <pabloperdomofalcon@gmail.com>
2021-05-02 12:30:12 +0100

    Add new version

---
+++ Tools/Crypted/Crypted.cs
@@ -0,0 +1,99 @@
+﻿//-------------------------------------------------------------------------------------------------
+// <author>Pablo Perdomo Falcón</author>
+// <copyright file="Crypted.cs" company="Pabllopf">GNU General Public License v3.0</copyright>
+//-------------------------------------------------------------------------------------------------
+namespace Alis.Tools
+{
+    using System;
+    using System.IO;
+    using System.Security.Cryptography;
+    using System.Text;
+
+    /// <summary>Control memory Security</summary>
+    /// <typeparam name="T">object to pass the algorithm</typeparam>
+    public class Crypted<T> : object
+    {
+        /// <summary>The data</summary>
+        private byte[] data;
+
+        /// <summary>The key</summary>
+        private byte[] key = new byte[0];
+
+        /// <summary>The vector</summary>
+        private byte[] vector = new byte[0];
+
+        /// <summary>Initializes a new instance of the <see cref="Crypted{T}" /> class.</summary>
+        /// <param name="data">The data.</param>
+        public Crypted(T data)
+        {
+            this.data = Encrypt(data); 
+        }
+
+        /// <summary>Sets the specified data.</summary>
+        /// <param name="data">The data.</param>
+        public void Set(T data) 
+        {
+            this.data = Encrypt(data);
+        }
+
+        /// <summary>Gets this instance.</summary>
+        /// <returns>Return value</returns>
+        public T Get() 
+        {
+            return Decrypt();
+        }
+
+        /// <summary>Encrypts the specified data.</summary>
+        /// <param name="data">The data.</param>
+        /// <returns>Return value.</returns>
+        private byte[] Encrypt(T data) 
+        {
+            Aes algorithm = Aes.Create();
+
+            if (key.Length == 0 || vector.Length == 0) 
+            {
+                key = algorithm.Key;
+                vector = algorithm.IV;
+            }
+
+            algorithm.Key = key;
+            algorithm.IV = vector;
+
+            ICryptoTransform crypto = algorithm.CreateEncryptor(algorithm.Key, algorithm.IV);
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (CryptoStream cryptoStream = new CryptoStream(memoryStream, crypto, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter streamWriter = new StreamWriter(cryptoStream))
+                    {
+                        streamWriter.Write(data);
+                    }
+
+                    return memoryStream.ToArray();
+                }
+            }
+        }
+
+        /// <summary>Decrypts this instance.</summary>
+        /// <returns>Return the value</returns>
+        private T Decrypt() 
+        {
+            Aes algorithm = Aes.Create();
+
+            algorithm.Key = key;
+            algorithm.IV = vector;
+
+            ICryptoTransform crypto = algorithm.CreateDecryptor(algorithm.Key, algorithm.IV);
+            using (MemoryStream memoryStream = new MemoryStream(data))
+            {
+                using (CryptoStream cryptoStream = new CryptoStream(memoryStream, crypto, CryptoStreamMode.Read))
+                {
+                    using (StreamReader streamReader = new StreamReader(cryptoStream))
+                    {
+                        return (T)Convert.ChangeType(streamReader.ReadToEnd(), typeof(T)) ?? throw new NullReferenceException(typeof(T).GetType().FullName);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Tools/Language/Language.cs
@@ -0,0 +1,105 @@
+﻿//-------------------------------------------------------------------------------------------------
+// <author>Pablo Perdomo Falcón</author>
+// <copyright file="Language.cs" company="Pabllopf">GNU General Public License v3.0</copyright>
+//-------------------------------------------------------------------------------------------------
+namespace Alis.Tools
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>Manage the languages of videogame.</summary>
+    public static class Language
+    {
+        /// <summary>Dictionary of current language</summary>
+        private static Dictionary<string, string> currentLanguage = new Dictionary<string, string>();
+
+        /// <summary>The directory</summary>
+        private static string directory = Environment.CurrentDirectory + "/Resources";
+
+        /// <summary>Occurs when [change].</summary>
+        public static event EventHandler<Idiom> OnChange;
+
+        /// <summary>Initializes the <see cref="Language" /> class.</summary>
+        static Language()
+        {
+            OnChange += Language_OnChange;
+        }
+
+        /// <summary>Translates to.</summary>
+        /// <param name="idiom">The idiom.</param>
+        public static void TranslateTo(Idiom idiom)
+        {
+            OnChange?.Invoke(idiom, idiom);
+        }
+
+        /// <summary>Gets the sentence.</summary>
+        /// <param name="idiom">The idiom.</param>
+        /// <param name="id">The sentence</param>
+        /// <returns>Return the sentence.</returns>
+        public static string GetSentence(Idiom idiom, string id) 
+        {
+            string directory = Environment.CurrentDirectory + "/Resources/";
+            string file = directory + "Languages.csv";
+
+            if (!Directory.Exists(directory)) 
+            {
+                throw new DirectoryNotFoundException(directory);
+            }
+
+            if (!File.Exists(file)) 
+            {
+                throw new FileNotFoundException(file);
+            }
+
+            string[] lineOfId = new List<string>(File.ReadLines(file, GetEncoding(file))).Find(i => i.Contains(id)).Split(',');
+            string[] languages = new List<string>(File.ReadLines(file, GetEncoding(file))).Find(i => i.Contains("LANGUAGE_EN")).Split(',');
+            string language = languages.ToList().Find(i => i.Contains(idiom.ToString()));
+            
+            return lineOfId[languages.ToList().IndexOf(language)];
+        }
+
+        /// <summary>Gets the encoding.</summary>
+        /// <param name="filename">The filename.</param>
+        /// <returns>Return the encode</returns>
+        public static Encoding GetEncoding(string filename)
+        {
+            var bom = new byte[4];
+            using (var file = new FileStream(filename, FileMode.Open, FileAccess.Read))
+            {
+                file.Read(bom, 0, 4);
+            }
+
+            switch (bom[0])
+            {
+                case 0x2b when bom[1] == 0x2f && bom[2] == 0x76:
+                    return Encoding.UTF7;
+                case 0xef when bom[1] == 0xbb && bom[2] == 0xbf:
+                    return Encoding.UTF8;
+                case 0xff when bom[1] == 0xfe && bom[2] == 0 && bom[3] == 0:
+                    return Encoding.UTF32;
+                case 0xff when bom[1] == 0xfe:
+                    return Encoding.Unicode;
+                case 0xfe when bom[1] == 0xff:
+                    return Encoding.BigEndianUnicode;
+                case 0 when bom[1] == 0 && bom[2] == 0xfe && bom[3] == 0xff:
+                    return new UTF32Encoding(true, true);
+                default:
+                    return Encoding.ASCII;
+            }
+        }
+
+        #region Define_Events
+
+        /// <summary>Languages the on change.</summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The e.</param>
+        [return: NotNull]
+        private static void Language_OnChange([NotNull] object sender, [NotNull] Idiom e) => Console.WriteLine("");
+
+        #endregion
+    }
+}
