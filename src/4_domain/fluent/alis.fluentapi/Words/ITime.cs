5fffe9b0f56a69260f365a586b8d4ff17509ff79
pabllopf <pabloperdomofalcon@gmail.com>
2021-11-03 12:52:16 +0100

    Add new tools

--- src/4_domain/fluent/alis.fluentapi/Words/ITime.cs
+++ src/4_domain/fluent/alis.fluentapi/Words/ITime.cs
@@ -35,7 +35,7 @@ namespace Alis.FluentApi.Words
     public interface ITime<Builder, Argument>
     {
         /// <summary>
-        ///     Times the value
+        /// Times the value
         /// </summary>
         /// <param name="value">The value</param>
         /// <returns>The builder</returns>
