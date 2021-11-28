ab33320eeab286971e774816d7930c5f3b27e437
pabllopf <pabloperdomofalcon@gmail.com>
2021-11-28 12:55:47 +0100

    add new test to tools of physcis

--- src/4_domain/core/systems/physics/2d/Alis.Core.Systems.Physics2D.Test/Tools/ConvexHull/TestChainHull.cs
+++ src/4_domain/core/systems/physics/2d/Alis.Core.Systems.Physics2D.Test/Tools/ConvexHull/TestChainHull.cs
@@ -1,11 +1,15 @@
 // 
 
+using System;
+using System.Numerics;
+using Alis.Core.Systems.Physics2D.Shared;
+using Alis.Core.Systems.Physics2D.Tools.ConvexHull;
 using NUnit.Framework;
 
 namespace Alis.Core.Systems.Physics2D.Test.Tools.ConvexHull
 {
     /// <summary>
-    /// The test chain hull class
+    ///     Test of class: Andrew's Monotone Chain Convex Hull algorithm. Used to get the convex hull of a point cloud.
     /// </summary>
     public class TestChainHull
     {
@@ -16,14 +20,25 @@ namespace Alis.Core.Systems.Physics2D.Test.Tools.ConvexHull
         public void Setup()
         {
         }
-
-        /// <summary>
-        ///     Tests that test 1
-        /// </summary>
+        
         [Test]
-        public void Test1()
+        public void TestGetConvexHull()
         {
-            Assert.Pass();
+            Vertices? vertices = new Vertices();
+            Vector2 point1 = new Vector2(0, 0);
+            Vector2 point2 = new Vector2(1, 0);
+            Vector2 point3 = new Vector2(0, 1);
+            
+            vertices.Add(point1);
+            vertices.Add(point2);
+            vertices.Add(point3);
+
+            Vertices? convexHull = ChainHull.GetConvexHull(vertices);
+            
+            Assert.AreEqual(convexHull.Count, 3);
+            Assert.IsTrue(convexHull[0] == vertices[0]);
+            Assert.IsTrue(convexHull[1] == vertices[1]);
+            Assert.IsTrue(convexHull[2] == vertices[2]);
         }
     }
 }
