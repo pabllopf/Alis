64ab0204961dd6357fad0cbd61bc0d7c4c715cd0
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2022-07-05 11:01:16 +0200

    Update restard_sln.bat

--- .scripts/windows/restard_sln.bat
+++ .scripts/windows/restard_sln.bat
@@ -1,4 +1,14 @@
 
+:choice
+set /P c=Are you sure you want to continue[Y/N]?
+if /I "%c%" EQU "Y" goto :somewhere
+if /I "%c%" EQU "N" goto :somewhere_else
+goto :choice
+
+:somewhere
+
+echo "Start"
+
 cd ..\..\
 
 dotnet new sln -o . -n Alis --force
@@ -16,3 +26,9 @@ FOR /d /r . %%d IN (obj) DO @IF EXIST "%%d" rd /s /q "%%d"
 for /r %%i in (*.csproj) do dotnet restore %%i
 
 cd .\.scripts\windows\
+
+:somewhere_else
+
+echo "Cancel"
+pause
+exit
