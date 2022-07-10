9f2be420f3f2a05c9290b946e7537a7b56416c5a
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2022-07-10 14:11:18 +0200

    Update the doc action

--- .scripts/windows/create_web.bat
+++ .scripts/windows/create_web.bat
@@ -1,9 +1,5 @@
-SET PATH=%PATH%;%CD%\\scripts\\graphviz\\bin
-
-dot -V
-
 @RD /S /Q ".\\docs\\docs"
 
-.\\scripts\\apidoc\\Doxygen\\doxygen.exe config
+.\\tools\\doxygen\\bin\\doxygen.exe config
 
 xcopy /E /Y /I .\\docs\\images .\\docs\\docs\\images
