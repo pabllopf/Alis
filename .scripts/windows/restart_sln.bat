1c5ccbb30605dc621db6dab2e3a893379ffbb230
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2022-07-13 21:52:32 +0200

    style: restore the sln

--- .scripts/windows/restart_sln.bat
+++ .scripts/windows/restart_sln.bat
@@ -18,10 +18,13 @@ dotnet new sln -o . -n Alis --force
 for /r %%i in (*.csproj) do dotnet sln Alis.sln add %%i
 
 rd /s /q .nuget
+rd /s /q .build
 
 FOR /d /r . %%d IN (bin) DO @IF EXIST "%%d" rd /s /q "%%d"
 
 FOR /d /r . %%d IN (obj) DO @IF EXIST "%%d" rd /s /q "%%d"
+
+for /r %%i in (*.csproj) do type default_csproj.props > %%i
 
 for /r %%i in (*.csproj) do dotnet restore %%i
 
