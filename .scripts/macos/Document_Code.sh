30583e87b955c9d1b1bb36b87389a186a5a91282
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2022-07-23 11:19:08 +0200

    fix: dont include xml files in repo

--- .scripts/macos/Document_Code.sh
+++ .scripts/macos/Document_Code.sh
@@ -1,8 +1,8 @@
 cd ../../
 
 /usr/local/share/dotnet/dotnet tool install --global dotnet-document --version 0.1.4-alpha
-/usr/local/share/dotnet/dotnet document apply ./Alis.sln 
+/usr/local/share/dotnet/dotnet document apply ./alis.sln 
 
-cd .scripts/macos/
+cd ./.scripts/macos/
 
 exit
