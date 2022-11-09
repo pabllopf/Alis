6d7bb99dc9112262086dd97da8d540f99c33af59
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2022-11-09 13:06:31 +0100

    fix: console errors

--- docs/web/js/script.js
+++ docs/web/js/script.js
@@ -103,61 +103,6 @@
   });
 
 
-  /* ========================================================================= */
-  /*   Contact Form Validating
-  /* ========================================================================= */
-
-  $('#contact-form').validate({
-      rules: {
-        name: {
-          required: true,
-          minlength: 4
-        },
-        email: {
-          required: true,
-          email: true
-        },
-        subject: {
-          required: false
-        },
-        message: {
-          required: true
-        }
-      },
-      messages: {
-        user_name: {
-          required: 'Come on, you have a name don\'t you?',
-          minlength: 'Your name must consist of at least 2 characters'
-        },
-        email: {
-          required: 'Please put your email address'
-        },
-        message: {
-          required: 'Put some messages here?',
-          minlength: 'Your name must consist of at least 2 characters'
-        }
-      },
-      submitHandler: function (form) {
-        $(form).ajaxSubmit({
-          type: 'POST',
-          data: $(form).serialize(),
-          url: 'sendmail.php',
-          success: function () {
-            $('#contact-form #success').fadeIn();
-          },
-          error: function () {
-            $('#contact-form #error').fadeIn();
-          }
-        });
-      }
-    }
-
-  );
-
-  /* ========================================================================= */
-  /*	On scroll fade/bounce effect
-  /* ========================================================================= */
-  var scroll = new SmoothScroll('a[href*="#"]');
 
   // -----------------------------
   //  Count Up
