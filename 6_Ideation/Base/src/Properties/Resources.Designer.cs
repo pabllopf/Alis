b1d1605baf2a641081e2682a6a25e7dba5369ada
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2022-12-24 12:57:18 +0000

    other: change the resource files

--- 6_Ideation/Base/src/Properties/Resources.Designer.cs
+++ 6_Ideation/Base/src/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace Alis.Core.Aspect.Base.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace Alis.Core.Aspect.Base.Properties {
         ///   Devuelve la instancia de ResourceManager almacenada en caché utilizada por esta clase.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Alis.Core.Aspect.Base.Properties.Resources", typeof(Resources).Assembly);
@@ -51,12 +51,92 @@ namespace Alis.Core.Aspect.Base.Properties {
         ///   búsquedas de recursos mediante esta clase de recurso fuertemente tipado.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Byte[].
+        /// </summary>
+        public static byte[] osx_arm64_libcsfml_audio_2_5_1 {
+            get {
+                object obj = ResourceManager.GetObject("osx_arm64_libcsfml_audio_2_5_1", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Byte[].
+        /// </summary>
+        public static byte[] osx_arm64_libcsfml_graphics_2_5_1 {
+            get {
+                object obj = ResourceManager.GetObject("osx_arm64_libcsfml_graphics_2_5_1", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Byte[].
+        /// </summary>
+        public static byte[] osx_arm64_libcsfml_system_2_5_1 {
+            get {
+                object obj = ResourceManager.GetObject("osx_arm64_libcsfml_system_2_5_1", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Byte[].
+        /// </summary>
+        public static byte[] osx_arm64_libcsfml_window_2_5_1 {
+            get {
+                object obj = ResourceManager.GetObject("osx_arm64_libcsfml_window_2_5_1", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Byte[].
+        /// </summary>
+        public static byte[] osx_x64_libcsfml_audio_2_5_1 {
+            get {
+                object obj = ResourceManager.GetObject("osx_x64_libcsfml_audio_2_5_1", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Byte[].
+        /// </summary>
+        public static byte[] osx_x64_libcsfml_graphics_2_5_1 {
+            get {
+                object obj = ResourceManager.GetObject("osx_x64_libcsfml_graphics_2_5_1", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Byte[].
+        /// </summary>
+        public static byte[] osx_x64_libcsfml_system_2_5_1 {
+            get {
+                object obj = ResourceManager.GetObject("osx_x64_libcsfml_system_2_5_1", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Byte[].
+        /// </summary>
+        public static byte[] osx_x64_libcsfml_window_2_5_1 {
+            get {
+                object obj = ResourceManager.GetObject("osx_x64_libcsfml_window_2_5_1", resourceCulture);
+                return ((byte[])(obj));
             }
         }
     }
