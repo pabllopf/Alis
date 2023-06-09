ae92dfc6a4ef6ee20213158b0736b96cb2d61050
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2023-06-09 14:26:47 +0200

    Update ImGuiGLRenderer.SDL.cs

--- 4_Operation/Graphic/src/ImGui/ImGuiGLRenderer.SDL.cs
+++ 4_Operation/Graphic/src/ImGui/ImGuiGLRenderer.SDL.cs
@@ -172,24 +172,15 @@ namespace Alis.Core.Graphic.ImGui
                     return;
                 }
                 case SdlEventType.SdlKeydown:
-                    if (evt.key.keysym.scancode == SdlScancode.SdlScancodeW)
-                    {
-                        io.KeysDown[(int)ImGuiKey.W] = true;
-                    }
-                    return;
                 case SdlEventType.SdlKeyup:
                 {
-                    if (evt.key.keysym.scancode == SdlScancode.SdlScancodeW)
-                    {
-                        io.KeysDown[(int)ImGuiKey.W] = false;
-                    }
-
-                    //SdlScancode key = evt.key.keysym.scancode;
-                    //io.KeysDown[(int) key] = evt.type == SdlEventType.SdlKeydown;
-                    //io.KeyShift = (SDL_GetModState() & SdlKeymod.KmodShift) != 0;
-                    //io.KeyCtrl = (SDL_GetModState() & SdlKeymod.KmodCtrl) != 0;
-                    //io.KeyAlt = (SDL_GetModState() & SdlKeymod.KmodAlt) != 0;
-                    //io.KeySuper = (SDL_GetModState() & SdlKeymod.KmodGui) != 0;
+                    SdlScancode key = evt.key.keysym.scancode;
+                    io.KeysDown[(int) key] = evt.type == SdlEventType.SdlKeydown;
+                    Console.WriteLine("io.KeysDown[" + key + "] = " + evt.type + io.KeysDown[(int) key]);
+                    io.KeyShift = (SDL_GetModState() & SdlKeymod.KmodShift) != 0;
+                    io.KeyCtrl = (SDL_GetModState() & SdlKeymod.KmodCtrl) != 0;
+                    io.KeyAlt = (SDL_GetModState() & SdlKeymod.KmodAlt) != 0;
+                    io.KeySuper = (SDL_GetModState() & SdlKeymod.KmodGui) != 0;
                     break;
                 }
             }
