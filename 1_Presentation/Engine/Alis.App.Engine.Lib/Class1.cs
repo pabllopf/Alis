7479d79d3b789f6baa7588cbcfe64a7d50626fe0
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2023-07-14 19:16:16 +0200

    feature: add the engine to web format

---
+++ 1_Presentation/Engine/Alis.App.Engine.Lib/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Alis.App.Engine.Lib
+{
+    public class Class1
+    {
+        [System.Runtime.InteropServices.UnmanagedCallersOnly(EntryPoint = "Answer")]
+        public static int Answer()
+        {
+            return 41;
+        }
+    }
+}
