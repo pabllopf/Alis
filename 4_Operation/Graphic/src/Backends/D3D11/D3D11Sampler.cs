c98a9542617d7a4e5ac99042f786bcb9134f1b04
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2023-08-12 13:20:35 +0100

    fix: veldrid config

--- 4_Operation/Graphic/src/Backends/D3D11/D3D11Sampler.cs
+++ 4_Operation/Graphic/src/Backends/D3D11/D3D11Sampler.cs
@@ -37,7 +37,7 @@ namespace Alis.Core.Graphic.Backends.D3D11
                 MinLOD = description.MinimumLod,
                 MaxLOD = description.MaximumLod,
                 MaxAnisotropy = (int)description.MaximumAnisotropy,
-                ComparisonFunction = comparision,
+                ComparisonFunc = comparision,
                 MipLODBias = description.LodBias,
                 BorderColor = ToRawColor4(description.BorderColor)
             };
