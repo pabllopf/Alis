c91a70a05e2b93bfa3794c1ce17c41cecf65f397
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2023-09-11 12:58:46 +0200

    new version 0.1.4

--- .scripts/macos/build_all.sh
+++ .scripts/macos/build_all.sh
@@ -15,7 +15,6 @@ echo "./**/bin/"
 rm -rf ./**/lib/
 echo "./**/lib/"
 
-
 for i in `find . -name "*.csproj" -type f`; 
   do if [[ $i != *".Template."* && $i != *".App."* && $i != *".Test."* && $i != *".Benchmark."* && $i != *".Sample."* ]] ; 
   then 
