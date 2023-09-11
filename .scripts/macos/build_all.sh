df4a9e58d984e7c6df52389e5d94c25c792ac895
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2023-09-11 12:18:49 +0200

    feature: new config to optimice the release libs

--- .scripts/macos/build_all.sh
+++ .scripts/macos/build_all.sh
@@ -1,9 +1,22 @@
 cd ../../
 
+rm -rf ./.build/
+echo "./.build/"
+
+rm -rf ./.nuget/
+echo "./.nuget/"
+
+rm -rf ./**/obj/
+echo "./**/obj/"
+
+rm -rf ./**/bin/
+echo "./**/bin/"
+
 for i in `find . -name "*.csproj" -type f`; 
   do if [[ $i != *".Template."* && $i != *".App."* && $i != *".Test."* && $i != *".Benchmark."* && $i != *".Sample."* ]] ; 
   then 
     echo "Build project $i" ;
+    dotnet restore $i;
     dotnet build $i -c Release; 
     dotnet build $i -r win-x64 -c Release;
     dotnet build $i -r win-arm64 -c Release;
@@ -14,6 +27,9 @@ for i in `find . -name "*.csproj" -type f`;
     dotnet build $i -r linux-x64 -c Release;
     dotnet build $i -r linux-x86 -c Release;
     dotnet build $i -r linux-arm64 -c Release;
+    else 
+      echo "Skip project $i";
+      dotnet restore $i;
 fi;done
 
 
