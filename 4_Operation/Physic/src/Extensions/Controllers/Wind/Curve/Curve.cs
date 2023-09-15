22f5ff59affc65544e73791e1e6d7c74ccc02993
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2023-09-15 12:29:01 +0200

    fix: Refactor this method to reduce its Cognitive Complexity from 16 to the 15 allowed of Curve

--- 4_Operation/Physic/src/Extensions/Controllers/Wind/Curve/Curve.cs
+++ 4_Operation/Physic/src/Extensions/Controllers/Wind/Curve/Curve.cs
@@ -75,7 +75,9 @@ namespace Alis.Core.Systems.Physics2D.Config.Extensions.Controllers.Wind.Curve
             return curve;
         }
 
-        /// <summary>Evaluate the value at a position of this <see cref="Curve" />.</summary>
+        /// <summary>
+        /// Evaluate the value at a position of this <see cref="Curve" />.
+        /// </summary>
         /// <param name="position">The position on this <see cref="Curve" />.</param>
         /// <returns>Value at the position on this <see cref="Curve" />.</returns>
         public float Evaluate(float position)
@@ -95,90 +97,96 @@ namespace Alis.Core.Systems.Physics2D.Config.Extensions.Controllers.Wind.Curve
 
             if (position < first.Position)
             {
-                switch (PreLoop)
-                {
-                    case CurveLoopType.Constant:
-                        //constant
-                        return first.Value;
-
-                    case CurveLoopType.Linear:
-                        // linear y = a*x +b with a target of last point
-                        return first.Value - first.TangentIn * (first.Position - position);
-
-                    case CurveLoopType.Cycle:
-                        //start -> end / start -> end
-                        int cycle = GetNumberOfCycle(position);
-                        float virtualPos = position - cycle * (last.Position - first.Position);
-                        return GetCurvePosition(virtualPos);
-
-                    case CurveLoopType.CycleOffset:
-                        //make the curve continue (with no step) so must up the curve each cycle of delta(value)
-                        cycle = GetNumberOfCycle(position);
-                        virtualPos = position - cycle * (last.Position - first.Position);
-                        return GetCurvePosition(virtualPos) + cycle * (last.Value - first.Value);
-
-                    case CurveLoopType.Oscillate:
-                        //go back on curve from end and target start 
-                        // start-> end / end -> start
-                        cycle = GetNumberOfCycle(position);
-                        if (0 == cycle % 2f) //if pair
-                        {
-                            virtualPos = position - cycle * (last.Position - first.Position);
-                        }
-                        else
-                        {
-                            virtualPos = last.Position - position + first.Position +
-                                         cycle * (last.Position - first.Position);
-                        }
-
-                        return GetCurvePosition(virtualPos);
-                }
+                return HandlePositionBeforeCurve(position, first, last);
             }
             else if (position > last.Position)
             {
-                int cycle;
-                switch (PostLoop)
+                return HandlePositionAfterCurve(position, first, last);
+            }
+
+            // In the curve
+            return GetCurvePosition(position);
+        }
+
+        /// <summary>
+        /// Handles the position before curve using the specified position
+        /// </summary>
+        /// <param name="position">The position</param>
+        /// <param name="first">The first</param>
+        /// <param name="last">The last</param>
+        /// <returns>The float</returns>
+        private float HandlePositionBeforeCurve(float position, CurveKey first, CurveKey last)
+        {
+            switch (PreLoop)
+            {
+                case CurveLoopType.Constant:
+                    return first.Value;
+
+                case CurveLoopType.Linear:
+                    return first.Value - first.TangentIn * (first.Position - position);
+
+                case CurveLoopType.Cycle:
+                case CurveLoopType.CycleOffset:
+                case CurveLoopType.Oscillate:
+                    int cycle = GetNumberOfCycle(position);
+                    float virtualPos = position - cycle * (last.Position - first.Position);
+                    return HandleLooping(virtualPos, first, last, cycle);
+            }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Handles the position after curve using the specified position
+        /// </summary>
+        /// <param name="position">The position</param>
+        /// <param name="first">The first</param>
+        /// <param name="last">The last</param>
+        /// <returns>The float</returns>
+        private float HandlePositionAfterCurve(float position, CurveKey first, CurveKey last)
+        {
+            switch (PostLoop)
+            {
+                case CurveLoopType.Constant:
+                    return last.Value;
+
+                case CurveLoopType.Linear:
+                    return last.Value + first.TangentOut * (position - last.Position);
+
+                case CurveLoopType.Cycle:
+                case CurveLoopType.CycleOffset:
+                case CurveLoopType.Oscillate:
+                    int cycle = GetNumberOfCycle(position);
+                    float virtualPos = position - cycle * (last.Position - first.Position);
+                    return HandleLooping(virtualPos, first, last, cycle);
+            }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Handles the looping using the specified virtual pos
+        /// </summary>
+        /// <param name="virtualPos">The virtual pos</param>
+        /// <param name="first">The first</param>
+        /// <param name="last">The last</param>
+        /// <param name="cycle">The cycle</param>
+        /// <returns>The float</returns>
+        private float HandleLooping(float virtualPos, CurveKey first, CurveKey last, int cycle)
+        {
+            if (PreLoop == CurveLoopType.Oscillate)
+            {
+                if (cycle % 2 == 0)
                 {
-                    case CurveLoopType.Constant:
-                        //constant
-                        return last.Value;
-
-                    case CurveLoopType.Linear:
-                        // linear y = a*x +b with a target of last point
-                        return last.Value + first.TangentOut * (position - last.Position);
-
-                    case CurveLoopType.Cycle:
-                        //start -> end / start -> end
-                        cycle = GetNumberOfCycle(position);
-                        float virtualPos = position - cycle * (last.Position - first.Position);
-                        return GetCurvePosition(virtualPos);
-
-                    case CurveLoopType.CycleOffset:
-                        //make the curve continue (with no step) so must up the curve each cycle of delta(value)
-                        cycle = GetNumberOfCycle(position);
-                        virtualPos = position - cycle * (last.Position - first.Position);
-                        return GetCurvePosition(virtualPos) + cycle * (last.Value - first.Value);
-
-                    case CurveLoopType.Oscillate:
-                        //go back on curve from end and target start 
-                        // start-> end / end -> start
-                        cycle = GetNumberOfCycle(position);
-                        if (0 == cycle % 2f) //if pair
-                        {
-                            virtualPos = position - cycle * (last.Position - first.Position);
-                        }
-                        else
-                        {
-                            virtualPos = last.Position - position + first.Position +
-                                         cycle * (last.Position - first.Position);
-                        }
-
-                        return GetCurvePosition(virtualPos);
+                    virtualPos = virtualPos - cycle * (last.Position - first.Position);
+                }
+                else
+                {
+                    virtualPos = last.Position - virtualPos + first.Position + cycle * (last.Position - first.Position);
                 }
             }
 
-            //in curve
-            return GetCurvePosition(position);
+            return GetCurvePosition(virtualPos);
         }
 
         /// <summary>Computes tangents for all keys in the collection.</summary>
