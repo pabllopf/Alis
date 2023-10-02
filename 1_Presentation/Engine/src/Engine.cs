e838f28524e3f0f3746c318ae3de0cd0398fe75f
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2023-10-02 17:30:53 +0200

    Update Engine.cs

--- 1_Presentation/Engine/src/Engine.cs
+++ 1_Presentation/Engine/src/Engine.cs
@@ -316,7 +316,7 @@ namespace Alis.App.Engine
             fonts.TexId = (IntPtr) _fontTextureId;
             fonts.ClearTexData();
 
-            
+            // CONFIG DOCKSPACE
             ImGuiViewportPtr viewport = ImGui.GetMainViewport();
             ImGui.SetNextWindowPos(viewport.WorkPos);
             ImGui.SetNextWindowSize(viewport.WorkSize);
