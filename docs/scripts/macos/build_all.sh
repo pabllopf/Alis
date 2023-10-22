f4936538c153e638072fd23f546c326c1a2688a2
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2023-10-22 20:18:53 +0200

    Update build_all.sh

--- docs/scripts/macos/build_all.sh
+++ docs/scripts/macos/build_all.sh
@@ -46,6 +46,26 @@ select yn in "Yes" "No"; do
           dotnet restore $i;
     fi;done
     
+    for i in `find . -name "*.csproj" -type f`; 
+          do if [[ $i != *".Template."* && $i != *".App."* && $i != *".Test."* && $i != *".Benchmark."* && $i != *".Sample."* ]] ; 
+          then 
+            echo "Build project $i" ;
+            dotnet restore $i;
+            dotnet build $i -c Debug; 
+            dotnet build $i -r win-x64 -c Debug;
+            dotnet build $i -r win-arm64 -c Debug;
+            dotnet build $i -r win-x86 -c Debug;
+            dotnet build $i -r win-arm -c Debug;
+            dotnet build $i -r osx-x64 -c Debug;
+            dotnet build $i -r osx-arm64 -c Debug;
+            dotnet build $i -r linux-x64 -c Debug;
+            dotnet build $i -r linux-x86 -c Debug;
+            dotnet build $i -r linux-arm64 -c Debug;
+            else 
+              echo "Skip project $i";
+              dotnet restore $i;
+        fi;done
+    
     
     cd ./docs/scripts/macos/ || exit 
 
