a8d83e87f064090590dad6bdcbc29021376cd1ff
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2023-12-24 18:32:14 +0100

    fix: trim error

---
+++ 2_Application/Alis/sample/alis.sample.flappy.bird/BirdController.cs
@@ -0,0 +1,53 @@
+// --------------------------------------------------------------------------
+// 
+//                               █▀▀█ ░█─── ▀█▀ ░█▀▀▀█
+//                              ░█▄▄█ ░█─── ░█─ ─▀▀▀▄▄
+//                              ░█─░█ ░█▄▄█ ▄█▄ ░█▄▄▄█
+// 
+//  --------------------------------------------------------------------------
+//  File:$FILENAME$
+// 
+//  Author:Pablo Perdomo Falcón
+//  Web:https://www.pabllopf.dev/
+// 
+//  Copyright (c) 2021 GNU General Public License v3.0
+// 
+//  This program is free software:you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.If not, see <http://www.gnu.org/licenses/>.
+// 
+//  --------------------------------------------------------------------------
+
+using System;
+using Alis.Core.Aspect.Base.Mapping;
+using Alis.Core.Aspect.Math;
+using Alis.Core.Aspect.Math.Vector;
+using Alis.Core.Ecs.Component;
+using Alis.Core.Ecs.Component.Collider;
+
+namespace Alis.Sample.Flappy.Bird
+{
+    public class BirdController : Component
+    {
+        public override void OnPressKey(SdlKeycode key)
+        {
+            if (key == SdlKeycode.SdlkSpace)
+            {
+                if (GameObject.Contains<BoxCollider>())
+                {
+                    GameObject.Get<BoxCollider>().Body.LinearVelocity = new Vector2(0, -30f);
+                    Console.WriteLine("Go up!");
+                }
+            }
+        }
+    }
+}
