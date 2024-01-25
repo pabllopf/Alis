82eda6f13c814e1773810bde1225c065a47ab30a
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2024-01-25 20:17:59 +0100

    Update SdlTest.cs

--- 4_Operation/Graphic/test/Sdl2/SdlTest.cs
+++ 4_Operation/Graphic/test/Sdl2/SdlTest.cs
@@ -197,19 +197,21 @@ namespace Alis.Core.Graphic.Test.Sdl2
 
                 // Act
                 int result = Sdl.AudioInit(name);
-                if (result == 0)
+                switch (result)
                 {
-                    // Assert
-                    // Here you need to assert that the result is as expected. This will depend on your implementation.
-                    Assert.Equal(0, result);
+                    case 0:
+                        // Assert
+                        // Here you need to assert that the result is as expected. This will depend on your implementation.
+                        Assert.Equal(0, result);
+                        break;
+                    case -1:
+                        // Assert
+                        // Here you need to assert that the result is as expected. This will depend on your implementation.
+                        Assert.Equal(-1, result);
+                        break;
                 }
-                if (result == -1)
-                {
-                    // Assert
-                    // Here you need to assert that the result is as expected. This will depend on your implementation.
-                    Assert.Equal(-1, result);
-                }
-                if (result == -1 || result == 0)
+
+                if (result != -1 && result != 0)
                 {
                     Assert.NotEqual(0, result);
                     Assert.NotEqual(-1, result);
