2009521afa3e9bffc487420be9295ce7e21fd988
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2024-02-04 12:57:19 +0100

    test: create simple test of audio module

--- 4_Operation/Audio/test/OS/PlayerTest.cs
+++ 4_Operation/Audio/test/OS/PlayerTest.cs
@@ -5,7 +5,7 @@
 //                              ░█─░█ ░█▄▄█ ▄█▄ ░█▄▄▄█
 // 
 //  --------------------------------------------------------------------------
-//  File:DefaultTest.cs
+//  File:PlayerTest.cs
 // 
 //  Author:Pablo Perdomo Falcón
 //  Web:https://www.pabllopf.dev/
@@ -29,18 +29,12 @@
 
 using Xunit;
 
-namespace Alis.Core.Audio.Test
+namespace Alis.Core.Audio.Test.OS
 {
-    /// <summary>
-    ///     The default test class
-    /// </summary>
-    public class DefaultTest
+    public class PlayerTest
     {
-        /// <summary>
-        ///     Tests that test
-        /// </summary>
         [Fact]
-        public void Test()
+        public void TestMethod()
         {
             Assert.True(true);
         }
