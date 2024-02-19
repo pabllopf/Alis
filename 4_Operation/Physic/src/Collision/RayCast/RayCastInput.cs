4c519663dfbfb26c93bb9025d0a916b9a92a2a13
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2024-02-19 15:50:10 +0100

    add fields to RayCastInput

--- 4_Operation/Physic/src/Collision/RayCast/RayCastInput.cs
+++ 4_Operation/Physic/src/Collision/RayCast/RayCastInput.cs
@@ -38,12 +38,12 @@ namespace Alis.Core.Physic.Collision.RayCast
         ///     The ray extends from p1 to p1 + maxFraction * (p2 - p1). If you supply a max fraction of 1, the ray extends
         ///     from p1 to p2. A max fraction of 0.5 makes the ray go from p1 and half way to p2.
         /// </summary>
-        public float Fraction;
+        public float Fraction { get; set; }
 
         /// <summary>The starting point of the ray.</summary>
-        public Vector2 Point1;
+        public Vector2 Point1 { get; set; }
 
         /// <summary>The ending point of the ray.</summary>
-        public Vector2 Point2;
+        public Vector2 Point2 { get; set; }
     }
 }
