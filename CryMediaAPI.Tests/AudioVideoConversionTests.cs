2ad280b9c1961a4734707eee37990c0b990107cb
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2024-02-28 08:07:55 +0100

    new video encoder

---
+++ CryMediaAPI.Tests/AudioVideoConversionTests.cs
@@ -0,0 +1,150 @@
+﻿using CryMediaAPI.Audio;
+using CryMediaAPI.Encoding.Builders;
+using CryMediaAPI.Resources;
+using CryMediaAPI.Video;
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using CryMediaAPI.BaseClasses;
+using Xunit;
+
+namespace CryMediaAPI.Tests
+{
+    public class AudioVideoConversionTests
+    {
+        [Fact]
+        public async Task ConversionTest()
+        {
+            string vpath = Res.GetPath(Res.Video_Mp4);
+            string apath = Res.GetPath(Res.Audio_Mp3);
+            string opath = "out-test-av-1.mp4";
+
+            try
+            {
+                using VideoReader vreader = new VideoReader(vpath);
+                await vreader.LoadMetadataAsync();
+                vreader.Load();
+
+                using AudioReader areader = new AudioReader(apath);
+                await areader.LoadMetadataAsync();
+                areader.Load();
+
+                // Get video and audio stream metadata
+                MediaStream vstream = vreader.Metadata.GetFirstVideoStream();
+                MediaStream astream = areader.Metadata.GetFirstAudioStream();
+
+                // Prepare writer (Converting to H.264 + AAC video)
+                using (AudioVideoWriter writer = new AudioVideoWriter(opath,
+                    vstream.Width.Value,
+                    vstream.Height.Value,
+                    vstream.AvgFrameRateNumber,
+                    astream.Channels.Value,
+                    astream.SampleRateNumber, 16,
+                    new H264Encoder().Create(),
+                    new AACEncoder().Create()))
+                {
+
+                    // Open for writing (this starts the FFmpeg process)
+                    writer.OpenWrite();
+
+                    // Copy raw data directly from stream to stream
+                    Task t2 = areader.DataStream.CopyToAsync(writer.InputDataStreamAudio);
+                    Task t1 = vreader.DataStream.CopyToAsync(writer.InputDataStreamVideo);
+
+                    await t1;
+                    await t2;
+                }
+
+                using VideoReader video = new VideoReader(opath);
+                await video.LoadMetadataAsync();
+
+                Assert.True(video.Metadata.Streams.Length == 2);
+
+                vstream = video.Metadata.GetFirstVideoStream();
+                astream = video.Metadata.GetFirstAudioStream();
+
+                Assert.True(Math.Abs(vstream.AvgFrameRateNumber - vreader.Metadata.AvgFramerate) < 0.1);
+                Assert.True(vstream.Width.Value == vreader.Metadata.Width);
+                Assert.True(vstream.Height.Value == vreader.Metadata.Height);
+
+                Assert.True(astream.SampleRateNumber == areader.Metadata.SampleRate);
+            }
+            finally
+            {
+                if (File.Exists(opath)) File.Delete(opath);
+            }
+        }
+
+        [Fact]
+        public async Task ConversionStreamTest()
+        {
+            string vpath = Res.GetPath(Res.Video_Mp4);
+            string apath = Res.GetPath(Res.Audio_Mp3);
+            string opath = "out-test-av-2.mp4";
+
+            try
+            {
+                using VideoReader vreader = new VideoReader(vpath);
+                await vreader.LoadMetadataAsync();
+                vreader.Load();
+
+                using AudioReader areader = new AudioReader(apath);
+                await areader.LoadMetadataAsync();
+                areader.Load();
+
+                // Get video and audio stream metadata
+                MediaStream vstream = vreader.Metadata.GetFirstVideoStream();
+                MediaStream astream = areader.Metadata.GetFirstAudioStream();
+
+                H264Encoder encoder = new H264Encoder
+                {
+                    Format = "flv"
+                };
+
+                using (FileStream filestream = File.Create(opath))
+                {
+                    // Prepare writer (Converting to H.264 + AAC video)
+                    using (AudioVideoWriter writer = new AudioVideoWriter(filestream,
+                        vstream.Width.Value,
+                        vstream.Height.Value,
+                        vstream.AvgFrameRateNumber,
+                        astream.Channels.Value,
+                        astream.SampleRateNumber, 16,
+                        encoder.Create(), 
+                        new AACEncoder().Create()))
+                    {
+
+                        // Open for writing (this starts the FFmpeg process)
+                        writer.OpenWrite();
+
+                        // Copy raw data directly from stream to stream
+                        Task t2 = areader.DataStream.CopyToAsync(writer.InputDataStreamAudio);
+                        Task t1 = vreader.DataStream.CopyToAsync(writer.InputDataStreamVideo);
+
+                        await t1;
+                        await t2;
+                    }
+                }
+
+                using VideoReader video = new VideoReader(opath);
+                await video.LoadMetadataAsync();
+
+                Assert.True(video.Metadata.Streams.Length == 2);
+
+                vstream = video.Metadata.GetFirstVideoStream();
+                astream = video.Metadata.GetFirstAudioStream();
+
+                Assert.True(Math.Abs(vstream.AvgFrameRateNumber - vreader.Metadata.AvgFramerate) < 0.1);
+                Assert.True(Math.Abs(video.Metadata.Duration - vreader.Metadata.Duration) < 0.2);
+                Assert.True(vstream.Width.Value == vreader.Metadata.Width);
+                Assert.True(vstream.Height.Value == vreader.Metadata.Height);
+                Assert.True(astream.SampleRateNumber == areader.Metadata.SampleRate);
+            }
+            finally
+            {
+                if (File.Exists(opath)) File.Delete(opath);
+            }
+        }
+    }
+}
---
+++ CryMediaAPI/Audio/AudioWriter.cs
@@ -0,0 +1,137 @@
+using System;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+using CryMediaAPI.BaseClasses;
+using CryMediaAPI.Encoding;
+using CryMediaAPI.Encoding.Builders;
+
+namespace CryMediaAPI.Audio;
+
+public class AudioWriter : MediaWriter<AudioFrame>, IDisposable
+{
+    string ffmpeg;
+    CancellationTokenSource csc;
+    internal Process ffmpegp;
+
+    public Process CurrentFFmpegProcess => ffmpegp;
+
+    public int Channels { get; }
+    public int SampleRate { get; }
+    public int BitDepth { get; }
+    public bool UseFilename { get; }
+    public EncoderOptions EncoderOptions { get; }
+
+    public Stream DestinationStream { get; private set; }
+    public Stream OutputDataStream { get; private set; }
+
+
+    /// <summary>
+    /// Used for encoding audio samples into a new audio file
+    /// </summary>
+    /// <param name="filename">Output audio file name/path</param>
+    /// <param name="channels">Input number of channels</param>
+    /// <param name="sampleRate">Input sample rate</param>
+    /// <param name="bitDepth">Input bits per sample</param>
+    /// <param name="encoderOptions">Extra FFmpeg encoding options that will be passed to FFmpeg</param>
+    /// <param name="ffmpegExecutable">Name or path to the ffmpeg executable</param>
+    public AudioWriter(string filename, int channels, int sampleRate, int bitDepth = 16,
+        EncoderOptions encoderOptions = null, string ffmpegExecutable = "ffmpeg")
+    {
+        if (channels <= 0 || sampleRate <= 0) throw new InvalidDataException("Channels/Sample rate have to be bigger than 0!");
+        if (bitDepth != 16 && bitDepth != 24 && bitDepth != 32) throw new InvalidOperationException("Acceptable bit depths are 16, 24 and 32");
+        if (string.IsNullOrEmpty(filename)) throw new NullReferenceException("Filename can't be null or empty!");
+
+        UseFilename = true;
+        ffmpeg = ffmpegExecutable;
+
+        Channels = channels;
+        BitDepth = bitDepth;
+        SampleRate = sampleRate;
+
+        Filename = filename;
+        EncoderOptions = encoderOptions ?? new MP3Encoder().Create();
+    }
+
+    /// <summary>
+    /// Used for encoding audio samples into a stream
+    /// </summary>
+    /// <param name="destinationStream">Output stream</param>
+    /// <param name="channels">Input number of channels</param>
+    /// <param name="sampleRate">Input sample rate</param>
+    /// <param name="bitDepth">Input bits per sample</param>
+    /// <param name="encoderOptions">Extra FFmpeg encoding options that will be passed to FFmpeg</param>
+    /// <param name="ffmpegExecutable">Name or path to the ffmpeg executable</param>
+    public AudioWriter(Stream destinationStream, int channels, int sampleRate, int bitDepth = 16,
+        EncoderOptions encoderOptions = null, string ffmpegExecutable = "ffmpeg")
+    {
+        if (channels <= 0 || sampleRate <= 0) throw new InvalidDataException("Channels/Sample rate have to be bigger than 0!");
+        if (bitDepth != 16 && bitDepth != 24 && bitDepth != 32) throw new InvalidOperationException("Acceptable bit depths are 16, 24 and 32");
+        UseFilename = false;
+        ffmpeg = ffmpegExecutable;
+
+        Channels = channels;
+        BitDepth = bitDepth;
+        SampleRate = sampleRate;
+
+        DestinationStream = destinationStream ?? throw new NullReferenceException("Stream can't be null!");
+        EncoderOptions = encoderOptions ?? new MP3Encoder().Create();
+    }
+
+    public void OpenWrite(bool showFFmpegOutput = false)
+    {
+        if (OpenedForWriting) throw new InvalidOperationException("File was already opened for writing!");
+
+        string cmd = $"-f s{BitDepth}le -channels {Channels} -sample_rate {SampleRate} -i - " +
+                     $"-c:a {EncoderOptions.EncoderName} {EncoderOptions.EncoderArguments} -f {EncoderOptions.Format}";
+
+        if (UseFilename)
+        {
+            if (File.Exists(Filename)) File.Delete(Filename);
+
+            InputDataStream = FFmpegWrapper.OpenInput(ffmpeg, $"{cmd} \"{Filename}\"", out ffmpegp, showFFmpegOutput);
+        }
+        else
+        {
+            csc = new CancellationTokenSource();
+
+            // using stream
+            (InputDataStream, OutputDataStream) = FFmpegWrapper.Open(ffmpeg, $"{cmd} -", out ffmpegp, showFFmpegOutput);
+            _ = OutputDataStream.CopyToAsync(DestinationStream, 81920, csc.Token); // 81920 is the default buffer size
+        }
+
+        OpenedForWriting = true;
+    }
+
+    /// <summary>
+    /// Closes output audio file.
+    /// </summary>
+    public void CloseWrite()
+    {
+        if (!OpenedForWriting) throw new InvalidOperationException("File is not opened for writing!");
+
+        try
+        {
+            InputDataStream.Dispose();
+            ffmpegp.WaitForExit();
+            csc?.Cancel();
+
+            if (!UseFilename) OutputDataStream?.Dispose();
+
+            try
+            {
+                if (ffmpegp?.HasExited == false) ffmpegp.Kill();
+            }
+            catch { }
+        }
+        finally
+        {
+            OpenedForWriting = false;
+        }
+    }
+
+    public void Dispose()
+    {
+        if (OpenedForWriting) CloseWrite();
+    }
+}
