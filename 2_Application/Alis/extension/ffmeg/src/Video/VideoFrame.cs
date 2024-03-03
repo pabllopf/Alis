ba18e9b897425f25af39c924a209275e99d5475f
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2024-03-03 20:12:07 +0100

    fix: the solution path

--- 2_Application/Alis/extension/ffmeg/src/Video/VideoFrame.cs
+++ 2_Application/Alis/extension/ffmeg/src/Video/VideoFrame.cs
@@ -18,6 +18,7 @@ namespace Alis.Extension.FFMeg.Video
         /// The frame buffer
         /// </summary>
         byte[] frameBuffer;
+
         /// <summary>
         /// Raw video data in RGB24 pixel format
         /// </summary>
@@ -27,6 +28,7 @@ namespace Alis.Extension.FFMeg.Video
         /// Video width in pixels
         /// </summary>
         public int Width { get; }
+
         /// <summary>
         /// Video height in pixels
         /// </summary>
@@ -38,45 +40,45 @@ namespace Alis.Extension.FFMeg.Video
         /// <param name="w">Width in pixels</param>
         /// <param name="h">Height in pixels</param>
         public VideoFrame(int w, int h)
-    {
-        if (w <= 0 || h <= 0) throw new InvalidDataException("Video frame dimensions have to be bigger than 0 pixels!");
+        {
+            if (w <= 0 || h <= 0) throw new InvalidDataException("Video frame dimensions have to be bigger than 0 pixels!");
 
-        Width = w;
-        Height = h;
+            Width = w;
+            Height = h;
 
-        size = Width * Height * 3;
-        frameBuffer = new byte[size];
-        RawData = frameBuffer.AsMemory();
-    }
+            size = Width * Height * 3;
+            frameBuffer = new byte[size];
+            RawData = frameBuffer.AsMemory();
+        }
 
         /// <summary>
         /// Loads frame data from stream.
         /// </summary>
         /// <param name="str">Stream containing raw frame data in RGB24 format</param>
         public bool Load(Stream str)
-    {
-        offset = 0;
-
-        while (offset < size)
         {
-            int r = str.Read(frameBuffer, offset, size - offset);
-            if (r <= 0)
+            offset = 0;
+
+            while (offset < size)
             {
-                if (offset == 0) return false;
-                else break;
+                int r = str.Read(frameBuffer, offset, size - offset);
+                if (r <= 0)
+                {
+                    if (offset == 0) return false;
+                    else break;
+                }
+
+                offset += r;
             }
 
-            offset += r;
-        }
+            // Adjust RawData length when changed
+            if (RawData.Length != offset)
+            {
+                RawData = frameBuffer.AsMemory().Slice(0, offset);
+            }
 
-        // Adjust RawData length when changed
-        if (RawData.Length != offset)
-        {
-            RawData = frameBuffer.AsMemory().Slice(0, offset);
+            return true;
         }
-
-        return true;
-    }
 
         /// <summary>
         /// Saves the output
@@ -87,21 +89,21 @@ namespace Alis.Extension.FFMeg.Video
         /// <param name="ffmpegExecutable">The ffmpeg executable</param>
         public void Save(string output, string encoder = "png", string extraParameters = "",
             string ffmpegExecutable = "ffmpeg")
-    {
-        if (File.Exists(output)) File.Delete(output);
-
-        using (Stream inp = FfMpegWrapper.OpenInput(ffmpegExecutable,
-                   $"-f rawvideo -video_size {Width}:{Height} -pixel_format rgb24 -i - " +
-                   $"-c:v {encoder} {extraParameters} -f image2pipe \"{output}\"",
-                   out _, false))
         {
-            // save it
-            Console.WriteLine("Saving frame...");
-            byte[] data = RawData.ToArray();
-            Console.WriteLine($"Writing Length {data.Length} bytes to ffmpeg...");
-            inp.Write(data, 0, data.Length);
+            if (File.Exists(output)) File.Delete(output);
+
+            using (Stream inp = FfMpegWrapper.OpenInput(ffmpegExecutable,
+                       $"-f rawvideo -video_size {Width}:{Height} -pixel_format rgb24 -i - " +
+                       $"-c:v {encoder} {extraParameters} -f image2pipe \"{output}\"",
+                       out _, false))
+            {
+                // save it
+                Console.WriteLine("Saving frame...");
+                byte[] data = RawData.ToArray();
+                Console.WriteLine($"Writing Length {data.Length} bytes to ffmpeg...");
+                inp.Write(data, 0, data.Length);
+            }
         }
-    }
 
         /// <summary>
         /// Returns part of memory that contains the pixel data.
@@ -109,18 +111,18 @@ namespace Alis.Extension.FFMeg.Video
         /// <param name="x">Starting X coordinate of wanted pixel/s</param>
         /// <param name="y">Starting Y coordinate of wanted pixel/s</param>
         /// <param name="length">Number of pixels to return from the starting pixel</param>
-        public Memory<byte> GetPixels(int x, int y, int length = 1) 
-    {
-        int index = (x + y * Width) * 3;
-        return RawData.Slice(index, length * 3);
-    }
+        public Memory<byte> GetPixels(int x, int y, int length = 1)
+        {
+            int index = (x + y * Width) * 3;
+            return RawData.Slice(index, length * 3);
+        }
 
         /// <summary>
         /// Clears the frame buffer
         /// </summary>
         public void Dispose()
-    {
-        frameBuffer = null;
-    }
+        {
+            frameBuffer = null;
+        }
     }
 }
