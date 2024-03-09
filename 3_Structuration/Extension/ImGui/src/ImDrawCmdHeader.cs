af9d3ef55e917d070ef486cbd03c18448e77cf3e
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2024-03-09 18:28:02 +0100

    style: union ImDrawCmdHeader

--- 3_Structuration/Extension/ImGui/src/ImDrawCmdHeader.cs
+++ 3_Structuration/Extension/ImGui/src/ImDrawCmdHeader.cs
@@ -33,22 +33,22 @@ using Alis.Core.Aspect.Math.Vector;
 namespace Alis.Core.Extension.ImGui
 {
     /// <summary>
-    ///     The im draw cmd header
+    /// The im draw cmd header
     /// </summary>
     public struct ImDrawCmdHeader
     {
         /// <summary>
-        ///     The clip rect
+        /// The clip rect
         /// </summary>
         public Vector4 ClipRect;
-
+        
         /// <summary>
-        ///     The texture id
+        /// The texture id
         /// </summary>
         public IntPtr TextureId;
-
+        
         /// <summary>
-        ///     The vtx offset
+        /// The vtx offset
         /// </summary>
         public uint VtxOffset;
     }
