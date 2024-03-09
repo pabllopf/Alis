60392fdfa82219fcf3650b4c8f9ba35b2053a38c
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2024-03-09 14:15:48 +0100

    fix: sample extension of alis

---
+++ 3_Structuration/Extension/ImGui/sample/Program.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Alis.Core.Extension.ImGui.Sample
+{
+    /// <summary>
+    /// The program class
+    /// </summary>
+    internal static class Program
+    {
+        /// <summary>
+        /// Main the args
+        /// </summary>
+        /// <param name="args">The args</param>
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 3_Structuration/Extension/ImGui/test/DummyTest.cs
@@ -0,0 +1,19 @@
+using Xunit;
+
+namespace Alis.Core.Extension.ImGui.Test
+{
+    /// <summary>
+    /// The dummy test class
+    /// </summary>
+    public class DummyTest
+    {
+        /// <summary>
+        /// Tests that default test
+        /// </summary>
+        [Fact]
+        public void DefaultTest()  
+        {
+            Assert.True(true);
+        }
+    }
+}
