579b5bb9f669760ab8e39328c94cf6097ee3b52e
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2024-03-20 18:01:26 +0000

    change the main loop

--- 2_Application/Extension/OpenGL/sample/Program.cs
+++ 2_Application/Extension/OpenGL/sample/Program.cs
@@ -1,11 +1,4 @@
 using System;
-using System.Runtime.InteropServices;
-using Alis.Core.Aspect.Math.Matrix;
-using Alis.Core.Graphic.Sdl2;
-using Alis.Core.Graphic.Sdl2.Enums;
-using Alis.Core.Graphic.Sdl2.Structs;
-using Alis.Extension.OpenGL.Enums;
-using Version = Alis.Core.Graphic.Sdl2.Structs.Version;
 
 namespace Alis.Extension.OpenGL.Sample
 {
@@ -13,12 +6,6 @@ namespace Alis.Extension.OpenGL.Sample
     {
         public static void Main()
         {
-            IntPtr window = default(IntPtr);
-            IntPtr context = default(IntPtr);
-
-            TriangleSample triangleSample = new TriangleSample();
-            CubeSample cubeSample = new CubeSample();
-            
             Console.WriteLine("Enter the number of the sample you want to run:");
             Console.WriteLine("1. Triangle Sample");
             Console.WriteLine("2. Cube Sample");
@@ -27,47 +14,14 @@ namespace Alis.Extension.OpenGL.Sample
             switch (sampleNumber)
             {
                 case 1:
-                    window = triangleSample.Initialize(out context);
+                    TriangleSample triangleSample = new TriangleSample();
+                    triangleSample.Run();
                     break;
                 case 2:
-                    window = cubeSample.Initialize(out context);
+                    CubeSample cubeSample = new CubeSample();
+                    cubeSample.Run();
                     break;
             }
-            
-            // Main loop
-            bool running = true;
-            while (running)
-            {
-                // Event handling
-                while (Sdl.PollEvent(out Event evt) != 0)
-                {
-                    if (evt.type == EventType.Quit)
-                    {
-                        running = false;
-                    }
-                }
-
-                // Clear the screen
-                Gl.GlClear(ClearBufferMask.ColorBufferBit);
-                // Draw the triangle
-                switch (sampleNumber)
-                {
-                    case 1:
-                        triangleSample.Draw();
-                        break;
-                    case 2:
-                        cubeSample.Draw();
-                        break;
-                }
-                
-                // Swap the buffers to display the triangle
-                Sdl.SwapWindow(window);
-            }
-
-            // Cleanup SDL
-            Sdl.DeleteContext(context);
-            Sdl.DestroyWindow(window);
-            Sdl.Quit();
         }
     }
 }
