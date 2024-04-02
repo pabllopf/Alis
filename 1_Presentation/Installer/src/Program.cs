6ae2b44b0d2a1ec180612f3ea2b022fbd82f1198
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2024-04-02 22:31:07 +0200

    Update Program.cs

--- 1_Presentation/Installer/src/Program.cs
+++ 1_Presentation/Installer/src/Program.cs
@@ -42,7 +42,8 @@ namespace Alis.App.Installer
         /// <param name="args">The args</param>
         public static void Main(string[] args)
         {
-            Console.WriteLine(" Hello World!");
+            Console.WriteLine("Please, press any key to continue...");
+            Console.ReadKey();
         }
     }
 }
