0612757ae60effe442cb40d3c95c142b8247af0b
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2024-04-09 20:11:33 +0200

    feature: new extension with path generator

---
+++ 1_Presentation/Extension/Math/PathGenerator/sample/Program.cs
@@ -0,0 +1,24 @@
+using Alis.Core.Aspect.Logging;
+
+namespace Alis.Extension.Math.HighSpeedPriorityQueue.Sample
+{
+    /// <summary>
+    ///     The program class
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        ///     Main
+        /// </summary>
+        public static void Main()
+        {
+            FastPriorityQueueExample.RunExample();
+
+            Logger.Info("------------------------------");
+
+            SimplePriorityQueueExample.RunExample();
+
+            Logger.Info("End Program...");
+        }
+    }
+}
