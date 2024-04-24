7d6e4c213772bf02e4cb8ec7b8df43624c73e3c2
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2024-04-24 10:23:44 +0200

    test: add new 20 test to json module

---
+++ 6_Ideation/Data/test/Json/MyClass.cs
@@ -0,0 +1,13 @@
+namespace Alis.Core.Aspect.Data.Test.Json
+{
+    /// <summary>
+    /// The my class
+    /// </summary>
+    public class MyClass
+    {
+        /// <summary>
+        /// The my field
+        /// </summary>
+        public string myField;
+    }
+}
