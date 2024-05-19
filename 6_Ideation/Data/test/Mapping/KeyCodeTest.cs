9fcbfc503340ba660bfb83e1fd23cd3f77e45d81
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2024-05-19 20:08:38 +0200

    test: Update KeyCodeTest.cs

--- 6_Ideation/Data/test/Mapping/KeyCodeTest.cs
+++ 6_Ideation/Data/test/Mapping/KeyCodeTest.cs
@@ -756,5 +756,519 @@ namespace Alis.Core.Aspect.Data.Test.Mapping
         {
             Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpPeriod | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpPeriod);
         }
+        
+        /// <summary>
+        /// Tests that kbdillumup should be correct value v 2
+        /// </summary>
+        [Fact]
+        public void Kbdillumup_ShouldBeCorrectValue_v2()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKbdillumup | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.Kbdillumup);
+        }
+        
+        /// <summary>
+        /// Tests that eject should be correct value v 2
+        /// </summary>
+        [Fact]
+        public void Eject_ShouldBeCorrectValue_v2()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeEject | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.Eject);
+        }
+        
+        /// <summary>
+        /// Tests that sleep should be correct value v 2
+        /// </summary>
+        [Fact]
+        public void Sleep_ShouldBeCorrectValue_v2()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeSleep | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.Sleep);
+        }
+        
+        /// <summary>
+        /// Tests that app 1 should be correct value v 2
+        /// </summary>
+        [Fact]
+        public void App1_ShouldBeCorrectValue_v2()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeApp1 | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.App1);
+        }
+        
+        /// <summary>
+        /// Tests that app 2 should be correct value v 2
+        /// </summary>
+        [Fact]
+        public void App2_ShouldBeCorrectValue_v2()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeApp2 | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.App2);
+        }
+        
+        /// <summary>
+        /// Tests that audiorewind should be correct value v 2
+        /// </summary>
+        [Fact]
+        public void Audiorewind_ShouldBeCorrectValue_v2()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeAudiorewind | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.Audiorewind);
+        }
+        
+        /// <summary>
+        /// Tests that audiofastforward should be correct value v 2
+        /// </summary>
+        [Fact]
+        public void Audiofastforward_ShouldBeCorrectValue_v2()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeAudiofastforward | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.Audiofastforward);
+        }
+        
+        /// <summary>
+        /// Tests that thousandsseparator should be correct value
+        /// </summary>
+        [Fact]
+        public void Thousandsseparator_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeThousandsseparator | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.Thousandsseparator);
+        }
+        
+        /// <summary>
+        /// Tests that decimalseparator should be correct value
+        /// </summary>
+        [Fact]
+        public void Decimalseparator_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeDecimalseparator | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.Decimalseparator);
+        }
+        
+        /// <summary>
+        /// Tests that currencyunit should be correct value
+        /// </summary>
+        [Fact]
+        public void Currencyunit_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeCurrencyunit | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.Currencyunit);
+        }
+        
+        /// <summary>
+        /// Tests that currencysubunit should be correct value
+        /// </summary>
+        [Fact]
+        public void Currencysubunit_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeCurrencysubunit | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.Currencysubunit);
+        }
+        
+        /// <summary>
+        /// Tests that kp leftparen should be correct value
+        /// </summary>
+        [Fact]
+        public void KpLeftparen_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpLeftparen | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpLeftparen);
+        }
+        
+        /// <summary>
+        /// Tests that kp rightparen should be correct value
+        /// </summary>
+        [Fact]
+        public void KpRightparen_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpRightparen | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpRightparen);
+        }
+        
+        /// <summary>
+        /// Tests that kp leftbrace should be correct value
+        /// </summary>
+        [Fact]
+        public void KpLeftbrace_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpLeftbrace | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpLeftbrace);
+        }
+        
+        /// <summary>
+        /// Tests that kp rightbrace should be correct value
+        /// </summary>
+        [Fact]
+        public void KpRightbrace_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpRightbrace | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpRightbrace);
+        }
+        
+        /// <summary>
+        /// Tests that kp tab should be correct value
+        /// </summary>
+        [Fact]
+        public void KpTab_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpTab | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpTab);
+        }
+        
+        /// <summary>
+        /// Tests that kp backspace should be correct value
+        /// </summary>
+        [Fact]
+        public void KpBackspace_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpBackspace | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpBackspace);
+        }
+        
+        /// <summary>
+        /// Tests that kp a should be correct value
+        /// </summary>
+        [Fact]
+        public void KpA_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpA | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpA);
+        }
+        
+        /// <summary>
+        /// Tests that kp b should be correct value
+        /// </summary>
+        [Fact]
+        public void KpB_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpB | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpB);
+        }
+        
+        /// <summary>
+        /// Tests that kp c should be correct value
+        /// </summary>
+        [Fact]
+        public void KpC_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpC | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpC);
+        }
+        
+        /// <summary>
+        /// Tests that kp d should be correct value
+        /// </summary>
+        [Fact]
+        public void KpD_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpD | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpD);
+        }
+        
+        /// <summary>
+        /// Tests that kp e should be correct value
+        /// </summary>
+        [Fact]
+        public void KpE_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpE | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpE);
+        }
+        
+        /// <summary>
+        /// Tests that kp f should be correct value
+        /// </summary>
+        [Fact]
+        public void KpF_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpF | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpF);
+        }
+        
+        /// <summary>
+        /// Tests that kp xor should be correct value
+        /// </summary>
+        [Fact]
+        public void KpXor_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpXor | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpXor);
+        }
+        
+        /// <summary>
+        /// Tests that kp power should be correct value
+        /// </summary>
+        [Fact]
+        public void KpPower_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpPower | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpPower);
+        }
+        
+        /// <summary>
+        /// Tests that kp percent should be correct value
+        /// </summary>
+        [Fact]
+        public void KpPercent_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpPercent | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpPercent);
+        }
+        
+        /// <summary>
+        /// Tests that kp less should be correct value
+        /// </summary>
+        [Fact]
+        public void KpLess_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpLess | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpLess);
+        }
+        
+        /// <summary>
+        /// Tests that kp greater should be correct value
+        /// </summary>
+        [Fact]
+        public void KpGreater_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpGreater | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpGreater);
+        }
+        
+        /// <summary>
+        /// Tests that kp ampersand should be correct value
+        /// </summary>
+        [Fact]
+        public void KpAmpersand_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpAmpersand | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpAmpersand);
+        }
+        
+        /// <summary>
+        /// Tests that kp dblampersand should be correct value
+        /// </summary>
+        [Fact]
+        public void KpDblampersand_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpDblampersand | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpDblampersand);
+        }
+        
+        /// <summary>
+        /// Tests that kp verticalbar should be correct value
+        /// </summary>
+        [Fact]
+        public void KpVerticalbar_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpVerticalbar | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpVerticalbar);
+        }
+        
+        /// <summary>
+        /// Tests that kp dblverticalbar should be correct value
+        /// </summary>
+        [Fact]
+        public void KpDblverticalbar_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpDblverticalbar | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpDblverticalbar);
+        }
+        
+        /// <summary>
+        /// Tests that kp colon should be correct value
+        /// </summary>
+        [Fact]
+        public void KpColon_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpColon | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpColon);
+        }
+        
+        /// <summary>
+        /// Tests that kp hash should be correct value
+        /// </summary>
+        [Fact]
+        public void KpHash_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpHash | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpHash);
+        }
+        
+        /// <summary>
+        /// Tests that kp space should be correct value
+        /// </summary>
+        [Fact]
+        public void KpSpace_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpSpace | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpSpace);
+        }
+        
+        /// <summary>
+        /// Tests that kp at should be correct value
+        /// </summary>
+        [Fact]
+        public void KpAt_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpAt | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpAt);
+        }
+        
+        /// <summary>
+        /// Tests that kp exclam should be correct value
+        /// </summary>
+        [Fact]
+        public void KpExclam_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpExclam | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpExclam);
+        }
+        
+        /// <summary>
+        /// Tests that kp memstore should be correct value
+        /// </summary>
+        [Fact]
+        public void KpMemstore_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpMemstore | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpMemstore);
+        }
+        
+        /// <summary>
+        /// Tests that kp memrecall should be correct value
+        /// </summary>
+        [Fact]
+        public void KpMemrecall_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpMemrecall | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpMemrecall);
+        }
+        
+        /// <summary>
+        /// Tests that kp memclear should be correct value
+        /// </summary>
+        [Fact]
+        public void KpMemclear_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpMemclear | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpMemclear);
+        }
+        
+        /// <summary>
+        /// Tests that kp memadd should be correct value
+        /// </summary>
+        [Fact]
+        public void KpMemadd_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpMemadd | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpMemadd);
+        }
+        
+        /// <summary>
+        /// Tests that kp memsubtract should be correct value
+        /// </summary>
+        [Fact]
+        public void KpMemsubtract_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpMemsubtract | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpMemsubtract);
+        }
+        
+        /// <summary>
+        /// Tests that kp memmultiply should be correct value
+        /// </summary>
+        [Fact]
+        public void KpMemmultiply_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpMemmultiply | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpMemmultiply);
+        }
+        
+        /// <summary>
+        /// Tests that kp memdivide should be correct value
+        /// </summary>
+        [Fact]
+        public void KpMemdivide_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpMemdivide | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpMemdivide);
+        }
+        
+        /// <summary>
+        /// Tests that kp plusminus should be correct value
+        /// </summary>
+        [Fact]
+        public void KpPlusminus_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpPlusminus | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpPlusminus);
+        }
+        
+        /// <summary>
+        /// Tests that kp clear should be correct value
+        /// </summary>
+        [Fact]
+        public void KpClear_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpClear | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpClear);
+        }
+        
+        /// <summary>
+        /// Tests that kp clearentry should be correct value
+        /// </summary>
+        [Fact]
+        public void KpClearentry_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpClearentry | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpClearentry);
+        }
+        
+        /// <summary>
+        /// Tests that kp binary should be correct value
+        /// </summary>
+        [Fact]
+        public void KpBinary_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpBinary | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpBinary);
+        }
+        
+        /// <summary>
+        /// Tests that kp octal should be correct value
+        /// </summary>
+        [Fact]
+        public void KpOctal_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpOctal | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpOctal);
+        }
+        
+        /// <summary>
+        /// Tests that kp decimal should be correct value
+        /// </summary>
+        [Fact]
+        public void KpDecimal_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpDecimal | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpDecimal);
+        }
+        
+        /// <summary>
+        /// Tests that kp hexadecimal should be correct value
+        /// </summary>
+        [Fact]
+        public void KpHexadecimal_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeKpHexadecimal | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.KpHexadecimal);
+        }
+        
+        /// <summary>
+        /// Tests that lctrl should be correct value
+        /// </summary>
+        [Fact]
+        public void Lctrl_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeLctrl | (SdlScancode) SdlInputConst.KScancodeMask), KeyCode.Lctrl);
+        }
+        
+        /// <summary>
+        /// Tests that cancel should be correct value
+        /// </summary>
+        [Fact]
+        public void Cancel_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeCancel | (SdlScancode)SdlInputConst.KScancodeMask), KeyCode.Cancel);
+        }
+        
+        /// <summary>
+        /// Tests that clear should be correct value
+        /// </summary>
+        [Fact]
+        public void Clear_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeClear | (SdlScancode)SdlInputConst.KScancodeMask), KeyCode.Clear);
+        }
+        
+        /// <summary>
+        /// Tests that prior should be correct value
+        /// </summary>
+        [Fact]
+        public void Prior_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodePrior | (SdlScancode)SdlInputConst.KScancodeMask), KeyCode.Prior);
+        }
+        
+        /// <summary>
+        /// Tests that return 2 should be correct value
+        /// </summary>
+        [Fact]
+        public void Return2_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeReturn2 | (SdlScancode)SdlInputConst.KScancodeMask), KeyCode.Return2);
+        }
+        
+        /// <summary>
+        /// Tests that separator should be correct value
+        /// </summary>
+        [Fact]
+        public void Separator_ShouldBeCorrectValue()
+        {
+            Assert.Equal((KeyCode) (SdlScancode.SdlScancodeSeparator | (SdlScancode)SdlInputConst.KScancodeMask), KeyCode.Separator);
+        }
+        
     }
 }
