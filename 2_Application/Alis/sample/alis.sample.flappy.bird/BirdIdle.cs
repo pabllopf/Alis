9f5d3965e519d51aeb24e3dd4c2e4590b619eac4
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2024-06-01 15:45:16 +0200

    release: fix dotnet version.

--- 2_Application/Alis/sample/alis.sample.flappy.bird/BirdIdle.cs
+++ 2_Application/Alis/sample/alis.sample.flappy.bird/BirdIdle.cs
@@ -45,6 +45,7 @@ namespace Alis.Sample.Flappy.Bird
         /// </summary>
         private const float RangeMovement = 10.0f;
         
+        
         /// <summary>
         ///     The velocity
         /// </summary>
