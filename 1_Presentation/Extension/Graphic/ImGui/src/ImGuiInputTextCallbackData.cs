b6cd2f8619a67b855c463cca5afd1ffda5fb94a1
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2024-07-06 14:07:03 +0200

    Update ImGuiInputTextCallbackData.cs

--- 1_Presentation/Extension/Graphic/ImGui/src/ImGuiInputTextCallbackData.cs
+++ 1_Presentation/Extension/Graphic/ImGui/src/ImGuiInputTextCallbackData.cs
@@ -39,61 +39,61 @@ namespace Alis.Extension.Graphic.ImGui
         /// <summary>
         ///     The event flag
         /// </summary>
-        public ImGuiInputTextFlags EventFlag;
+        public ImGuiInputTextFlags EventFlag { get; set; }
 
         /// <summary>
         ///     The flags
         /// </summary>
-        public ImGuiInputTextFlags Flags;
+        public ImGuiInputTextFlags Flags { get; set; }
 
         /// <summary>
         ///     The user data
         /// </summary>
-        public IntPtr UserData;
+        public IntPtr UserData { get; set; }
 
         /// <summary>
         ///     The event char
         /// </summary>
-        public ushort EventChar;
+        public ushort EventChar { get; set; }
 
         /// <summary>
         ///     The event key
         /// </summary>
-        public ImGuiKey EventKey;
+        public ImGuiKey EventKey { get; set; }
 
         /// <summary>
         ///     The buf
         /// </summary>
-        public IntPtr Buf;
+        public IntPtr Buf { get; set; }
 
         /// <summary>
         ///     The buf text len
         /// </summary>
-        public int BufTextLen;
+        public int BufTextLen { get; set; }
 
         /// <summary>
         ///     The buf size
         /// </summary>
-        public int BufSize;
+        public int BufSize { get; set; }
 
         /// <summary>
         ///     The buf dirty
         /// </summary>
-        public byte BufDirty;
+        public byte BufDirty { get; set; }
 
         /// <summary>
         ///     The cursor pos
         /// </summary>
-        public int CursorPos;
+        public int CursorPos { get; set; }
 
         /// <summary>
         ///     The selection start
         /// </summary>
-        public int SelectionStart;
+        public int SelectionStart { get; set; }
 
         /// <summary>
         ///     The selection end
         /// </summary>
-        public int SelectionEnd;
+        public int SelectionEnd { get; set; }
     }
 }
