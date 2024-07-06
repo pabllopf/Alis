cced3e20e298d77924e2bf446a6c7b356346940e
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2024-07-06 14:06:22 +0200

    Update ImFontGlyph.cs

--- 1_Presentation/Extension/Graphic/ImGui/src/ImFontGlyph.cs
+++ 1_Presentation/Extension/Graphic/ImGui/src/ImFontGlyph.cs
@@ -37,61 +37,61 @@ namespace Alis.Extension.Graphic.ImGui
         /// <summary>
         ///     The colored
         /// </summary>
-        public uint Colored;
+        public uint Colored {get; set;}
 
         /// <summary>
         ///     The visible
         /// </summary>
-        public uint Visible;
+        public uint Visible {get; set;}
 
         /// <summary>
         ///     The codepoint
         /// </summary>
-        public uint Codepoint;
+        public uint Codepoint {get; set;}
 
         /// <summary>
         ///     The advance
         /// </summary>
-        public float AdvanceX;
+        public float AdvanceX {get; set;}
 
         /// <summary>
         ///     The
         /// </summary>
-        public float X0;
+        public float X0 {get; set;}
 
         /// <summary>
         ///     The
         /// </summary>
-        public float Y0;
+        public float Y0 {get; set;}
 
         /// <summary>
         ///     The
         /// </summary>
-        public float X1;
+        public float X1 {get; set;}
 
         /// <summary>
         ///     The
         /// </summary>
-        public float Y1;
+        public float Y1 {get; set;}
 
         /// <summary>
         ///     The
         /// </summary>
-        public float U0;
+        public float U0 {get; set;}
 
         /// <summary>
         ///     The
         /// </summary>
-        public float V0;
+        public float V0 {get; set;}
 
         /// <summary>
         ///     The
         /// </summary>
-        public float U1;
+        public float U1 {get; set;}
 
         /// <summary>
         ///     The
         /// </summary>
-        public float V1;
+        public float V1 {get; set;}
     }
 }
