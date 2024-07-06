87e8b1d1703de995fb27d75201bd99d7245729b6
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2024-07-06 14:08:58 +0200

    Update ImGuiPlatformIO.cs

--- 1_Presentation/Extension/Graphic/ImGui/src/ImGuiPlatformIO.cs
+++ 1_Presentation/Extension/Graphic/ImGui/src/ImGuiPlatformIO.cs
@@ -39,126 +39,126 @@ namespace Alis.Extension.Graphic.ImGui
         /// <summary>
         ///     The platform createwindow
         /// </summary>
-        public IntPtr PlatformCreateWindow;
+        public IntPtr PlatformCreateWindow { get; set; }
 
         /// <summary>
         ///     The platform destroywindow
         /// </summary>
-        public IntPtr PlatformDestroyWindow;
+        public IntPtr PlatformDestroyWindow { get; set; }
 
         /// <summary>
         ///     The platform showwindow
         /// </summary>
-        public IntPtr PlatformShowWindow;
+        public IntPtr PlatformShowWindow { get; set; }
 
         /// <summary>
         ///     The platform setwindowpos
         /// </summary>
-        public IntPtr PlatformSetWindowPos;
+        public IntPtr PlatformSetWindowPos { get; set; }
 
         /// <summary>
         ///     The platform getwindowpos
         /// </summary>
-        public IntPtr PlatformGetWindowPos;
+        public IntPtr PlatformGetWindowPos { get; set; }
 
         /// <summary>
         ///     The platform setwindowsize
         /// </summary>
-        public IntPtr PlatformSetWindowSize;
+        public IntPtr PlatformSetWindowSize { get; set; }
 
         /// <summary>
         ///     The platform getwindowsize
         /// </summary>
-        public IntPtr PlatformGetWindowSize;
+        public IntPtr PlatformGetWindowSize { get; set; }
 
         /// <summary>
         ///     The platform setwindowfocus
         /// </summary>
-        public IntPtr PlatformSetWindowFocus;
+        public IntPtr PlatformSetWindowFocus { get; set; }
 
         /// <summary>
         ///     The platform getwindowfocus
         /// </summary>
-        public IntPtr PlatformGetWindowFocus;
+        public IntPtr PlatformGetWindowFocus { get; set; }
 
         /// <summary>
         ///     The platform getwindowminimized
         /// </summary>
-        public IntPtr PlatformGetWindowMinimized;
+        public IntPtr PlatformGetWindowMinimized { get; set; }
 
         /// <summary>
         ///     The platform setwindowtitle
         /// </summary>
-        public IntPtr PlatformSetWindowTitle;
+        public IntPtr PlatformSetWindowTitle { get; set; }
 
         /// <summary>
         ///     The platform setwindowalpha
         /// </summary>
-        public IntPtr PlatformSetWindowAlpha;
+        public IntPtr PlatformSetWindowAlpha { get; set; }
 
         /// <summary>
         ///     The platform updatewindow
         /// </summary>
-        public IntPtr PlatformUpdateWindow;
+        public IntPtr PlatformUpdateWindow { get; set; }
 
         /// <summary>
         ///     The platform renderwindow
         /// </summary>
-        public IntPtr PlatformRenderWindow;
+        public IntPtr PlatformRenderWindow { get; set; }
 
         /// <summary>
         ///     The platform swapbuffers
         /// </summary>
-        public IntPtr PlatformSwapBuffers;
+        public IntPtr PlatformSwapBuffers { get; set; }
 
         /// <summary>
         ///     The platform getwindowdpiscale
         /// </summary>
-        public IntPtr PlatformGetWindowDpiScale;
+        public IntPtr PlatformGetWindowDpiScale { get; set; }
 
         /// <summary>
         ///     The platform onchangedviewport
         /// </summary>
-        public IntPtr PlatformOnChangedViewport;
+        public IntPtr PlatformOnChangedViewport { get; set; }
 
         /// <summary>
         ///     The platform createvksurface
         /// </summary>
-        public IntPtr PlatformCreateVkSurface;
+        public IntPtr PlatformCreateVkSurface { get; set; }
 
         /// <summary>
         ///     The renderer createwindow
         /// </summary>
-        public IntPtr RendererCreateWindow;
+        public IntPtr RendererCreateWindow { get; set; }
 
         /// <summary>
         ///     The renderer destroywindow
         /// </summary>
-        public IntPtr RendererDestroyWindow;
+        public IntPtr RendererDestroyWindow { get; set; }
 
         /// <summary>
         ///     The renderer setwindowsize
         /// </summary>
-        public IntPtr RendererSetWindowSize;
+        public IntPtr RendererSetWindowSize { get; set; }
 
         /// <summary>
         ///     The renderer renderwindow
         /// </summary>
-        public IntPtr RendererRenderWindow;
+        public IntPtr RendererRenderWindow { get; set; }
 
         /// <summary>
         ///     The renderer swapbuffers
         /// </summary>
-        public IntPtr RendererSwapBuffers;
+        public IntPtr RendererSwapBuffers { get; set; }
 
         /// <summary>
         ///     The monitors
         /// </summary>
-        public ImVector Monitors;
+        public ImVector Monitors { get; set; }
 
         /// <summary>
         ///     The viewports
         /// </summary>
-        public ImVector Viewports;
+        public ImVector Viewports { get; set; }
     }
 }
