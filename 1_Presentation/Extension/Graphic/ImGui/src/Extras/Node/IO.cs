1956a544db1896cfa19a29ce84c20d3483906774
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2024-07-07 14:06:36 +0200

    test: pro check actual state

--- 1_Presentation/Extension/Graphic/ImGui/src/Extras/Node/IO.cs
+++ 1_Presentation/Extension/Graphic/ImGui/src/Extras/Node/IO.cs
@@ -38,7 +38,6 @@ namespace Alis.Extension.Graphic.ImGui.Extras.Node
         ///     The emulate three button mouse
         /// </summary>
         public EmulateThreeButtonMouse EmulateThreeButtonMouse { get; set; }
-
         
         /// <summary>
         ///     The link detach with modifier click
