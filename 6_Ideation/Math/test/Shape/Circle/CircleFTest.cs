0a50e674f4486400301b8a17a7dc03b921ef1b9c
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2024-07-18 20:28:58 +0200

    test: 100% covert of math module

---
+++ 6_Ideation/Math/test/Shape/Circle/CircleFTest.cs
@@ -0,0 +1,72 @@
+// --------------------------------------------------------------------------
+// 
+//                               █▀▀█ ░█─── ▀█▀ ░█▀▀▀█
+//                              ░█▄▄█ ░█─── ░█─ ─▀▀▀▄▄
+//                              ░█─░█ ░█▄▄█ ▄█▄ ░█▄▄▄█
+// 
+//  --------------------------------------------------------------------------
+//  File:CircleFTest.cs
+// 
+//  Author:Pablo Perdomo Falcón
+//  Web:https://www.pabllopf.dev/
+// 
+//  Copyright (c) 2021 GNU General Public License v3.0
+// 
+//  This program is free software:you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.If not, see <http://www.gnu.org/licenses/>.
+// 
+//  --------------------------------------------------------------------------
+
+using Alis.Core.Aspect.Math.Shape.Circle;
+using Xunit;
+
+namespace Alis.Core.Aspect.Math.Test.Shape.Circle
+{
+    /// <summary>
+    /// The circle test class
+    /// </summary>
+    public class CircleFTest
+    {
+        /// <summary>
+        /// Tests that constructor initializes properties correctly
+        /// </summary>
+        [Fact]
+        public void Constructor_InitializesPropertiesCorrectly()
+        {
+            CircleF circle = new CircleF {X = 1.0f, Y = 2.0f, R = 3.0f};
+
+            Assert.Equal(1.0f, circle.X);
+            Assert.Equal(2.0f, circle.Y);
+            Assert.Equal(3.0f, circle.R);
+        }
+
+        /// <summary>
+        /// Tests that properties set values correctly
+        /// </summary>
+        /// <param name="x">The </param>
+        /// <param name="y">The </param>
+        /// <param name="r">The </param>
+        [Theory]
+        [InlineData(0f, 0f, 0f)]
+        [InlineData(-1f, -1f, 1f)]
+        [InlineData(float.MaxValue, float.MaxValue, float.MaxValue)]
+        public void Properties_SetValuesCorrectly(float x, float y, float r)
+        {
+            CircleF circle = new CircleF {X = x, Y = y, R = r};
+
+            Assert.Equal(x, circle.X);
+            Assert.Equal(y, circle.Y);
+            Assert.Equal(r, circle.R);
+        }
+    }
+}
---
+++ 6_Ideation/Math/test/Shape/Line/LineITest.cs
@@ -0,0 +1,75 @@
+// --------------------------------------------------------------------------
+// 
+//                               █▀▀█ ░█─── ▀█▀ ░█▀▀▀█
+//                              ░█▄▄█ ░█─── ░█─ ─▀▀▀▄▄
+//                              ░█─░█ ░█▄▄█ ▄█▄ ░█▄▄▄█
+// 
+//  --------------------------------------------------------------------------
+//  File:LineITest.cs
+// 
+//  Author:Pablo Perdomo Falcón
+//  Web:https://www.pabllopf.dev/
+// 
+//  Copyright (c) 2021 GNU General Public License v3.0
+// 
+//  This program is free software:you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.If not, see <http://www.gnu.org/licenses/>.
+// 
+//  --------------------------------------------------------------------------
+
+using Alis.Core.Aspect.Math.Shape.Line;
+using Xunit;
+
+namespace Alis.Core.Aspect.Math.Test.Shape.Line
+{
+    /// <summary>
+    /// The line test class
+    /// </summary>
+    public class LineITest
+    {
+        /// <summary>
+        /// Tests that constructor initializes properties correctly
+        /// </summary>
+        [Fact]
+        public void Constructor_InitializesPropertiesCorrectly()
+        {
+            LineI line = new LineI {X1 = 1, Y1 = 2, X2 = 3, Y2 = 4};
+
+            Assert.Equal(1, line.X1);
+            Assert.Equal(2, line.Y1);
+            Assert.Equal(3, line.X2);
+            Assert.Equal(4, line.Y2);
+        }
+
+        /// <summary>
+        /// Tests that properties set values correctly
+        /// </summary>
+        /// <param name="x1">The </param>
+        /// <param name="y1">The </param>
+        /// <param name="x2">The </param>
+        /// <param name="y2">The </param>
+        [Theory]
+        [InlineData(0, 0, 0, 0)]
+        [InlineData(-1, -2, -3, -4)]
+        [InlineData(int.MaxValue, int.MaxValue, int.MinValue, int.MinValue)]
+        public void Properties_SetValuesCorrectly(int x1, int y1, int x2, int y2)
+        {
+            LineI line = new LineI {X1 = x1, Y1 = y1, X2 = x2, Y2 = y2};
+
+            Assert.Equal(x1, line.X1);
+            Assert.Equal(y1, line.Y1);
+            Assert.Equal(x2, line.X2);
+            Assert.Equal(y2, line.Y2);
+        }
+    }
+}
