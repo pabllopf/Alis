f2fa3e1137adfe7e7c44daa5776958c89e5da333
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2024-08-31 19:41:42 +0200

    fix: math calculations

--- 2_Application/Alis/src/Core/Ecs/VideoGame.cs
+++ 2_Application/Alis/src/Core/Ecs/VideoGame.cs
@@ -146,6 +146,12 @@ namespace Alis.Core.Ecs
         [JsonPropertyName("_Context_")]
         public Context Context { get; set; }
         
+        
+        /// <summary>
+        /// The target frame duration
+        /// </summary>
+        private const double TargetFrameDuration = 1.0 / 240.0;
+
         /// <summary>
         ///     Run program
         /// </summary>
@@ -154,107 +160,115 @@ namespace Alis.Core.Ecs
             OnInit();
             OnAwake();
             OnStart();
-            
+
             currentTime = Context.TimeManager.Clock.Elapsed.TotalSeconds;
             accumulator = 0;
-            
+
             // Variables for calculating FPS
             lastTime = Context.TimeManager.Clock.Elapsed.TotalSeconds;
             Context.TimeManager.FrameCount = 0;
             Context.TimeManager.TotalFrames = 0;
             Context.TimeManager.AverageFrames = 0;
-            
+
             // Variables for calculating average FPS
             totalTime = 0;
-            
+
             // Variables for SmoothDeltaTime
             lastDeltaTime = 0f;
             smoothDeltaTimeSum = 0f;
             smoothDeltaTimeCount = 0;
-            
+
             // Variable for log output
             lastLogTime = Context.TimeManager.Clock.Elapsed.TotalSeconds;
-            
-            
+
             while (Context.TimeManager.IsRunning)
             {
-                double newTime = Context.TimeManager.Clock.Elapsed.TotalSeconds;
+                double frameStartTime = Context.TimeManager.Clock.Elapsed.TotalSeconds;
+                double newTime = frameStartTime;
                 Context.TimeManager.DeltaTime = (float) (newTime - currentTime);
-                
+
                 // Update Context.TimeManager properties
                 Context.TimeManager.UnscaledDeltaTime = (float) (newTime - currentTime);
                 Context.TimeManager.UnscaledTime += Context.TimeManager.UnscaledDeltaTime;
                 Context.TimeManager.UnscaledTimeAsDouble += Context.TimeManager.UnscaledDeltaTime;
                 Context.TimeManager.Time = Context.TimeManager.UnscaledTime * Context.TimeManager.TimeScale;
                 Context.TimeManager.TimeAsDouble = Context.TimeManager.UnscaledTimeAsDouble * Context.TimeManager.TimeScale;
-                
+
                 // Update MaximumDeltaTime
                 Context.TimeManager.MaximumDeltaTime = Math.Max(Context.TimeManager.MaximumDeltaTime, Context.TimeManager.DeltaTime);
-                
+
                 currentTime = newTime;
                 accumulator += Context.TimeManager.DeltaTime;
-                
+
                 // Increment frame counter
                 Context.TimeManager.FrameCount++;
                 Context.TimeManager.TotalFrames++;
-                
+
                 // If a second has passed since the last FPS calculation
                 if (newTime - lastTime >= 1.0)
                 {
                     // Calculate average FPS
                     totalTime += newTime - lastTime;
                     Context.TimeManager.AverageFrames = (int) (Context.TimeManager.TotalFrames / totalTime);
-                    
+
                     // Reset frame counter and update last time
                     Context.TimeManager.FrameCount = 0;
                     lastTime = newTime;
                 }
-                
+
                 OnDispatchEvents();
                 OnBeforeUpdate();
                 OnUpdate();
                 OnAfterUpdate();
-                
+
                 // Run fixed methods
                 while (accumulator >= Context.TimeManager.Configuration.FixedTimeStep)
                 {
                     Context.TimeManager.InFixedTimeStep = true;
-                    
+
                     Context.TimeManager.FixedTime += Context.TimeManager.Configuration.FixedTimeStep;
                     Context.TimeManager.FixedTimeAsDouble += Context.TimeManager.Configuration.FixedTimeStep;
                     Context.TimeManager.FixedDeltaTime = Context.TimeManager.Configuration.FixedTimeStep;
                     Context.TimeManager.FixedUnscaledDeltaTime = Context.TimeManager.Configuration.FixedTimeStep / Context.TimeManager.TimeScale;
-                    
+
                     // Update FixedUnscaledTime and FixedUnscaledTimeAsDouble
                     Context.TimeManager.FixedUnscaledTime += Context.TimeManager.FixedUnscaledDeltaTime;
                     Context.TimeManager.FixedUnscaledTimeAsDouble += Context.TimeManager.FixedUnscaledDeltaTime;
-                    
+
                     OnBeforeFixedUpdate();
                     OnFixedUpdate();
                     OnAfterFixedUpdate();
-                    
-                    accumulator -= Context.TimeManager.Configuration.FixedTimeStep;
-                    
+
+                    accumulator %= Context.TimeManager.Configuration.FixedTimeStep;
+
                     Context.TimeManager.InFixedTimeStep = false;
                 }
-                
+
                 OnCalculate();
                 OnDraw();
                 OnGui();
-                
+
                 // Update SmoothDeltaTime
                 smoothDeltaTimeSum += Context.TimeManager.DeltaTime - lastDeltaTime;
                 smoothDeltaTimeCount++;
                 Context.TimeManager.SmoothDeltaTime = smoothDeltaTimeSum / smoothDeltaTimeCount;
                 lastDeltaTime = Context.TimeManager.DeltaTime;
-                
+
                 lastLogTime = LastLogTime(newTime, lastLogTime);
+
+                // Calculate frame duration and sleep if necessary
+                double frameEndTime = Context.TimeManager.Clock.Elapsed.TotalSeconds;
+                double frameDuration = frameEndTime - frameStartTime;
+                if (frameDuration < TargetFrameDuration)
+                {
+                    Thread.Sleep((int) ((TargetFrameDuration - frameDuration) * 1000));
+                }
             }
-            
+
             OnStop();
             OnExit();
         }
-        
+
         /// <summary>
         ///     Exits this instance
         /// </summary>
