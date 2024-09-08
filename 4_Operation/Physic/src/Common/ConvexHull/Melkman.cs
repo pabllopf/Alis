70bbe1614363f7d386bcac7039e4fea6e6ea078f
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2024-09-08 11:53:23 +0200

    delete coments

--- 4_Operation/Physic/src/Common/ConvexHull/Melkman.cs
+++ 4_Operation/Physic/src/Common/ConvexHull/Melkman.cs
@@ -27,10 +27,7 @@
 // 
 //  --------------------------------------------------------------------------
 
-/* Original source Farseer Physics Engine:
- * Copyright (c) 2014 Ian Qvist, http://farseerphysics.codeplex.com
- * Microsoft Permissive License (Ms-PL) v1.1
- */
+
 
 using Alis.Core.Aspect.Math.Vector;
 
