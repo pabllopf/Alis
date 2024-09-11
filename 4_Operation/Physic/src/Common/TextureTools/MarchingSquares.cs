5e6e46ed2b6dfe57415b1c6130aefc67b5e15d19
Perdomo Falcon, Pablo <pabloperdomofalcon@gmail.com>
2024-09-11 13:04:03 +0200

    Update MarchingSquares.cs

--- 4_Operation/Physic/src/Common/TextureTools/MarchingSquares.cs
+++ 4_Operation/Physic/src/Common/TextureTools/MarchingSquares.cs
@@ -34,33 +34,6 @@ using Alis.Core.Physic.Collision;
 
 namespace Alis.Core.Physic.Common.TextureTools
 {
-    // Ported by Matthew Bettcher - Feb 2011
-
-    /*
-    Copyright (c) 2010, Luca Deltodesco
-    All rights reserved.
-
-    Redistribution and use in source and binary forms, with or without modification, are permitted
-    provided that the following conditions are met:
-
-        * Redistributions of source code must retain the above copyright notice, this list of conditions
-          and the following disclaimer.
-        * Redistributions in binary form must reproduce the above copyright notice, this list of
-          conditions and the following disclaimer in the documentation and/or other materials provided
-          with the distribution.
-        * Neither the name of the nape project nor the names of its contributors may be used to endorse
-         or promote products derived from this software without specific prior written permission.
-
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
-    IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-    FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
-    CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
-    IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
-    OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-    */
-
     /// <summary>
     ///     The marching squares class
     /// </summary>
@@ -605,7 +578,7 @@ namespace Alis.Core.Physic.Common.TextureTools
 
                 if (head != null)
                 {
-                    if (value != null)
+                    if (!EqualityComparer<T>.Default.Equals(value, default(T)))
                     {
                         do
                         {
@@ -746,7 +719,7 @@ namespace Alis.Core.Physic.Common.TextureTools
                 EqualityComparer<T> comparer = EqualityComparer<T>.Default;
                 if (head != null)
                 {
-                    if (value != null)
+                    if (!EqualityComparer<T>.Default.Equals(value, default(T)))
                     {
                         do
                         {
@@ -762,7 +735,7 @@ namespace Alis.Core.Physic.Common.TextureTools
                     {
                         do
                         {
-                            if (head._elt == null)
+                            if (EqualityComparer<T>.Default.Equals(head._elt, default(T)))
                             {
                                 return head;
                             }
