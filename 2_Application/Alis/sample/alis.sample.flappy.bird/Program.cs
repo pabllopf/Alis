40c326258f842f8646def9845892d42dadba78f6
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2024-11-21 13:35:29 +0000

    feature: add menu bar

--- 2_Application/Alis/sample/alis.sample.flappy.bird/Program.cs
+++ 2_Application/Alis/sample/alis.sample.flappy.bird/Program.cs
@@ -73,7 +73,7 @@ namespace Alis.Sample.Flappy.Bird
                             .Build())
                         .Build())
                     .Physic(physic => physic
-                        .Debug(true)
+                        .Debug(false)
                         .DebugColor(Color.Red)
                         .Gravity(0.0f, -4.5f)
                         .Build())
