955fc2a78eac94d4c8eb9910567291fdf7502099
Raúl Lozano Ponce <raul.lozano.ponce@hotmail.com>
2024-12-31 00:00:00 +0000

    feature: add new module to create file dialog cross-platforms

---
+++ 1_Presentation/Extension/Io/FileDialog/src/Native/nfdresult_t.cs
@@ -0,0 +1,21 @@
+namespace NativeFileDialogSharp.Native
+{
+    /// <summary>
+    /// The nfdresult enum
+    /// </summary>
+    public enum nfdresult_t
+    {
+        /// <summary>
+        /// The nfd error nfdresult
+        /// </summary>
+        NFD_ERROR,
+        /// <summary>
+        /// The nfd okay nfdresult
+        /// </summary>
+        NFD_OKAY,
+        /// <summary>
+        /// The nfd cancel nfdresult
+        /// </summary>
+        NFD_CANCEL
+    }
+}
