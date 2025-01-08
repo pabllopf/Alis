9dab061a282181411aa0292c58688a1caa303c23
Claudia2000pf <claper1992@gmail.com>
2025-01-08 00:00:00 +0000

    fix: more than 300 code smellss

--- 1_Presentation/Extension/Multimedia/FFmpeg/src/FFMpegWrapper.cs
+++ 1_Presentation/Extension/Multimedia/FFmpeg/src/FFMpegWrapper.cs
@@ -41,68 +41,6 @@ namespace Alis.Extension.Multimedia.FFmpeg
     public static class FfMpegWrapper
     {
         /// <summary>
-        ///     The muxing support enum
-        /// </summary>
-        public enum MuxingSupport
-        {
-            /// <summary>
-            ///     The mux demux muxing support
-            /// </summary>
-            MuxDemux = 0,
-
-            /// <summary>
-            ///     The mux muxing support
-            /// </summary>
-            Mux = 1,
-
-            /// <summary>
-            ///     The demux muxing support
-            /// </summary>
-            Demux = 2
-        }
-
-        /// <summary>
-        ///     The verbosity enum
-        /// </summary>
-        public enum Verbosity
-        {
-            /// <summary>
-            ///     Show nothing at all; be silent.
-            /// </summary>
-            Quiet = 0,
-
-            /// <summary>
-            ///     Show informative messages during processing. This is in addition to warnings and errors. This is the default value.
-            /// </summary>
-            Info = 1,
-
-            /// <summary>
-            ///     Same as info, except more verbose.
-            /// </summary>
-            Verbose = 2,
-
-            /// <summary>
-            ///     Show everything, including debugging information.
-            /// </summary>
-            Debug = 3,
-
-            /// <summary>
-            ///     Show all warnings and errors. Any message related to possibly incorrect or unexpected events will be shown.
-            /// </summary>
-            Warning = 4,
-
-            /// <summary>
-            ///     Show all errors, including ones which can be recovered from.
-            /// </summary>
-            Error = 5,
-
-            /// <summary>
-            ///     Only show fatal errors. These are errors after which the process absolutely cannot continue.
-            /// </summary>
-            Fatal = 6
-        }
-
-        /// <summary>
         ///     The regex
         /// </summary>
         private static readonly Regex CodecRegex = new Regex(@"(?<type>[VAS\.])[F\.][S\.][X\.][B\.][D\.] (?<codec>[a-zA-Z0-9_-]+)\W+(?<description>.*)\n?", RegexOptions.Compiled, TimeSpan.FromSeconds(10));
