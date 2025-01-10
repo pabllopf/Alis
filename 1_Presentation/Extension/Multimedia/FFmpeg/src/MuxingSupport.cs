4f3071a103350d8fb6e8e43765dc15caacf4df7c
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2025-01-10 17:54:36 +0000

    fix: more than 300 code smellss

---
+++ 1_Presentation/Extension/Multimedia/FFmpeg/src/MuxingSupport.cs
@@ -0,0 +1,23 @@
+namespace Alis.Extension.Multimedia.FFmpeg
+{
+    /// <summary>
+    ///     The muxing support enum
+    /// </summary>
+    public enum MuxingSupport
+    {
+        /// <summary>
+        ///     The mux demux muxing support
+        /// </summary>
+        MuxDemux = 0,
+
+        /// <summary>
+        ///     The mux muxing support
+        /// </summary>
+        Mux = 1,
+
+        /// <summary>
+        ///     The demux muxing support
+        /// </summary>
+        Demux = 2
+    }
+}
