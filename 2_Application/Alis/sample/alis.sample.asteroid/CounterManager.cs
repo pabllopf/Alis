4e5a97fe75b0057df3df352d5db048ad46685973
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2025-01-29 00:00:00 +0000

    feature: add counter points and timer

---
+++ 2_Application/Alis/sample/alis.sample.asteroid/CounterManager.cs
@@ -0,0 +1,76 @@
+﻿// --------------------------------------------------------------------------
+// 
+//                               █▀▀█ ░█─── ▀█▀ ░█▀▀▀█
+//                              ░█▄▄█ ░█─── ░█─ ─▀▀▀▄▄
+//                              ░█─░█ ░█▄▄█ ▄█▄ ░█▄▄▄█
+// 
+//  --------------------------------------------------------------------------
+//  File:CounterManager.cs
+// 
+//  Author:Pablo Perdomo Falcón
+//  Web:https://www.pabllopf.dev/
+// 
+//  Copyright (c) 2021 GNU General Public License v3.0
+// 
+//  This program is free software:you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.If not, see <http://www.gnu.org/licenses/>.
+// 
+//  --------------------------------------------------------------------------
+
+using Alis.Core.Aspect.Data.Resource;
+using Alis.Core.Aspect.Math.Definition;
+using Alis.Core.Ecs.Component;
+using Alis.Core.Graphic.Fonts;
+
+namespace Alis.Sample.Asteroid
+{
+    public class CounterManager : AComponent
+    {
+        public int counter = 0;
+        public FontManager fontManager;
+
+        public override void OnStart()
+        {
+            fontManager = Context.GraphicManager.FontManager;
+            fontManager.LoadFont("MONO", 16, AssetManager.Find("mono.bmp"));
+        }
+        
+        public override void OnUpdate()
+        {
+        }
+        
+        /// <summary>
+        ///     Ons the gui
+        /// </summary>
+        public override void OnGui()
+        {
+            fontManager.RenderText("MONO", $"{counter}", 100, 0, Color.White, 32);
+        }
+        
+        /// <summary>
+        ///     Increments this instance
+        /// </summary>
+        public void Increment()
+        {
+            counter += 10;
+        }
+
+        /// <summary>
+        ///     Resets this instance
+        /// </summary>
+        public void Reset()
+        {
+            counter = 0;
+        }
+    }
+}
---
+++ 2_Application/Alis/sample/alis.sample.asteroid/TimerController.cs
@@ -0,0 +1,36 @@
+﻿using Alis.Core.Aspect.Data.Resource;
+        using Alis.Core.Aspect.Math.Definition;
+        using Alis.Core.Ecs.Component;
+        using Alis.Core.Graphic.Fonts;
+        
+        namespace Alis.Sample.Asteroid
+        {
+            public class TimerController : AComponent
+            {
+                private FontManager fontManager;
+                private double lastUpdateTime;
+                    
+                public override void OnStart()
+                {
+                    fontManager = Context.GraphicManager.FontManager;
+                    fontManager.LoadFont("MONO", 14, AssetManager.Find("mono.bmp"));
+                    lastUpdateTime = Context.TimeManager.RealtimeSinceStartupAsDouble;
+                }
+                    
+                public override void OnUpdate()
+                {
+                    double currentTime = GameObject.Context.TimeManager.RealtimeSinceStartupAsDouble;
+                    if (currentTime - lastUpdateTime >= 1.0)
+                    {
+                        lastUpdateTime = currentTime;
+                        OnGui();
+                    }
+                }
+                    
+                public override void OnGui()
+                {
+                    float time = (float)(Context.TimeManager.RealtimeSinceStartupAsDouble / 1000.0);
+                    fontManager.RenderText("MONO", $"{time:F2}", 512, 0, Color.White, 32);
+                }
+            }
+        }
