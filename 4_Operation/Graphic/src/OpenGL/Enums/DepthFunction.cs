863b8bc8fa7a8460d02373126dbf22a00338bf05
Ricardo Villarta <rvr.99@hotmail.com>
2025-02-28 00:00:00 +0000

    feature: add GlDepthFunc

---
+++ 4_Operation/Graphic/src/OpenGL/Enums/DepthFunction.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------
+// 
+//                               █▀▀█ ░█─── ▀█▀ ░█▀▀▀█
+//                              ░█▄▄█ ░█─── ░█─ ─▀▀▀▄▄
+//                              ░█─░█ ░█▄▄█ ▄█▄ ░█▄▄▄█
+// 
+//  --------------------------------------------------------------------------
+//  File:DepthFunction.cs
+// 
+//  Author:Pablo Perdomo Falcón
+//  Web:https://www.pabllopf.dev/
+// 
+//  Copyright (c) 2021 GNU General Public License v3.0
+// 
+//  This program is free software:you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.If not, see <http://www.gnu.org/licenses/>.
+// 
+//  --------------------------------------------------------------------------
+
+namespace Alis.Core.Graphic.OpenGL.Enums
+{
+    public enum DepthFunction
+    {
+        Never = 0x0200,
+        Less = 0x0201,
+        Equal = 0x0202,
+        LEqual = 0x0203,
+        Greater = 0x0204,
+        NotEqual = 0x0205,
+        GEqual = 0x0206,
+        Always = 0x0207
+    }
+}
