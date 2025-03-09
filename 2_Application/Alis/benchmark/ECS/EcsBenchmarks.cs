810c24a65f32f36e795c0a131c25f904b37c7a03
GabrielRT01 <gabrirot12@gmail.com>
2025-03-09 00:00:00 +0000

    fix: add image of benchmark

--- 2_Application/Alis/benchmark/ECS/EcsBenchmarks.cs
+++ 2_Application/Alis/benchmark/ECS/EcsBenchmarks.cs
@@ -21,7 +21,7 @@ namespace Alis.Benchmark.ECS
         /// <summary>
         /// The 
         /// </summary>
-        [Params(10, 100, 1000)]
+        [Params(10)]
         public int N;
 
         /// <summary>
