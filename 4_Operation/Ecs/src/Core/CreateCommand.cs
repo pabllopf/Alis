3576f0e4ed542d54eed328688d2451ff1edeecad
Perdomo Falcon, Pablo <pabloperdomofalcon@gmail.com>
2025-03-26 09:46:21 +0100

    fix: optimice the creacte comand and deletecomand

--- 4_Operation/Ecs/src/Core/CreateCommand.cs
+++ 4_Operation/Ecs/src/Core/CreateCommand.cs
@@ -1,7 +1,9 @@
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace Alis.Core.Ecs.Core
 {
-    [StructLayout( LayoutKind.Auto )]
-    internal record struct CreateCommand(EntityIDOnly Entity, int BufferIndex, int BufferLength);
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    [SkipLocalsInit]
+    internal readonly record struct CreateCommand(EntityIDOnly Entity, int BufferIndex, int BufferLength);
 }
