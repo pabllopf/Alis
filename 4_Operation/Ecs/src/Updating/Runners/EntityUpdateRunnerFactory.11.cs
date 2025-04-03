69d28bdf39cfb9ee9faf11439fb6ece5f2e7f609
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2025-04-04 00:00:00 +0100

    fix: namespace Alis.Core.Ecs.Updating.Runners

---
+++ 4_Operation/Ecs/src/Updating/Runners/EntityUpdateRunnerFactory.11.cs
@@ -0,0 +1,13 @@
+﻿using Alis.Core.Ecs.Collections;
+using Alis.Core.Ecs.Operations;
+
+namespace Alis.Core.Ecs.Updating.Runners
+{
+    public class EntityUpdateRunnerFactory<TComp, TArg1, TArg2, TArg3, TArg4, TArg5, TArg6, TArg7, TArg8, TArg9, TArg10, TArg11> : IComponentStorageBaseFactory, IComponentStorageBaseFactory<TComp>
+        where TComp : IEntityComponent<TArg1, TArg2, TArg3, TArg4, TArg5, TArg6, TArg7, TArg8, TArg9, TArg10, TArg11>
+    {
+        ComponentStorageBase IComponentStorageBaseFactory.Create(int capacity) => new EntityUpdate<TComp, TArg1, TArg2, TArg3, TArg4, TArg5, TArg6, TArg7, TArg8, TArg9, TArg10, TArg11>(capacity);
+        IdTable IComponentStorageBaseFactory.CreateStack() => new IdTable<TComp>();
+        ComponentStorage<TComp> IComponentStorageBaseFactory<TComp>.CreateStronglyTyped(int capacity) => new EntityUpdate<TComp, TArg1, TArg2, TArg3, TArg4, TArg5, TArg6, TArg7, TArg8, TArg9, TArg10, TArg11>(capacity);
+    }
+}
