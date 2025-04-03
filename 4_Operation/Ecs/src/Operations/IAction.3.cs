229b74e2c4f920c6e67dde540d8c5948dc7eac4e
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2025-04-04 00:00:00 +0100

    style: add iactions

---
+++ 4_Operation/Ecs/src/Operations/IAction.3.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace Alis.Core.Ecs.Operations
+{
+    public interface IAction<TArg1, TArg2, TArg3>
+    {
+        void Run(ref TArg1 arg1, ref TArg2 arg2, ref TArg3 arg3);
+    }
+}
---
+++ 4_Operation/Ecs/src/Operations/IAction.4.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Alis.Core.Ecs.Operations
+{
+    public interface IAction<TArg1, TArg2, TArg3, TArg4>
+    {
+        
+        void Run(ref TArg1 arg1, ref TArg2 arg2, ref TArg3 arg3, ref TArg4 arg4);
+    }
+}
