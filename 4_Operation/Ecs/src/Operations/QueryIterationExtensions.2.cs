be13fe055ccea243d160844762bb2bb464c70694
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2025-04-06 17:35:01 +0200

    fix: public static void Delegate

---
+++ 4_Operation/Ecs/src/Operations/QueryIterationExtensions.2.cs
@@ -0,0 +1,61 @@
+﻿
+
+using System.Runtime.CompilerServices;
+
+namespace Alis.Core.Ecs.Operations
+{
+    public static partial class QueryIterationExtensions
+    {
+        /// <summary>
+        /// Executes a delegate for every entity in a query, using the specified component types.
+        /// </summary>
+        /// <param name="query">The query to iterate over.</param>
+        /// <param name="action">The behavior to execute on every component set.</param>
+        public static void Delegate<T1, T2>(this Query query, QueryDelegates.Query<T1, T2> action)
+        {
+            foreach (var archetype in query.AsSpan())
+            {
+                //use ref instead of span to avoid extra locals
+                ref T1 c1 = ref archetype.GetComponentDataReference<T1>();
+                ref T2 c2 = ref archetype.GetComponentDataReference<T2>();
+
+
+                //downcounting is faster
+                for (nint i = archetype.EntityCount - 1; i >= 0; i--)
+                {
+                    action(ref c1, ref c2);
+
+                    c1 = ref Unsafe.Add(ref c1, 1);
+                    c2 = ref Unsafe.Add(ref c2, 1);
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// Executes a inlinable struct instance method for every entity in a query, using the specified component types.
+        /// </summary>
+        /// <param name="query">The query to iterate over.</param>
+        /// <param name="action">The struct behavior to execute on every component set.</param>
+        public static void Inline<TAction, T1, T2>(this Query query, TAction action)
+            where TAction : IAction<T1, T2>
+        {
+            foreach (var archetype in query.AsSpan())
+            {
+                //use ref instead of span to avoid extra locals
+                ref T1 c1 = ref archetype.GetComponentDataReference<T1>();
+                ref T2 c2 = ref archetype.GetComponentDataReference<T2>();
+
+
+                for (nint i = archetype.EntityCount - 1; i >= 0; i--)
+                {
+                    action.Run(ref c1, ref c2);
+
+                    c1 = ref Unsafe.Add(ref c1, 1);
+                    c2 = ref Unsafe.Add(ref c2, 1);
+
+                }
+            }
+        }
+    }
+}
