7444d3f8d2aa7048f9b5560816d21c0375e87ffb
Ricardo Villarta <rvr.99@hotmail.com>
2025-06-08 00:00:00 +0100

    refactor names and delete _

--- 4_Operation/Graphic/src/Stb/StbImage.cs
+++ 4_Operation/Graphic/src/Stb/StbImage.cs
@@ -1,83 +1,132 @@
+// --------------------------------------------------------------------------
+// 
+//                               █▀▀█ ░█─── ▀█▀ ░█▀▀▀█
+//                              ░█▄▄█ ░█─── ░█─ ─▀▀▀▄▄
+//                              ░█─░█ ░█▄▄█ ▄█▄ ░█▄▄▄█
+// 
+//  --------------------------------------------------------------------------
+//  File:StbImage.cs
+// 
+//  Author:Pablo Perdomo Falcón
+//  Web:https://www.pabllopf.dev/
+// 
+//  Copyright (c) 2021 GNU General Public License v3.0
+// 
+//  This program is free software:you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.If not, see <http://www.gnu.org/licenses/>.
+// 
+//  --------------------------------------------------------------------------
+
 using System;
-        using System.IO;
-        using System.Runtime.InteropServices;
-        using Alis.Core.Graphic.Stb.Hebron.Runtime;
-        
-        namespace Alis.Core.Graphic.Stb
+using System.IO;
+using System.Runtime.InteropServices;
+using Alis.Core.Graphic.Stb.Hebron.Runtime;
+
+namespace Alis.Core.Graphic.Stb
+{
+    /// <summary>
+    ///     The stb image class
+    /// </summary>
+    public
+        static unsafe partial class StbImage
+    {
+        /// <summary>
+        ///     The stbi failure reason
+        /// </summary>
+        public static string Stbigfailurereason;
+
+        /// <summary>
+        ///     The stbi parse png file invalid chunk
+        /// </summary>
+        public static readonly char[] Stbiparsepngfileinvalidchunk = new char[25];
+
+        /// <summary>
+        ///     Gets the value of the native allocations
+        /// </summary>
+        public static int NativeAllocations => MemoryStats.Allocations;
+
+        /// <summary>
+        ///     Stbis the err using the specified str
+        /// </summary>
+        /// <param name="str">The str</param>
+        /// <returns>The int</returns>
+        private static int Stbierr(string str)
         {
-            /// <summary>
-            ///     The stb image class
-            /// </summary>
-            public static partial class StbImage
-            {
-                public static string StbiGFailureReason;
-        
-                public static readonly char[] StbiParsePngFileInvalidChunk = new char[25];
-        
-                public static int NativeAllocations => MemoryStats.Allocations;
-        
-                private static int stbi__err(string str)
-                {
-                    StbiGFailureReason = str;
-                    return 0;
-                }
-        
-                public static byte stbi__get8(StbiContext s)
-                {
-                    int b = s.Stream.ReadByte();
-                    if (b == -1)
-                    {
-                        return 0;
-                    }
-        
-                    return (byte)b;
-                }
-        
-                public static void stbi__skip(StbiContext s, int skip)
-                {
-                    s.Stream.Seek(skip, SeekOrigin.Current);
-                }
-        
-                public static void Stbirewind(StbiContext s)
-                {
-                    s.Stream.Seek(0, SeekOrigin.Begin);
-                }
-        
-                public static int stbi__at_eof(StbiContext s) => s.Stream.Position == s.Stream.Length ? 1 : 0;
-        
-                public static int stbi__getn(StbiContext s, IntPtr buf, int size)
-                {
-                    if (s.TempBuffer == null || s.TempBuffer.Length < size)
-                    {
-                        s.TempBuffer = new byte[size * 2];
-                    }
-        
-                    int result = s.Stream.Read(s.TempBuffer, 0, size);
-                    Marshal.Copy(s.TempBuffer, 0, buf, result);
-        
-                    return result;
-                }
-        
-                public class StbiContext
-                {
-                    private readonly Stream _stream;
-        
-                    public byte[] TempBuffer;
-        
-                    public int ImgN = 0;
-        
-                    public int ImgOutN = 0;
-        
-                    public uint ImgX = 0;
-        
-                    public uint ImgY = 0;
-        
-                    public StbiContext(Stream stream)
-                    {
-                        _stream = stream ?? throw new ArgumentNullException(nameof(stream));
-                    }
-        
-                    public Stream Stream => _stream;
-                }
-            }
+            Stbigfailurereason = str;
+            return 0;
         }
+
+        /// <summary>
+        ///     Stbis the get 8 using the specified s
+        /// </summary>
+        /// <param name="s">The </param>
+        /// <returns>The byte</returns>
+        public static byte Stbiget8(Stbicontext s)
+        {
+            int b = s.Stream.ReadByte();
+            if (b == -1)
+            {
+                return 0;
+            }
+
+            return (byte) b;
+        }
+
+        /// <summary>
+        ///     Stbis the skip using the specified s
+        /// </summary>
+        /// <param name="s">The </param>
+        /// <param name="skip">The skip</param>
+        public static void Stbiskip(Stbicontext s, int skip)
+        {
+            s.Stream.Seek(skip, SeekOrigin.Current);
+        }
+
+        /// <summary>
+        ///     Stbis the rewind using the specified s
+        /// </summary>
+        /// <param name="s">The </param>
+        public static void Stbirewind(Stbicontext s)
+        {
+            s.Stream.Seek(0, SeekOrigin.Begin);
+        }
+
+        /// <summary>
+        ///     Stbis the at eof using the specified s
+        /// </summary>
+        /// <param name="s">The </param>
+        /// <returns>The int</returns>
+        public static int Stbiateof(Stbicontext s) => s.Stream.Position == s.Stream.Length ? 1 : 0;
+
+        /// <summary>
+        ///     Stbis the getn using the specified s
+        /// </summary>
+        /// <param name="s">The </param>
+        /// <param name="buf">The buf</param>
+        /// <param name="size">The size</param>
+        /// <returns>The result</returns>
+        public static int Stbigetn(Stbicontext s, byte* buf, int size)
+        {
+            if (s.TempBuffer == null ||
+                s.TempBuffer.Length < size)
+            {
+                s.TempBuffer = new byte[size * 2];
+            }
+
+            int result = s.Stream.Read(s.TempBuffer, 0, size);
+            Marshal.Copy(s.TempBuffer, 0, new IntPtr(buf), result);
+
+            return result;
+        }
+    }
+}
---
+++ 4_Operation/Graphic/src/Stb/Stbicontext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+
+namespace Alis.Core.Graphic.Stb
+{
+    /// <summary>
+    ///     The stbi context class
+    /// </summary>
+    public class Stbicontext
+    {
+        /// <summary>
+        ///     The stream
+        /// </summary>
+        private readonly Stream stream;
+
+        /// <summary>
+        ///     The temp buffer
+        /// </summary>
+        public byte[] TempBuffer;
+
+        /// <summary>
+        ///     The img
+        /// </summary>
+        public int Imgn = 0;
+
+        /// <summary>
+        ///     The img out
+        /// </summary>
+        public int Imgoutn = 0;
+
+        /// <summary>
+        ///     The img
+        /// </summary>
+        public uint Imgx = 0;
+
+        /// <summary>
+        ///     The img
+        /// </summary>
+        public uint Imgy = 0;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Stbicontext" /> class
+        /// </summary>
+        /// <param name="stream">The stream</param>
+        /// <exception cref="ArgumentNullException">stream</exception>
+        public Stbicontext(Stream stream)
+        {
+            if (stream == null)
+            {
+                throw new ArgumentNullException("stream");
+            }
+
+            this.stream = stream;
+        }
+
+        /// <summary>
+        ///     Gets the value of the stream
+        /// </summary>
+        public Stream Stream => stream;
+    }
+}
