1add758ed61a179aba28f2000dc19120b7de3fb0
Claudia2000pf <claper1992@gmail.com>
2025-06-08 00:00:00 +0100

    intptr fix

--- 4_Operation/Graphic/src/Stb/Delegate2.cs
+++ 4_Operation/Graphic/src/Stb/Delegate2.cs
@@ -1,7 +1,9 @@
-﻿namespace Alis.Core.Graphic.Stb
+﻿using System;
+
+namespace Alis.Core.Graphic.Stb
 {
     /// <summary>
     ///     The delegate
     /// </summary>
-    public unsafe delegate byte* Delegate2(byte* arg0, byte* arg1, byte* arg2, int arg3, int arg4);
+    public unsafe delegate byte* Delegate2(IntPtr arg0, IntPtr arg1, IntPtr arg2, int arg3, int arg4);
 }
