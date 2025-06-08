255e7e6f420b4967ece16380193144c555c796f4
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2025-06-09 00:00:00 +0100

    move struct and classes to files

---
+++ 4_Operation/Graphic/src/Stb/Stbihuffman.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Alis.Core.Graphic.Stb
+{
+    /// <summary>
+    ///     The stbi huffman
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public unsafe struct Stbihuffman
+    {
+        /// <summary>
+        ///     The fast
+        /// </summary>
+        public fixed byte fast[512];
+
+        /// <summary>
+        ///     The code
+        /// </summary>
+        public fixed ushort code[256];
+
+        /// <summary>
+        ///     The values
+        /// </summary>
+        public fixed byte values[256];
+
+        /// <summary>
+        ///     The size
+        /// </summary>
+        public fixed byte size[257];
+
+        /// <summary>
+        ///     The maxcode
+        /// </summary>
+        public fixed uint maxcode[18];
+
+        /// <summary>
+        ///     The delta
+        /// </summary>
+        public fixed int delta[17];
+    }
+}
