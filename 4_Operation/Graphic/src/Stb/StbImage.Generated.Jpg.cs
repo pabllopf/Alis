422db7ccba3dfe80dc000a55f90fadc3b419e290
Perdomo Falcon, Pablo <pabloperdomofalcon@gmail.com>
2025-06-10 17:10:47 +0200

    fix delegate to intptr

--- 4_Operation/Graphic/src/Stb/StbImage.Generated.Jpg.cs
+++ 4_Operation/Graphic/src/Stb/StbImage.Generated.Jpg.cs
@@ -1993,8 +1993,13 @@ namespace Alis.Core.Graphic.Stb
         /// <param name="pcr">The pcr</param>
         /// <param name="count">The count</param>
         /// <param name="step">The step</param>
-        public static void StbiYCbCrtoRgBrow(byte* @out, byte* y, byte* pcb, byte* pcr, int count, int step)
+        public static void StbiYCbCrtoRgBrow(IntPtr @outPtr, IntPtr yPtr, IntPtr pcbPtr, IntPtr pcrPtr, int count, int step)
         {
+            byte* @out = (byte*)@outPtr;
+            int* y = (int*)yPtr;
+            byte* pcb = (byte*)pcbPtr;
+            byte* pcr = (byte*)pcrPtr;
+            
             int i = 0;
             for (i = 0; i < count; ++i)
             {
@@ -2212,7 +2217,7 @@ namespace Alis.Core.Graphic.Stb
                             }
                             else
                             {
-                                z.YCbCrtoRgBkernel(@out, y, coutput[1], coutput[2], (int) z.S.Imgx, n);
+                                z.YCbCrtoRgBkernel((IntPtr)@out, (IntPtr)y, (IntPtr)coutput[1], (IntPtr)coutput[2], (int) z.S.Imgx, n);
                             }
                         }
                         else if (z.S.Imgn == 4)
@@ -2231,7 +2236,7 @@ namespace Alis.Core.Graphic.Stb
                             }
                             else if (z.App14Colortransform == 2)
                             {
-                                z.YCbCrtoRgBkernel(@out, y, coutput[1], coutput[2], (int) z.S.Imgx, n);
+                                z.YCbCrtoRgBkernel((IntPtr)@out, (IntPtr)y, (IntPtr)coutput[1], (IntPtr)coutput[2], (int) z.S.Imgx, n);
                                 for (i = 0; i < z.S.Imgx; ++i)
                                 {
                                     byte m = coutput[3][i];
@@ -2243,7 +2248,7 @@ namespace Alis.Core.Graphic.Stb
                             }
                             else
                             {
-                                z.YCbCrtoRgBkernel(@out, y, coutput[1], coutput[2], (int) z.S.Imgx, n);
+                                z.YCbCrtoRgBkernel((IntPtr)@out, (IntPtr)y, (IntPtr)coutput[1], (IntPtr)coutput[2], (int) z.S.Imgx, n);
                             }
                         }
                         else
