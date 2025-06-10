120289b495cd21ab048bc70b52707f3a2f68d800
Perdomo Falcon, Pablo <pabloperdomofalcon@gmail.com>
2025-06-10 17:14:21 +0200

    Update Delegate0.cs

--- 4_Operation/Graphic/src/Stb/Delegate0.cs
+++ 4_Operation/Graphic/src/Stb/Delegate0.cs
@@ -5,5 +5,5 @@ namespace Alis.Core.Graphic.Stb
     /// <summary>
     ///     The delegate
     /// </summary>
-    public unsafe delegate void Delegate0(IntPtr arg0, int arg1, IntPtr arg2);
+    public  delegate void Delegate0(IntPtr arg0, int arg1, IntPtr arg2);
 }
