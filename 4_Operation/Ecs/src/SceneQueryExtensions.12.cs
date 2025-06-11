3d9bb680302680d7ed3d0ecb037939777e3959b1
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2025-06-11 18:33:07 +0200

    fiz: the targetframework destiny

--- 4_Operation/Ecs/src/SceneQueryExtensions.12.cs
+++ 4_Operation/Ecs/src/SceneQueryExtensions.12.cs
@@ -1,3 +1,33 @@
+// --------------------------------------------------------------------------
+// 
+//                               █▀▀█ ░█─── ▀█▀ ░█▀▀▀█
+//                              ░█▄▄█ ░█─── ░█─ ─▀▀▀▄▄
+//                              ░█─░█ ░█▄▄█ ▄█▄ ░█▄▄▄█
+// 
+//  --------------------------------------------------------------------------
+//  File:SceneQueryExtensions.12.cs
+// 
+//  Author:Pablo Perdomo Falcón
+//  Web:https://www.pabllopf.dev/
+// 
+//  Copyright (c) 2021 GNU General Public License v3.0
+// 
+//  This program is free software:you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.If not, see <http://www.gnu.org/licenses/>.
+// 
+//  --------------------------------------------------------------------------
+
+using System.Runtime.InteropServices;
 using Alis.Core.Ecs.Core.Memory;
 using Alis.Core.Ecs.Systems;
 
@@ -29,19 +59,19 @@ namespace Alis.Core.Ecs
 
         {
 #if (NETSTANDARD || NETFRAMEWORK || NETCOREAPP) && (!NET6_0_OR_GREATER)
-        if (scene.QueryCache.TryGetValue(QueryHashCache<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>.Value,
-                out Query value)) return value;
+            if (scene.QueryCache.TryGetValue(QueryHashCache<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>.Value,
+                    out Query value)) return value;
 
-        value = scene.CreateQuery(MemoryHelpers.ReadOnlySpanToImmutableArray([
-            default(T1).Rule, default(T2).Rule, default(T3).Rule, default(T4).Rule, default(T5).Rule, default(T6).Rule,
-            default(T7).Rule, default(T8).Rule, default(T9).Rule, default(T10).Rule, default(T11).Rule,
-            default(T12).Rule
-        ]));
-        scene.QueryCache[QueryHashCache<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>.Value] = value;
-        return value;
+            value = scene.CreateQuery(MemoryHelpers.ReadOnlySpanToImmutableArray([
+                default(T1).Rule, default(T2).Rule, default(T3).Rule, default(T4).Rule, default(T5).Rule, default(T6).Rule,
+                default(T7).Rule, default(T8).Rule, default(T9).Rule, default(T10).Rule, default(T11).Rule,
+                default(T12).Rule
+            ]));
+            scene.QueryCache[QueryHashCache<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>.Value] = value;
+            return value;
 #else
             ref Query cachedValue =
-                ref System.Runtime.InteropServices.CollectionsMarshal.GetValueRefOrAddDefault(scene.QueryCache, QueryHashCache<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>.Value, out bool exists);
+                ref CollectionsMarshal.GetValueRefOrAddDefault(scene.QueryCache, QueryHashCache<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>.Value, out bool exists);
             if (!exists)
             {
                 cachedValue =
