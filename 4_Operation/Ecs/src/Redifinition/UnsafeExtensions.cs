020e1f475ff1807dbce2bbe75dc42cb35fe53d1c
Perdomo Falcon, Pablo <pabloperdomofalcon@gmail.com>
2025-07-07 13:31:44 +0200

    Update UnsafeExtensions.cs

--- 4_Operation/Ecs/src/Redifinition/UnsafeExtensions.cs
+++ 4_Operation/Ecs/src/Redifinition/UnsafeExtensions.cs
@@ -9,19 +9,6 @@ namespace Alis.Core.Ecs.Redifinition
     /// </summary>
     public static class UnsafeExtensions
     {
-#if (NETSTANDARD || NETFRAMEWORK || NETCOREAPP) && (!NET6_0_OR_GREATER)
-       /// <summary>
-        ///     Unsafes the array index using the specified arr
-        /// </summary>
-        /// <typeparam name="T">The </typeparam>
-        /// <param name="arr">The arr</param>
-        /// <param name="index">The index</param>
-        /// <returns>The ref</returns>
-        public static ref T UnsafeArrayIndex<T>(this T[] arr, nint index)
-        {
-            return ref Unsafe.Add(ref arr[0], index);
-        }
-        
         /// <summary>
         ///     Unsafes the array index using the specified arr
         /// </summary>
@@ -34,55 +21,6 @@ namespace Alis.Core.Ecs.Redifinition
             return ref Unsafe.Add(ref arr[0], index);
         }
         
-        /// <summary>
-        ///     Unsafes the array index using the specified arr
-        /// </summary>
-        /// <typeparam name="T">The </typeparam>
-        /// <param name="arr">The arr</param>
-        /// <param name="index">The index</param>
-        /// <returns>The ref</returns>
-        public static ref T UnsafeArrayIndex<T>(this T[] arr, ushort index)
-        {
-            return ref Unsafe.Add(ref arr[0], index);
-        }
-        #else
-         /// <summary>
-        ///     Unsafes the array index using the specified arr
-        /// </summary>
-        /// <typeparam name="T">The </typeparam>
-        /// <param name="arr">The arr</param>
-        /// <param name="index">The index</param>
-        /// <returns>The ref</returns>
-        public static ref T UnsafeArrayIndex<T>(this T[] arr, nint index)
-        {
-            return ref Unsafe.Add(ref MemoryMarshal.GetArrayDataReference(arr), index);
-        }
-
-        /// <summary>
-        ///     Unsafes the array index using the specified arr
-        /// </summary>
-        /// <typeparam name="T">The </typeparam>
-        /// <param name="arr">The arr</param>
-        /// <param name="index">The index</param>
-        /// <returns>The ref</returns>
-        public static ref T UnsafeArrayIndex<T>(this T[] arr, int index)
-        {
-            return ref Unsafe.Add(ref MemoryMarshal.GetArrayDataReference(arr), index);
-        }
-
-        /// <summary>
-        ///     Unsafes the array index using the specified arr
-        /// </summary>
-        /// <typeparam name="T">The </typeparam>
-        /// <param name="arr">The arr</param>
-        /// <param name="index">The index</param>
-        /// <returns>The ref</returns>
-        public static ref T UnsafeArrayIndex<T>(this T[] arr, ushort index)
-        {
-            return ref Unsafe.Add(ref MemoryMarshal.GetArrayDataReference(arr), index);
-        }
-        #endif
-
         /// <summary>
         ///     Unsafes the span index using the specified arr
         /// </summary>
@@ -91,18 +29,6 @@ namespace Alis.Core.Ecs.Redifinition
         /// <param name="index">The index</param>
         /// <returns>The ref</returns>
         public static ref T UnsafeSpanIndex<T>(this Span<T> arr, int index)
-        {
-            return ref Unsafe.Add(ref MemoryMarshal.GetReference(arr), index);
-        }
-
-        /// <summary>
-        ///     Unsafes the span index using the specified arr
-        /// </summary>
-        /// <typeparam name="T">The </typeparam>
-        /// <param name="arr">The arr</param>
-        /// <param name="index">The index</param>
-        /// <returns>The ref</returns>
-        public static ref T UnsafeSpanIndex<T>(this Span<T> arr, ushort index)
         {
             return ref Unsafe.Add(ref MemoryMarshal.GetReference(arr), index);
         }
