379670ee98e622d1d31d4487689ed3b98fcdd9d7
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2025-07-15 00:00:00 +0100

    fix: render color

--- 1_Presentation/Engine/src/web/ImGui.cs
+++ 1_Presentation/Engine/src/web/ImGui.cs
@@ -28,6 +28,44 @@ namespace Alis.App.Engine.Web
             _commands.Add($"ImGui.Text(\"{text}\");");
         }
 
+
+        public static void ColorEdit3(string label, float[] colorVar)
+        {
+            if (colorVar.Length != 3)
+                throw new ArgumentException("Color array must have exactly 3 elements for RGB.");
+
+            string jsArray = "[" + string.Join(", ", colorVar) + "]";
+            string safeLabel = label.Replace("\"", "\\\"");
+
+            _commands.Add($"(function() {{ let color = {jsArray}; if (ImGui.ColorEdit3(\"{safeLabel}\", color)) {{ DotNet.invokeMethodAsync('Alis.App.Engine.Web', 'SetColorFromJs', color[0], color[1], color[2]); }} }})();");
+        }
+
+
+
+        public static void PlotLines(string label, float[] valuesVar, int valuesLength, int offset, string overlayText, float scaleMin, float scaleMax, float sizeX, float sizeY)
+        {
+            string jsArray = "[" + string.Join(", ", valuesVar) + "]";
+            _commands.Add($"ImGui.PlotLines(\"{label}\", {jsArray}, {valuesLength}, {offset}, \"{overlayText}\", {scaleMin}, {scaleMax}, new ImVec2({sizeX}, {sizeY}));");
+        }
+
+
+        public static void PlotHistogram(string label, float[] valuesVar, int valuesLength, int offset, string overlayText, float scaleMin, float scaleMax, float sizeX, float sizeY)
+        {
+            string jsArray = "[" + string.Join(", ", valuesVar) + "]";
+            _commands.Add($"ImGui.PlotHistogram(\"{label}\", {jsArray}, {valuesLength}, {offset}, \"{overlayText}\", {scaleMin}, {scaleMax}, new ImVec2({sizeX}, {sizeY}));");
+        }
+        
+        
+        public static void TextColored(float r, float g, float b, float a, string text)
+        {
+            _commands.Add($"ImGui.TextColored(new ImVec4({r}, {g}, {b}, {a}), \"{text}\");");
+        }
+
+        public static void TextDisabled(string text)
+        {
+            _commands.Add($"ImGui.TextDisabled(\"{text}\");");
+        }
+
         public static void End()
         {
             _commands.Add("ImGui.End();");
