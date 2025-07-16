505213c0cfbf652a7c6b22705cec6a6f2584ccd8
Perdomo Falcon, Pablo <pabloperdomofalcon@gmail.com>
2025-07-16 11:27:22 +0200

    feature: add simple sample engine on web blazor

---
+++ 1_Presentation/Engine/src/web/HelloInterop.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.JSInterop;
+
+namespace Alis.App.Engine.Web
+{
+    public static class HelloInterop
+    {
+        private static float _checkboxValue = 0.5f;
+
+        [JSInvokable]
+        public static async Task RenderUI()
+        {
+            
+            Dictionary<string, float> values = await ImGui.Process(imgui =>
+            {
+                imgui.Begin("Hola");
+                imgui.Text("Texto");
+                imgui.SliderFloat("Slider", _checkboxValue, 0.0f, 1.0f);
+                imgui.End();
+                
+                imgui.Begin("Hola mundo 2");
+                imgui.Text("Texto 3332");
+                imgui.SliderFloat("Slider22", _checkboxValue, 0.0f, 1.0f);
+                imgui.End();
+            });
+
+            if (values.TryGetValue("Slider", out float newValue))
+            {
+                _checkboxValue = newValue;
+            }
+            
+            if (values.TryGetValue("Slider22", out float newValue2))
+            {
+                _checkboxValue = newValue2;
+            }
+
+            
+        }
+    
+    
+    
+    }
+}
---
+++ 1_Presentation/Engine/src/web/wwwroot/system.config.js
@@ -0,0 +1,9 @@
+System.config({
+    map: {
+        imports: {
+            "bind-imgui": "../build/bind-imgui.js",
+            "imgui-js": "../build/imgui.js",
+            "main": "./build/main.js",
+        }
+    },
+});
