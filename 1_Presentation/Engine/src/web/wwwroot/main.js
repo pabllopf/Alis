f6b733319255aaeb7f2abecef92015f45d7a6e24
Perdomo Falcon, Pablo <pabloperdomofalcon@gmail.com>
2025-07-24 12:53:29 +0200

    fix: custom style  web

--- 1_Presentation/Engine/src/web/wwwroot/main.js
+++ 1_Presentation/Engine/src/web/wwwroot/main.js
@@ -10,7 +10,56 @@ canvas.width = canvas.clientWidth * devicePixelRatio;
 canvas.height = canvas.clientHeight * devicePixelRatio;
 
 await ImGuiImplWeb.InitWebGL(canvas);
+setImGuiStyle();
 
+function setImGuiStyle() {
+    console.info("Setting ImGui style...");
+    
+    ImGui.StyleColorsDark();
+
+    const io = ImGui.GetIO();
+    io.FontGlobalScale = 2.0;
+
+    io.ConfigFlags |= ImGui.ConfigFlags.DockingEnable;
+
+    const style = ImGui.GetStyle();
+    
+    // Parámetros de estilo
+    style.WindowRounding = 0.0;
+    style.ChildRounding = 4.0;
+    style.FrameRounding = 4.0;
+    style.PopupRounding = 4.0;
+    style.ScrollbarRounding = 4.0;
+    style.GrabRounding = 2.0;
+    style.LogSliderDeadzone = 4.0;
+    style.TabRounding = 4.0;
+    style.WindowBorderSize = 1.0;
+    style.ChildBorderSize = 1.0;
+    style.PopupBorderSize = 1.0;
+    style.FrameBorderSize = 1.0;
+    style.TabBorderSize = 1.0;
+    style.WindowPadding = new ImVec2(10, 10);
+    style.FramePadding = new ImVec2(6, 6);
+    style.ItemSpacing = new ImVec2(6, 6);
+    style.ItemInnerSpacing = new ImVec2(6, 6);
+    style.CellPadding = new ImVec2(10, 10);
+    style.TouchExtraPadding = new ImVec2(0, 0);
+    style.IndentSpacing = 15;
+    style.ScrollbarSize = 12;
+    style.GrabMinSize = 10;
+    style.WindowTitleAlign = new ImVec2(0.5, 0.5);
+    style.ButtonTextAlign = new ImVec2(0.5, 0.5);
+    style.DisplayWindowPadding = new ImVec2(19, 19);
+    style.DisplaySafeAreaPadding = new ImVec2(3, 3);
+    style.AntiAliasedLines = true;
+    style.AntiAliasedFill = true;
+    style.CurveTessellationTol = 1.25;
+    style.CircleTessellationMaxError = 0.2;
+    style.Alpha = 1.0;
+    style.DisabledAlpha = 0.6;
+
+    console.info("ImGui style set to dark");
+}
 
 window.ImGuiInterop = {
     processFrame: function(commands) {
@@ -105,86 +154,72 @@ let evalCode = "";
 
 async function frame() {
     ImGuiImplWeb.BeginRenderWebGL();
+    
+    // Puedes usar PushStyleColor para sobreescribir colores por frame
+   ImGui.PushStyleColor(ImGui.Col.TextDisabled,         0xFF808080);
+   
+   ImGui.PushStyleColor(ImGui.Col.WindowBg,             0xFF212426);
+   ImGui.PushStyleColor(ImGui.Col.ChildBg,              0xFF212426);
+   ImGui.PushStyleColor(ImGui.Col.PopupBg,              0xFF212426);
+   ImGui.PushStyleColor(ImGui.Col.Border,               0xFF404040);
+   ImGui.PushStyleColor(ImGui.Col.BorderShadow,         0x00000000);
+   
+   ImGui.PushStyleColor(ImGui.Col.FrameBg,              0xFF333333);
+   ImGui.PushStyleColor(ImGui.Col.FrameBgHovered,       0xFF4D4D4D);
+   ImGui.PushStyleColor(ImGui.Col.FrameBgActive,        0xFF666666);
+   
+   ImGui.PushStyleColor(ImGui.Col.TitleBg,              0xFF1A1A1A);
+   ImGui.PushStyleColor(ImGui.Col.TitleBgActive,        0xFF1A1A1A);
+   ImGui.PushStyleColor(ImGui.Col.TitleBgCollapsed,     0xFF1A1A1A);
+   
+   
+   ImGui.PushStyleColor(ImGui.Col.MenuBarBg,            0xFF262626);
+   ImGui.PushStyleColor(ImGui.Col.ScrollbarBg,          0xFF1A1A1A);
+   ImGui.PushStyleColor(ImGui.Col.ScrollbarGrab,        0xFF4D4D4D);
+   ImGui.PushStyleColor(ImGui.Col.ScrollbarGrabHovered, 0xFF666666);
+   ImGui.PushStyleColor(ImGui.Col.ScrollbarGrabActive,  0xFF808080);
+   ImGui.PushStyleColor(ImGui.Col.CheckMark,            0xFFFA9743);
+   ImGui.PushStyleColor(ImGui.Col.SliderGrab,           0xFFFA9743);
+   ImGui.PushStyleColor(ImGui.Col.SliderGrabActive,     0xFFFA9743);
+   ImGui.PushStyleColor(ImGui.Col.Button,               0xFF333333);
+   ImGui.PushStyleColor(ImGui.Col.ButtonHovered,        0xFF4D4D4D);
+   ImGui.PushStyleColor(ImGui.Col.ButtonActive,         0xFF666666);
+   ImGui.PushStyleColor(ImGui.Col.Header,               0xFF333333);
+   ImGui.PushStyleColor(ImGui.Col.HeaderHovered,        0xFF4D4D4D);
+   ImGui.PushStyleColor(ImGui.Col.HeaderActive,         0xFF666666);
+   ImGui.PushStyleColor(ImGui.Col.Separator,            0xFF404040);
+   ImGui.PushStyleColor(ImGui.Col.SeparatorHovered,     0xFF4D4D4D);
+   ImGui.PushStyleColor(ImGui.Col.SeparatorActive,      0xFF666666);
+   ImGui.PushStyleColor(ImGui.Col.ResizeGrip,           0xFF333333);
+   ImGui.PushStyleColor(ImGui.Col.ResizeGripHovered,    0xFF4D4D4D);
+   ImGui.PushStyleColor(ImGui.Col.ResizeGripActive,     0xFF666666);
+   ImGui.PushStyleColor(ImGui.Col.Tab,                  0xFF1A1A1A);
+   ImGui.PushStyleColor(ImGui.Col.TabHovered,           0xFF4D4D4D);
+   ImGui.PushStyleColor(ImGui.Col.TabActive,            0xFF666666);
+   ImGui.PushStyleColor(ImGui.Col.TabUnfocused,         0xFF1A1A1A);
+   ImGui.PushStyleColor(ImGui.Col.TabUnfocusedActive,   0xFF666666);
+   ImGui.PushStyleColor(ImGui.Col.PlotLines,            0xFF9C9C9C);
+   ImGui.PushStyleColor(ImGui.Col.PlotLinesHovered,     0xFFB3B3B3);
+   ImGui.PushStyleColor(ImGui.Col.PlotHistogram,        0xFF9C9C9C);
+   ImGui.PushStyleColor(ImGui.Col.PlotHistogramHovered, 0xFFB3B3B3);
+   ImGui.PushStyleColor(ImGui.Col.TextSelectedBg,       0xFFFA9743);
+   ImGui.PushStyleColor(ImGui.Col.DragDropTarget,       0xFFFA9743);
+   ImGui.PushStyleColor(ImGui.Col.NavHighlight,         0xFFFA9743);
+   ImGui.PushStyleColor(ImGui.Col.NavWindowingHighlight,0xFFFA9743);
+   ImGui.PushStyleColor(ImGui.Col.NavWindowingDimBg,    0x99333333);
+   ImGui.PushStyleColor(ImGui.Col.ModalWindowDimBg,     0x99333333);
 
-    ImGui.SetNextWindowPos(new ImVec2(10, 10), ImGui.Cond.Once);
-    ImGui.SetNextWindowSize(new ImVec2(330, 400), ImGui.Cond.Once);
-    ImGui.Begin("WebGL");
+    ImGui.PushStyleColor(ImGui.Col.Text,                 0xFFFFFFFF);
 
-    ImGui.SeparatorText("Welcome");
-    ImGui.Text("Welcome to jsimgui!");
-    ImGui.TextDisabled(`Using ImGui v${ImGui.GetVersion()}-docking`);
-
-    ImGui.Spacing();
-
-    if (ImGui.TreeNode("Other Examples")) {
-        ImGui.Bullet();
-        if (ImGui.TextLink("Three.js")) {
-            globalThis.open("https://mori2003.github.io/jsimgui/docs/examples/threegl/", "_self");
-        }
-        ImGui.SameLine();
-        ImGui.Text("(WebGL2 Renderer)");
-        ImGui.Spacing();
-        ImGui.TreePop();
-    }
-
-    if (ImGui.TreeNode("Source Code")) {
-        if (ImGui.TextLink("Github")) {
-            globalThis.open("https://github.com/mori2003/jsimgui/", "_self");
-        }
-        ImGui.TreePop();
-    }
-
-    ImGui.Spacing();
-    ImGui.Checkbox("Show ImGui Demo", showDemo);
-    ImGui.SameLine();
-    if (ImGui.Checkbox("Enable Docking", docking)) {
-        if (docking[0]) {
-            const io = ImGui.GetIO();
-            io.ConfigFlags |= ImGui.ConfigFlags.DockingEnable;
-        } else {
-            const io = ImGui.GetIO();
-            io.ConfigFlags &= ~ImGui.ConfigFlags.DockingEnable;
-        }
-    }
-
-    ImGui.SeparatorText("Features");
-
-    if (ImGui.CollapsingHeader("Widgets")) {
-        if (ImGui.Button("Button")) {
-            alert("Button pressed");
-        }
-
-        ImGui.Text("Text");
-        ImGui.TextColored(new ImVec4(1, 1, 0, 1), "Colored Text");
-        ImGui.TextDisabled("Disabled Text");
-
-        ImGui.Image(new ImTextureRef(jsLogo), new ImVec2(50, 50));
-
-        const values = [0, 1, 2, 3, 4];
-        ImGui.ColorEdit3("clearColor", color);
-        ImGui.PlotLines("My Plot", values, values.length, 0, "", 0, 4, new ImVec2(0, 80));
-        ImGui.PlotHistogram("My Histogram", values, values.length, 0, "", 0, 4, new ImVec2(0, 80));
-    }
-
-    /*
-    // 🔄 Llamada al método C# que devuelve el código
-    const newCode = await DotNet.invokeMethodAsync("Alis.App.Engine.Web", "GetImGuiCode");
-    code[0] = newCode;
-
-    // Ejecutar el código recibido
-    evalCode = code[0];
-    try {
-        eval(evalCode);
-    } catch (e) {
-        console.error("Error al ejecutar código desde Blazor:", e);
-    }*/
-
+    
+    ImGui.ShowDemoWindow(showDemo);
+    
+    
     await DotNet.invokeMethodAsync("Alis.App.Engine.Web", "RenderUi");
+    
+    ImGui.PopStyleColor(48);
 
-    ImGui.End();
-
-    if (showDemo[0]) ImGui.ShowDemoWindow(showDemo);
-
+    
     context.clearColor(color[0], color[1], color[2], 1.0);
     context.clear(context.COLOR_BUFFER_BIT);
 
