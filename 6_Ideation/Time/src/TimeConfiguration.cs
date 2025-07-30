76028d21f47223e6ddb7cb350de739bab57ab7bd
Perdomo Falcon, Pablo <pabloperdomofalcon@gmail.com>
2025-07-30 14:04:49 +0200

    docs: add new diagram

--- 6_Ideation/Time/src/TimeConfiguration.cs
+++ 6_Ideation/Time/src/TimeConfiguration.cs
@@ -32,42 +32,5 @@ namespace Alis.Core.Aspect.Time
     /// <summary>
     ///     The time class
     /// </summary>
-    public class TimeConfiguration
-    {
-        /// <summary>
-        ///     Initializes a new instance of the <see cref="TimeConfiguration" /> class
-        /// </summary>
-        /// <param name="fixedTimeStep">The fixed time step</param>
-        /// <param name="maximumAllowedTimeStep">The maximum allowed time step</param>
-        /// <param name="timeScale">The time scale</param>
-        public TimeConfiguration(float fixedTimeStep = 0.016f, float maximumAllowedTimeStep = 0.1f, float timeScale = 1.00f)
-        {
-            FixedTimeStep = fixedTimeStep;
-            MaximumAllowedTimeStep = maximumAllowedTimeStep;
-            TimeScale = timeScale;
-        }
-
-        /// <summary>
-        ///     A framerate-independent interval that dictates when physics calculations and FixedUpdate() events are performed.
-        /// </summary>
-        public float FixedTimeStep { get; set; }
-
-        /// <summary>
-        ///     A framerate-independent interval that caps the worst case scenario when frame-rate is low.
-        ///     Physics calculations and FixedUpdate() events will not be performed for longer time than specified.
-        /// </summary>
-        public float MaximumAllowedTimeStep { get; set; }
-
-        /// <summary>
-        ///     The speed at which time progresses.
-        ///     Change this value to simulate bullet-time effects.
-        ///     A value of 1 means real-time. A value of .5 means half speed; a value of 2 is double speed.
-        /// </summary>
-        public float TimeScale { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the value of the log output
-        /// </summary>
-        public bool LogOutput { get; set; } = true;
-    }
+    public record struct TimeConfiguration(float FixedTimeStep = 0.016f, float MaximumAllowedTimeStep = 0.1f, float TimeScale = 1.00f, bool LogOutput = true);
 }
