e849da661998cf384283e05da4cc2e2dc32e24e4
Pedro D.GR <pedrorng80@gmail.com>
2025-08-30 00:00:00 +0100

    fix: all comment xml of files generators

--- 4_Operation/Ecs/generator/Models/SourceOutput.cs
+++ 4_Operation/Ecs/generator/Models/SourceOutput.cs
@@ -2,6 +2,11 @@ using System.Runtime.InteropServices;
 
 namespace Alis.Core.Ecs.Generator.Models
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="Name"></param>
+    /// <param name="Source"></param>
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
 public record struct SourceOutput(string Name, string Source);
 }
