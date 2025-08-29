c8582de7d26ed9eb8dbabefd47070d03e7715ffb
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2025-08-30 00:00:00 +0100

    fix: the pack of generators

--- docs/scripts/windows/run_pack_all.bat
+++ docs/scripts/windows/run_pack_all.bat
@@ -1,5 +1,16 @@
 cd ../../../
 
+
+rd /s /q .nuget
+rd /s /q .build
+
+FOR /d /r . %%d IN (bin) DO @IF EXIST "%%d" rd /s /q "%%d"
+
+FOR /d /r . %%d IN (obj) DO @IF EXIST "%%d" rd /s /q "%%d"
+
+for /r %%i in (*.csproj) do dotnet build %%i -c Release
+
+
 set "version=6.0.0"
 set "publishDir=.\.publish\%version%"
 
