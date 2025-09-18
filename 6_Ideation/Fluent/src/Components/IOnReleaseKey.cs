cba7419eb59de68f7318824e3c0e73045f74bb9a
Chgv99 <christiangv99@gmail.com>
2025-09-19 00:00:00 +0100

    feature: add new IOnHoldKey, IOnReleaseKey

---
+++ 6_Ideation/Fluent/src/Components/IOnReleaseKey.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Alis.Core.Aspect.Fluent.Components
+{
+    /// <summary>
+    /// Interfaz para gestionar el evento de soltar una tecla
+    /// </summary>
+    public interface IOnReleaseKey
+    {
+        void OnReleaseKey(ConsoleKey key);
+    }
+}
+
