67e6b2e4024d3c28cfa00f0fd7508fbb1d936270
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2025-10-19 16:34:03 +0200

    feature: new script to pack all nugets

---
+++ docs/scripts/macos/pack_all.sh
@@ -0,0 +1,26 @@
+echo "Do you want to continue? (y/n)"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) 
+
+    cd ../../../
+    
+    find . -type d -name "publish" -exec rm -Rf {} \;
+    find . -type d -name ".publish" -exec rm -Rf {} \;
+                
+    for i in `find . -name "*.csproj" -type f`; 
+      do if [[ $i == *".Template."* || $i == *".App."* || $i == *".Test."* || $i == *".Benchmark."* || $i == *".Sample."* ]] ; 
+      then 
+        echo "Skip project $i"; 
+      else 
+        dotnet pack -c Release $i -o ./.publish/test/ ; 
+      fi;done
+    
+    cd ./docs/scripts/macos/ || exit 
+
+break;;
+No ) 
+          echo "Goodbye!"
+          exit;;
+    esac
+done
