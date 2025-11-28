c61d03777064a5cf3c3bb2a7787da1e2b69f8824
Claudia2000pf <claper1992@gmail.com>
2025-11-28 00:00:00 +0000

    feature: Refactor ImguiSample to require INativePlatform
    ImguiSample now takes INativePlatform in its constructor and uses it for window size and OpenGL context management. Initialization, rendering, and cleanup logic for ImGui and OpenGL resources have been expanded and moved into ImguiSample. Program.cs updated to instantiate ImguiSample after platform and OpenGL context initialization.

--- 1_Presentation/Extension/Graphic/Ui/sample/Examples/ImguiSample.cs
+++ 1_Presentation/Extension/Graphic/Ui/sample/Examples/ImguiSample.cs
@@ -27,36 +27,270 @@
 // 
 //  --------------------------------------------------------------------------
 
+using System;
+using System.Runtime.InteropServices;
+using Alis.Core.Aspect.Logging;
+using Alis.Core.Aspect.Math.Matrix;
+using Alis.Core.Graphic.OpenGL;
+using Alis.Core.Graphic.OpenGL.Enums;
+using Alis.Core.Graphic.Platforms;
+using Alis.Extension.Graphic.Ui;
+
 namespace Alis.Extension.Graphic.Ui.Sample.Examples
 {
-    /// <summary>
-    /// The imgui sample class
-    /// </summary>
-    /// <seealso cref="IExample"/>
     public class ImguiSample : IExample
     {
-        /// <summary>
-        /// Initializes this instance
-        /// </summary>
+        private readonly INativePlatform _platform;
+        private IntPtr _context;
+
+        private uint _fontTexture;
+        private uint _vao;
+        private uint _vbo;
+        private uint _ebo;
+        private uint _shaderProgram;
+
+        private bool _showDemo = true;
+        private int _counter;
+
+        public ImguiSample(INativePlatform platform)
+        {
+            _platform = platform;
+        }
+
         public void Initialize()
         {
+            // Create ImGui context (uses local wrapper)
+            _context = ImGui.CreateContext();
+            ImGui.SetCurrentContext(_context);
+            var io = ImGui.GetIo();
+            io.BackendFlags |= ImGuiBackendFlags.HasMouseCursors | ImGuiBackendFlags.HasSetMousePos;
+            io.ConfigFlags |= ImGuiConfigFlags.NavEnableKeyboard;
+            ImGui.StyleColorsDark();
 
+            // Build font atlas using safe API and upload to GL
+            var fonts = io.Fonts;
+            // Request font atlas data as managed byte[] (RGBA32)
+            fonts.GetTexDataAsRgba32(out byte[] pixels, out int width, out int height, out _);
+
+            if (pixels != null && width > 0 && height > 0)
+            {
+                _fontTexture = Gl.GenTexture();
+                Gl.GlBindTexture(TextureTarget.Texture2D, _fontTexture);
+                Gl.GlTexParameteri(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, TextureParameter.ClampToEdge);
+                Gl.GlTexParameteri(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, TextureParameter.ClampToEdge);
+                Gl.GlTexParameteri(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, TextureParameter.Nearest);
+                Gl.GlTexParameteri(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, TextureParameter.Nearest);
+
+                // Set pixel alignment
+                Gl.GlPixelStorei(StoreParameter.UnpackAlignment, 1);
+
+                GCHandle handle = GCHandle.Alloc(pixels, GCHandleType.Pinned);
+                try
+                {
+                    IntPtr ptr = handle.AddrOfPinnedObject();
+                    Gl.GlTexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, width, height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, ptr);
+                }
+                finally
+                {
+                    handle.Free();
+                }
+
+                // Tell ImGui about our texture id (store as IntPtr)
+                fonts.SetTexId((IntPtr)_fontTexture);
+                // CPU side can be freed
+                // Note: wrapper may provide ClearTexData
+                // fonts.ClearTexData(); // only if available
+            }
+
+            // Create simple shader program
+            const string vertexShaderSource = "#version 330 core\n" +
+                                              "layout (location = 0) in vec2 Position;\n" +
+                                              "layout (location = 1) in vec2 UV;\n" +
+                                              "layout (location = 2) in vec4 Color;\n" +
+                                              "out vec2 Frag_UV;\n" +
+                                              "out vec4 Frag_Color;\n" +
+                                              "uniform mat4 ProjMtx;\n" +
+                                              "void main() { Frag_UV = UV; Frag_Color = Color; gl_Position = ProjMtx * vec4(Position.xy, 0, 1); }\n";
+
+            const string fragmentShaderSource = "#version 330 core\n" +
+                                                "in vec2 Frag_UV;\n" +
+                                                "in vec4 Frag_Color;\n" +
+                                                "uniform sampler2D Texture;\n" +
+                                                "out vec4 Out_Color;\n" +
+                                                "void main() { Out_Color = Frag_Color * texture(Texture, Frag_UV.st); }\n";
+
+            uint vert = Gl.GlCreateShader(ShaderType.VertexShader);
+            Gl.ShaderSource(vert, vertexShaderSource);
+            Gl.GlCompileShader(vert);
+            if (!Gl.GetShaderCompileStatus(vert))
+            {
+                Logger.Info("Vertex shader compile error: " + Gl.GetShaderInfoLog(vert));
+            }
+
+            uint frag = Gl.GlCreateShader(ShaderType.FragmentShader);
+            Gl.ShaderSource(frag, fragmentShaderSource);
+            Gl.GlCompileShader(frag);
+            if (!Gl.GetShaderCompileStatus(frag))
+            {
+                Logger.Info("Fragment shader compile error: " + Gl.GetShaderInfoLog(frag));
+            }
+
+            _shaderProgram = Gl.GlCreateProgram();
+            Gl.GlAttachShader(_shaderProgram, vert);
+            Gl.GlAttachShader(_shaderProgram, frag);
+            Gl.GlLinkProgram(_shaderProgram);
+            if (!Gl.GetProgramLinkStatus(_shaderProgram))
+            {
+                Logger.Info("Shader link error: " + Gl.GetProgramInfoLog(_shaderProgram));
+            }
+
+            Gl.GlDeleteShader(vert);
+            Gl.GlDeleteShader(frag);
+
+            // Create VAO/VBO/EBO
+            _vao = Gl.GenVertexArray();
+            _vbo = Gl.GenBuffer();
+            _ebo = Gl.GenBuffer();
+
+            Gl.GlBindVertexArray(_vao);
+            Gl.GlBindBuffer(BufferTarget.ArrayBuffer, _vbo);
+            Gl.GlBindBuffer(BufferTarget.ElementArrayBuffer, _ebo);
+
+            // ImDrawVert layout: pos(2 floats), uv(2 floats), col(uint) => stride
+            int stride = Marshal.SizeOf<ImDrawVert>();
+            Gl.VertexAttribPointer(0, 2, VertexAttribPointerType.Float, false, stride, IntPtr.Zero);
+            Gl.EnableVertexAttribArray(0);
+            Gl.VertexAttribPointer(1, 2, VertexAttribPointerType.Float, false, stride, new IntPtr(8));
+            Gl.EnableVertexAttribArray(1);
+            Gl.VertexAttribPointer(2, 4, VertexAttribPointerType.UnsignedByte, true, stride, new IntPtr(16));
+            Gl.EnableVertexAttribArray(2);
+
+            Gl.GlBindBuffer(BufferTarget.ArrayBuffer, 0);
+            Gl.GlBindVertexArray(0);
         }
 
-        /// <summary>
-        /// Draws this instance
-        /// </summary>
         public void Draw()
         {
+            var io = ImGui.GetIo();
+            io.DisplaySize = new Alis.Core.Aspect.Math.Vector.Vector2F(_platform.GetWindowWidth(), _platform.GetWindowHeight());
 
+            ImGui.NewFrame();
+
+            if (_showDemo)
+            {
+                ImGui.ShowDemoWindow(ref _showDemo);
+            }
+
+            ImGui.Begin("Ejemplo Alis ImGui");
+            ImGui.Text($"Contador: {_counter}");
+            if (ImGui.Button("Incrementar")) _counter++;
+            ImGui.End();
+
+            ImGui.Render();
+            var drawData = ImGui.GetDrawData();
+            RenderDrawData(drawData);
         }
 
-        /// <summary>
-        /// Cleanups this instance
-        /// </summary>
+        private void RenderDrawData(ImDrawData drawData)
+        {
+            if (drawData.CmdListsCount == 0)
+                return;
+
+            Gl.GlEnable(EnableCap.Blend);
+            Gl.GlBlendEquation(BlendEquationMode.FuncAdd);
+            Gl.GlBlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
+            Gl.GlDisable(EnableCap.CullFace);
+            Gl.GlDisable(EnableCap.DepthTest);
+            Gl.GlEnable(EnableCap.ScissorTest);
+
+            Gl.GlViewport(0, 0, _platform.GetWindowWidth(), _platform.GetWindowHeight());
+
+            float l = 0.0f;
+            float r = ImGui.GetIo().DisplaySize.X;
+            float t = 0.0f;
+            float b = ImGui.GetIo().DisplaySize.Y;
+
+            var ortho = new Matrix4X4(
+                2.0f / (r - l), 0, 0, 0,
+                0, 2.0f / (t - b), 0, 0,
+                0, 0, -1.0f, 0,
+                (r + l) / (l - r), (t + b) / (b - t), 0, 1.0f);
+
+            Gl.GlUseProgram(_shaderProgram);
+            int projLocation = Gl.GlGetUniformLocation(_shaderProgram, "ProjMtx");
+            Gl.UniformMatrix4Fv(projLocation, ortho);
+            int texLocation = Gl.GlGetUniformLocation(_shaderProgram, "Texture");
+            Gl.GlUniform1I(texLocation, 0);
+
+            Gl.GlBindVertexArray(_vao);
+
+            for (int n = 0; n < drawData.CmdListsCount; n++)
+            {
+                var cmdList = drawData.CmdListsRange[n];
+
+                int vtxBufferSize = cmdList.VtxBuffer.Size * Marshal.SizeOf<ImDrawVert>();
+                int idxBufferSize = cmdList.IdxBuffer.Size * sizeof(ushort);
+
+                Gl.GlBindBuffer(BufferTarget.ArrayBuffer, _vbo);
+                // copy vertex data
+                // Marshal.Copy from unmanaged ptr to GC pinned buffer via GlBufferData expects IntPtr source
+                Gl.GlBufferData(BufferTarget.ArrayBuffer, new IntPtr(vtxBufferSize), cmdList.VtxBuffer.Data, BufferUsageHint.StreamDraw);
+
+                Gl.GlBindBuffer(BufferTarget.ElementArrayBuffer, _ebo);
+                Gl.GlBufferData(BufferTarget.ElementArrayBuffer, new IntPtr(idxBufferSize), cmdList.IdxBuffer.Data, BufferUsageHint.StreamDraw);
+
+                long idxOffset = 0;
+                for (int cmdi = 0; cmdi < cmdList.CmdBuffer.Size; cmdi++)
+                {
+                    ImDrawCmd pcmd = cmdList.CmdBuffer[cmdi];
+                    if (pcmd.UserCallback != IntPtr.Zero)
+                    {
+                        // user callback (not handled)
+                    }
+                    else
+                    {
+                        IntPtr texIdPtr = pcmd.GetTexId();
+                        uint texId = texIdPtr == IntPtr.Zero ? _fontTexture : (uint)texIdPtr.ToInt64();
+
+                        Gl.GlActiveTexture(TextureUnit.Texture0);
+                        Gl.GlBindTexture(TextureTarget.Texture2D, texId);
+
+                        int x = (int)pcmd.ClipRect.X;
+                        int y = (int)(ImGui.GetIo().DisplaySize.Y - pcmd.ClipRect.W);
+                        int width = (int)(pcmd.ClipRect.Z - pcmd.ClipRect.X);
+                        int height = (int)(pcmd.ClipRect.W - pcmd.ClipRect.Y);
+                        Gl.GlScissor(x, y, width, height);
+
+                        Gl.GlDrawElements(PrimitiveType.Triangles, (int)pcmd.ElemCount, DrawElementsType.UnsignedShort, new IntPtr(idxOffset * sizeof(ushort)));
+                    }
+
+                    idxOffset += pcmd.ElemCount;
+                }
+            }
+
+            Gl.GlDisable(EnableCap.ScissorTest);
+            Gl.GlEnable(EnableCap.DepthTest);
+            Gl.GlDisable(EnableCap.Blend);
+            Gl.GlBindVertexArray(0);
+            Gl.GlUseProgram(0);
+        }
+
         public void Cleanup()
         {
+            if (_vbo != 0) Gl.DeleteBuffer(_vbo);
+            if (_ebo != 0) Gl.DeleteBuffer(_ebo);
+            if (_vao != 0) Gl.DeleteVertexArray(_vao);
+            if (_shaderProgram != 0) Gl.GlDeleteProgram(_shaderProgram);
+            if (_fontTexture != 0) Gl.DeleteTexture(_fontTexture);
 
+            ImGui.SetCurrentContext(new IntPtr());
+            // No DestroyContext wrapper; if exists use ImGui.DestroyContext
+        }
+
+        // Parameterless constructor: provided to avoid mismatches in some build contexts.
+        public ImguiSample()
+        {
+            _platform = null;
         }
     }
 }
