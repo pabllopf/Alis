ac0cbac1206ba882acf9912240e7ab40672bbc18
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2025-11-30 13:21:52 +0100

    Add font and icon assets, enhance ImGui setup
    Added JetBrains Mono and FontAwesome font files, as well as logo images to the assets directory. Updated Program.cs to initialize ImGui context, configure font and icon loading from memory, set up docking and viewport features, and add OpenGL texture loading for ImGui font atlas. These changes improve UI appearance and enable advanced ImGui features.

--- 1_Presentation/Extension/Graphic/Ui/sample/Program.cs
+++ 1_Presentation/Extension/Graphic/Ui/sample/Program.cs
@@ -28,10 +28,18 @@
 //  --------------------------------------------------------------------------
 
 using System;
+using System.IO;
+using System.Runtime.InteropServices;
 using Alis.Core.Aspect.Logging;
+using Alis.Core.Aspect.Math.Vector;
+using Alis.Core.Aspect.Memory;
 using Alis.Core.Graphic.OpenGL;
 using Alis.Core.Graphic.OpenGL.Enums;
 using Alis.Core.Graphic.Platforms;
+using Alis.Extension.Graphic.Ui.Extras.GuizMo;
+using Alis.Extension.Graphic.Ui.Extras.Node;
+using Alis.Extension.Graphic.Ui.Extras.Plot;
+using Alis.Extension.Graphic.Ui.Fonts;
 using Alis.Extension.Graphic.Ui.Sample.Examples;
 
 namespace Alis.Extension.Graphic.Ui.Sample
@@ -67,6 +75,9 @@ namespace Alis.Extension.Graphic.Ui.Sample
                 platform.Cleanup();
                 return;
             }
+            
+            IntPtr _context = ImGui.CreateContext();
+            ImGui.SetCurrentContext(_context);
 
             platform.MakeContextCurrent();
             Gl.Initialize(platform.GetProcAddress);
@@ -76,10 +87,93 @@ namespace Alis.Extension.Graphic.Ui.Sample
             // Crear el ejemplo aquí, después de que el contexto nativo y GL estén listos
             example = new ImguiSample(platform);
 
-            example.Initialize();
+            //example.Initialize();
             platform.ShowWindow();
             platform.SetTitle("C# + OpenGL Platform - ImGui");
+            
+            ImGuiIoPtr io = ImGui.GetIo();
+            
+            io.DisplaySize = new Vector2F(320, 320);
+            
+            Logger.Info($@"IMGUI VERSION {ImGui.GetVersion()}");
+            
+            io.BackendFlags |= ImGuiBackendFlags.RendererHasVtxOffset | ImGuiBackendFlags.PlatformHasViewports | ImGuiBackendFlags.HasGamepad | ImGuiBackendFlags.HasMouseHoveredViewport | ImGuiBackendFlags.HasMouseCursors;
 
+
+            // Enable Keyboard Controls
+            io.ConfigFlags |= ImGuiConfigFlags.NavEnableKeyboard;
+            io.ConfigFlags |= ImGuiConfigFlags.NavEnableGamepad;
+
+            // CONFIG DOCKSPACE 
+            io.ConfigFlags |= ImGuiConfigFlags.DockingEnable;
+            io.ConfigFlags |= ImGuiConfigFlags.ViewportsEnable;
+            
+            ImNodes.CreateContext();
+            ImPlot.CreateContext();
+            ImGuizMo.SetImGuiContext(_context);
+            ImGui.SetCurrentContext(_context);
+
+            // REBUILD ATLAS
+            ImFontAtlasPtr fonts = ImGui.GetIo().Fonts;
+            
+            int fontSize = 14;
+            int fontSizeIcon = 18;
+            
+            MemoryStream fontFileSolid = AssetRegistry.GetResourceMemoryStreamByName("JetBrainsMono-Bold.ttf");
+            IntPtr fontData = Marshal.AllocHGlobal((int) fontFileSolid.Length);
+            byte[] fontDataBytes = new byte[fontFileSolid.Length];
+            fontFileSolid.ReadExactly(fontDataBytes, 0, (int) fontFileSolid.Length);
+            Marshal.Copy(fontDataBytes, 0, fontData, (int) fontFileSolid.Length);
+            ImFontPtr fontLoaded16Solid = fonts.AddFontFromMemoryTtf(fontData, fontSize, fontSize);
+            
+            try
+            {
+                ImFontConfigPtr iconsConfig = ImGui.ImFontConfig();
+                iconsConfig.MergeMode = true;
+                iconsConfig.SnapH = true;
+                iconsConfig.GlyphMinAdvanceX = 18;
+
+                ushort[] iconRanges = new ushort[3];
+                iconRanges[0] = FontAwesome5.IconMin;
+                iconRanges[1] = FontAwesome5.IconMax;
+                iconRanges[2] = 0;
+
+                // Allocate GCHandle to pin IconRanges in memory
+                GCHandle iconRangesHandle = GCHandle.Alloc(iconRanges, GCHandleType.Pinned);
+
+                IntPtr rangePtr = iconRangesHandle.AddrOfPinnedObject();
+
+
+                MemoryStream fontAwesome = AssetRegistry.GetResourceMemoryStreamByName(FontAwesome5.NameLight);
+                IntPtr fontAwesomeData = Marshal.AllocHGlobal((int) fontAwesome.Length);
+                byte[] fontAwesomeDataBytes = new byte[fontAwesome.Length];
+                fontAwesome.ReadExactly(fontAwesomeDataBytes, 0, (int) fontAwesome.Length);
+                Marshal.Copy(fontAwesomeDataBytes, 0, fontAwesomeData, (int) fontAwesome.Length);
+                fonts.AddFontFromMemoryTtf(fontAwesomeData, fontSizeIcon, fontSizeIcon, iconsConfig, rangePtr);
+            }
+            catch (Exception e)
+            {
+                Logger.Exception(@$"ERROR, FONT ICONS NOT FOUND: {FontAwesome5.NameLight} {e.Message}");
+                return;
+            }
+            
+            fonts.GetTexDataAsRgba32(out IntPtr pixelData, out int width, out int height, out int _);
+            uint _fontTextureId = LoadTexture(pixelData, width, height);
+            fonts.TexId = (IntPtr) _fontTextureId;
+            fonts.ClearTexData();
+            
+            ImGuiViewportPtr viewport = ImGui.GetMainViewport();
+            
+            ImGuiStyle Style = ImGui.GetStyle();
+            ImGui.StyleColorsDark();
+            Style.WindowRounding = 0.0f;
+            Style.Colors2 = new Vector4F(0.00f, 0.00f, 0.00f, 1.00f);
+            
+            
+            uint _vboHandle = Gl.GenBuffer();
+            uint _elementsHandle = Gl.GenBuffer();
+            uint _vertexArrayObject = Gl.GenVertexArray();
+            
             bool running = true;
             while (running)
             {
@@ -88,7 +182,7 @@ namespace Alis.Extension.Graphic.Ui.Sample
                 {
                     Logger.Info($"Tecla pulsada: {key}");
                 }
-
+                
                 example.Draw();
                 platform.SwapBuffers();
                 int glError = Gl.GlGetError();
@@ -101,5 +195,29 @@ namespace Alis.Extension.Graphic.Ui.Sample
             example.Cleanup();
             platform.Cleanup();
         }
+        
+        
+        /// <summary>
+        ///     Loads the texture using the specified pixel data
+        /// </summary>
+        /// <param name="pixelData">The pixel data</param>
+        /// <param name="width">The width</param>
+        /// <param name="height">The height</param>
+        /// <param name="format">The format</param>
+        /// <param name="internalFormat">The internal format</param>
+        /// <returns>The texture id</returns>
+        private static uint LoadTexture(IntPtr pixelData, int width, int height, PixelFormat format = PixelFormat.Rgba, PixelInternalFormat internalFormat = PixelInternalFormat.Rgba)
+        {
+            uint textureId = Gl.GenTexture();
+            Gl.GlPixelStorei(StoreParameter.UnpackAlignment, 1);
+            Gl.GlBindTexture(TextureTarget.Texture2D, textureId);
+            Gl.GlTexImage2D(TextureTarget.Texture2D, 0, internalFormat, width, height, 0, format, PixelType.UnsignedByte, pixelData);
+            Gl.GlTexParameteri(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, TextureParameter.Linear);
+            Gl.GlTexParameteri(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, TextureParameter.Linear);
+            Gl.GlBindTexture(TextureTarget.Texture2D, 0);
+            return textureId;
+        }
     }
+    
+    
 }
