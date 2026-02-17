34852d6f66ee5908cf8b3569635aaefdbf3df714
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-02-17 09:17:17 +0000

    feature: Add Asteroid Android sample project
    Add a new Alis.Sample.Asteroid.Android sample: adds project file, Program.cs, Info.plist, app.manifest, app.ico and multiple game asset files (graphics, fonts, music, sounds). The new csproj includes build/publish settings (AOT, trimming, self-contained, macOS bundle target and dynamic plist generation) and analyzer/generator project references. Update alis.sln to register the new project. Also include a small defensive fix in GraphicManager.cs to only set the window icon when an icon is configured.

---
+++ 2_Application/Alis/sample/alis.sample.asteroid/android/Program.cs
@@ -0,0 +1,69 @@
+// --------------------------------------------------------------------------
+// 
+//                               █▀▀█ ░█─── ▀█▀ ░█▀▀▀█
+//                              ░█▄▄█ ░█─── ░█─ ─▀▀▀▄▄
+//                              ░█─░█ ░█▄▄█ ▄█▄ ░█▄▄▄█
+// 
+//  --------------------------------------------------------------------------
+//  File:Program.cs
+// 
+//  Author:Pablo Perdomo Falcón
+//  Web:https://www.pabllopf.dev/
+// 
+//  Copyright (c) 2021 GNU General Public License v3.0
+// 
+//  This program is free software:you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.If not, see <http://www.gnu.org/licenses/>.
+// 
+//  --------------------------------------------------------------------------
+
+using Alis.Core.Ecs.Systems;
+
+namespace Alis.Sample.Asteroid.Android
+{
+    /// <summary>
+    ///     The program class
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        ///     Main the args
+        /// </summary>
+        /// <param name="args">The args</param>
+        public static void Main(string[] args)
+        {
+            VideoGame
+                .Create()
+                .Settings(setting => setting
+                    .General(general => general
+                        .Name("Asteroids")
+                        .Author("Pablo Perdomo Falcón")
+                        .Description("Asteroids game")
+                        .License("GNU General Public License v3.0")
+                        .Icon("app.ico")
+                    )
+                    .Audio(audio => audio
+                        .Volume(0)
+                    )
+                    .Graphic(graphic => graphic
+                        .FrameRate(60)
+                    )
+                    .Physic(physic => physic
+                        .Debug(true)
+                        .Gravity(0.0f, -9.8f)
+                    )
+                )
+                .Run();
+        }
+    }
+}
