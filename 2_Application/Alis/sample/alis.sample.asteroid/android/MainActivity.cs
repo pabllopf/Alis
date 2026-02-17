d657824dfe17a3d55134e69ec59678278082da90
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-02-17 11:44:24 +0000

    fix: Switch MainActivity to OpenGL view; remove resources
    Replace the layout-based UI with a programmatic GLSurfaceView. MainActivity now creates a GlView and sets it as the content view; added GlView and TriangleRenderer (GLES20) to render a simple triangle, with shader compilation, buffer setup and compatibility hooks. Updated AndroidManifest: removed icon/roundIcon attributes and set a literal application label and activity theme. Removed activity_main layout, adaptive icon XMLs, all mipmap icon images, ic_launcher_background and strings.xml since resources are no longer used.

--- 2_Application/Alis/sample/alis.sample.asteroid/android/MainActivity.cs
+++ 2_Application/Alis/sample/alis.sample.asteroid/android/MainActivity.cs
@@ -1,13 +1,111 @@
-namespace Alis.Sample.Asteroid.Android;
+using Android.App;
+using Android.OS;
+using Android.Content;
+using Android.Opengl;
+using Java.Nio;
+using Android.Util;
 
-[Activity(Label = "@string/app_name", MainLauncher = true)]
-public class MainActivity : Activity
+namespace Alis.Sample.Asteroid.Android
 {
-    protected override void OnCreate(Bundle? savedInstanceState)
+    [Activity(Label = "Alis.Sample.Asteroid.Android", MainLauncher = true, Theme = "@android:style/Theme.NoTitleBar")]
+    public class MainActivity : Activity
     {
-        base.OnCreate(savedInstanceState);
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            var glView = new GlView(this);
+            SetContentView(glView);
+        }
+    }
 
-        // Set our view from the "main" layout resource
-        SetContentView(Resource.Layout.activity_main);
+    public class GlView : GLSurfaceView
+    {
+        public GlView(Context context) : base(context)
+        {
+            SetEGLContextClientVersion(2);
+            SetRenderer(new TriangleRenderer());
+        }
+    }
+
+    public class TriangleRenderer : Java.Lang.Object, GLSurfaceView.IRenderer
+    {
+        private FloatBuffer? vertexBuffer;
+        private int program;
+        private int positionHandle;
+        private static readonly float[] triangleCoords = {
+            0f, 0.5f,
+            -0.5f, -0.5f,
+            0.5f, -0.5f
+        };
+        private const int COORDS_PER_VERTEX = 2;
+        private static readonly string vertexShaderCode =
+            "attribute vec2 vPosition; void main() { gl_Position = vec4(vPosition, 0.0, 1.0); }";
+        private static readonly string fragmentShaderCode =
+            "precision mediump float; void main() { gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0); }";
+
+        public void OnSurfaceCreated(Java.Lang.Object gl, Java.Lang.Object config)
+        {
+            Log.Debug("AlisGL", "OnSurfaceCreated llamado");
+            ByteBuffer bb = ByteBuffer.AllocateDirect(triangleCoords.Length * 4);
+            bb.Order(ByteOrder.NativeOrder());
+            vertexBuffer = bb.AsFloatBuffer();
+            vertexBuffer.Put(triangleCoords);
+            vertexBuffer.Position(0);
+
+            int vertexShader = LoadShader(GLES20.GlVertexShader, vertexShaderCode);
+            int fragmentShader = LoadShader(GLES20.GlFragmentShader, fragmentShaderCode);
+
+            program = GLES20.GlCreateProgram();
+            GLES20.GlAttachShader(program, vertexShader);
+            GLES20.GlAttachShader(program, fragmentShader);
+            GLES20.GlLinkProgram(program);
+
+            positionHandle = GLES20.GlGetAttribLocation(program, "vPosition");
+        }
+
+        public void OnDrawFrame(Java.Lang.Object gl)
+        {
+            Log.Debug("AlisGL", "OnDrawFrame llamado");
+            if (vertexBuffer == null) {
+                Log.Error("AlisGL", "vertexBuffer es null");
+                return;
+            }
+            GLES20.GlClearColor(1f, 0f, 0f, 1f);
+            GLES20.GlClear(GLES20.GlColorBufferBit);
+            GLES20.GlUseProgram(program);
+            GLES20.GlEnableVertexAttribArray(positionHandle);
+            vertexBuffer.Position(0);
+            GLES20.GlVertexAttribPointer(positionHandle, COORDS_PER_VERTEX, GLES20.GlFloat, false, 0, vertexBuffer);
+            GLES20.GlDrawArrays(GLES20.GlTriangles, 0, 3);
+            GLES20.GlDisableVertexAttribArray(positionHandle);
+        }
+
+        public void OnSurfaceChanged(Java.Lang.Object gl, int width, int height)
+        {
+            Log.Debug("AlisGL", $"OnSurfaceChanged llamado: width={width}, height={height}");
+            GLES20.GlViewport(0, 0, width, height);
+        }
+
+        private int LoadShader(int type, string shaderCode)
+        {
+            int shader = GLES20.GlCreateShader(type);
+            GLES20.GlShaderSource(shader, shaderCode);
+            GLES20.GlCompileShader(shader);
+            return shader;
+        }
+
+        // Métodos para compatibilidad con IGL10/EGLConfig
+        public void OnDrawFrame(Javax.Microedition.Khronos.Opengles.IGL10? gl)
+        {
+            OnDrawFrame((Java.Lang.Object)null!);
+        }
+        public void OnSurfaceChanged(Javax.Microedition.Khronos.Opengles.IGL10? gl, int width, int height)
+        {
+            OnSurfaceChanged((Java.Lang.Object)null!, width, height);
+        }
+        public void OnSurfaceCreated(Javax.Microedition.Khronos.Opengles.IGL10? gl, Javax.Microedition.Khronos.Egl.EGLConfig? config)
+        {
+            OnSurfaceCreated((Java.Lang.Object)null!, (Java.Lang.Object)null!);
+        }
     }
 }
