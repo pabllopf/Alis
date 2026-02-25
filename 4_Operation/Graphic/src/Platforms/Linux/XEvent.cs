5e31aadb0d9b29328ebb7b2a92ff5d7b95754c4d
GabrielRT01 <gabrirot12@gmail.com>
2026-02-25 00:00:00 +0000

    release: new version control

--- 4_Operation/Graphic/src/Platforms/Linux/XEvent.cs
+++ 4_Operation/Graphic/src/Platforms/Linux/XEvent.cs
@@ -34,6 +34,7 @@ using System.Runtime.InteropServices;
 using Alis.Core.Aspect.Logging;
 using System.Diagnostics.CodeAnalysis;
 
+
 namespace Alis.Core.Graphic.Platforms.Linux
 {
     /// <summary>
