5d9df847f518b62885956fe671fc4209576ccb7a
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-03-07 14:44:41 +0000

    test: pro chgeck all

--- 1_Presentation/Extension/Payment/Stripe/src/CheckoutSessionResult.cs
+++ 1_Presentation/Extension/Payment/Stripe/src/CheckoutSessionResult.cs
@@ -7,6 +7,7 @@ namespace Alis.Extension.Payment.Stripe
     /// </summary>
     public class CheckoutSessionResult
     {
+        
         /// <summary>
         /// Gets or sets the value of the session id
         /// </summary>
