4ecf20bedaf450760f9a4ada40db9fdaed0d5a98
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-03-09 19:52:56 +0000

    fix: Consolidate RefTuple types into RefTuple.cs
    Delete separate RefTuple.2..RefTuple.8.cs files and merge all RefTuple generic ref-struct variants into RefTuple.cs. Added System.Runtime.InteropServices import and applied [StructLayout(LayoutKind.Sequential, Pack = 1)] where appropriate. Each RefTuple<T1..T8> now defines ItemN fields and Deconstruct methods (using Ref<T> from Alis.Core.Ecs.Kernel). This centralizes tuple definitions to simplify maintenance and reduce file fragmentation.

--- 4_Operation/Ecs/src/Systems/RefTuple.cs
+++ 4_Operation/Ecs/src/Systems/RefTuple.cs
@@ -28,6 +28,7 @@
 //  --------------------------------------------------------------------------
 
 using System;
+using System.Runtime.InteropServices;
 using Alis.Core.Ecs.Kernel;
 
 namespace Alis.Core.Ecs.Systems
@@ -37,6 +38,7 @@ namespace Alis.Core.Ecs.Systems
     /// <summary>
     ///     A tuple of multiple references.
     /// </summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
     public ref struct RefTuple<T>
     {
         /// <summary>
@@ -50,6 +52,318 @@ namespace Alis.Core.Ecs.Systems
         public void Deconstruct(out Ref<T> @ref)
         {
             @ref = Item1;
+        }
+    }
+    
+    /// <summary>
+    ///     The ref tuple
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public ref struct RefTuple<T1, T2>
+    {
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T1> Item1;
+
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T2> Item2;
+
+
+        /// <summary>
+        ///     Allows tuple deconstruction syntax to be used.
+        /// </summary>
+        public void Deconstruct(out Ref<T1> ref1, out Ref<T2> ref2)
+        {
+            ref1 = Item1;
+            ref2 = Item2;
+        }
+    }
+    
+    /// <summary>
+    ///     The ref tuple
+    /// </summary>
+    public ref struct RefTuple<T1, T2, T3>
+    {
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T1> Item1;
+
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T2> Item2;
+
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T3> Item3;
+
+
+        /// <summary>
+        ///     Allows tuple deconstruction syntax to be used.
+        /// </summary>
+        public void Deconstruct(out Ref<T1> ref1, out Ref<T2> ref2, out Ref<T3> ref3)
+        {
+            ref1 = Item1;
+            ref2 = Item2;
+            ref3 = Item3;
+        }
+    }
+    
+    /// <summary>
+    ///     The ref tuple
+    /// </summary>
+    public ref struct RefTuple<T1, T2, T3, T4>
+    {
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T1> Item1;
+
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T2> Item2;
+
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T3> Item3;
+
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T4> Item4;
+
+
+        /// <summary>
+        ///     Allows tuple deconstruction syntax to be used.
+        /// </summary>
+        public void Deconstruct(out Ref<T1> ref1, out Ref<T2> ref2, out Ref<T3> ref3, out Ref<T4> ref4)
+        {
+            ref1 = Item1;
+            ref2 = Item2;
+            ref3 = Item3;
+            ref4 = Item4;
+        }
+    }
+    
+    /// <summary>
+    ///     The ref tuple
+    /// </summary>
+    public ref struct RefTuple<T1, T2, T3, T4, T5>
+    {
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T1> Item1;
+
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T2> Item2;
+
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T3> Item3;
+
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T4> Item4;
+
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T5> Item5;
+
+
+        /// <summary>
+        ///     Allows tuple deconstruction syntax to be used.
+        /// </summary>
+        public void Deconstruct(out Ref<T1> ref1, out Ref<T2> ref2, out Ref<T3> ref3, out Ref<T4> ref4, out Ref<T5> ref5)
+        {
+            ref1 = Item1;
+            ref2 = Item2;
+            ref3 = Item3;
+            ref4 = Item4;
+            ref5 = Item5;
+        }
+    }
+    
+    /// <summary>
+    ///     The ref tuple
+    /// </summary>
+    public ref struct RefTuple<T1, T2, T3, T4, T5, T6>
+    {
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T1> Item1;
+
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T2> Item2;
+
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T3> Item3;
+
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T4> Item4;
+
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T5> Item5;
+
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T6> Item6;
+
+
+        /// <summary>
+        ///     Allows tuple deconstruction syntax to be used.
+        /// </summary>
+        public void Deconstruct(out Ref<T1> ref1, out Ref<T2> ref2, out Ref<T3> ref3, out Ref<T4> ref4, out Ref<T5> ref5,
+            out Ref<T6> ref6)
+        {
+            ref1 = Item1;
+            ref2 = Item2;
+            ref3 = Item3;
+            ref4 = Item4;
+            ref5 = Item5;
+            ref6 = Item6;
+        }
+    }
+    
+    /// <summary>
+    ///     The ref tuple
+    /// </summary>
+    public ref struct RefTuple<T1, T2, T3, T4, T5, T6, T7>
+    {
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T1> Item1;
+
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T2> Item2;
+
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T3> Item3;
+
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T4> Item4;
+
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T5> Item5;
+
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T6> Item6;
+
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T7> Item7;
+
+
+        /// <summary>
+        ///     Allows tuple deconstruction syntax to be used.
+        /// </summary>
+        public void Deconstruct(out Ref<T1> ref1, out Ref<T2> ref2, out Ref<T3> ref3, out Ref<T4> ref4, out Ref<T5> ref5,
+            out Ref<T6> ref6, out Ref<T7> ref7)
+        {
+            ref1 = Item1;
+            ref2 = Item2;
+            ref3 = Item3;
+            ref4 = Item4;
+            ref5 = Item5;
+            ref6 = Item6;
+            ref7 = Item7;
+        }
+    }
+    
+    /// <summary>
+    ///     The ref tuple
+    /// </summary>
+    public ref struct RefTuple<T1, T2, T3, T4, T5, T6, T7, T8>
+    {
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T1> Item1;
+
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T2> Item2;
+
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T3> Item3;
+
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T4> Item4;
+
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T5> Item5;
+
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T6> Item6;
+
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T7> Item7;
+
+        /// <summary>
+        ///     The item
+        /// </summary>
+        public Ref<T8> Item8;
+
+
+        /// <summary>
+        ///     Allows tuple deconstruction syntax to be used.
+        /// </summary>
+        public void Deconstruct(out Ref<T1> ref1, out Ref<T2> ref2, out Ref<T3> ref3, out Ref<T4> ref4, out Ref<T5> ref5,
+            out Ref<T6> ref6, out Ref<T7> ref7, out Ref<T8> ref8)
+        {
+            ref1 = Item1;
+            ref2 = Item2;
+            ref3 = Item3;
+            ref4 = Item4;
+            ref5 = Item5;
+            ref6 = Item6;
+            ref7 = Item7;
+            ref8 = Item8;
         }
     }
 }
