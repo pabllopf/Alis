c5c09650bc0944cad3468183e9561020ba7d12c1
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-03-09 17:40:50 +0000

    fix: Merge ChunkQueryEnumerator variants into one file
    Remove separate ChunkQueryEnumerator.{2..8}.cs files and consolidate all generic arities (T1..T8) into a single ChunkQueryEnumerator.cs. Adds System.Runtime.InteropServices import and StructLayout attributes for QueryEnumerable proxies. This simplifies maintenance by centralizing the enumerator implementations while preserving existing behavior (scene Enter/ExitDisallowState, Current/MoveNext/Dispose) and removes the fragmented per-arity files.

--- 4_Operation/Ecs/src/Systems/ChunkQueryEnumerator.cs
+++ 4_Operation/Ecs/src/Systems/ChunkQueryEnumerator.cs
@@ -28,6 +28,7 @@
 //  --------------------------------------------------------------------------
 
 using System;
+using System.Runtime.InteropServices;
 using Alis.Core.Ecs.Kernel.Archetypes;
 
 namespace Alis.Core.Ecs.Systems
@@ -97,12 +98,564 @@ namespace Alis.Core.Ecs.Systems
         ///     Proxy type for foreach syntax
         /// </summary>
         /// <param name="query">The query to wrap.</param>
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
         public readonly struct QueryEnumerable(Query query)
         {
             /// <summary>
             ///     Gets the enumerator over a query.
             /// </summary>
             public ChunkQueryEnumerator<T> GetEnumerator() => new ChunkQueryEnumerator<T>(query);
+        }
+    }
+    
+      /// <summary>
+    ///     The chunk query enumerator
+    /// </summary>
+    public ref struct ChunkQueryEnumerator<T1, T2>
+    {
+        /// <summary>
+        ///     The scene
+        /// </summary>
+        private readonly Scene _scene;
+
+        /// <summary>
+        ///     The archetypes
+        /// </summary>
+        private readonly Span<Archetype> _archetypes;
+
+        /// <summary>
+        ///     The archetype index
+        /// </summary>
+        private int _archetypeIndex;
+
+        /// <summary>
+        ///     Initializes a new instance of the  class
+        /// </summary>
+        /// <param name="query">The query</param>
+        private ChunkQueryEnumerator(Query query)
+        {
+            _scene = query.Scene;
+            _scene.EnterDisallowState();
+            _archetypes = query.AsSpan();
+            _archetypeIndex = -1;
+        }
+
+        /// <summary>
+        ///     The current tuple of component chunks.
+        /// </summary>
+        public ChunkTuple<T1, T2> Current
+        {
+            get
+            {
+                Archetype cur = _archetypes[_archetypeIndex];
+                return new ChunkTuple<T1, T2>
+                {
+                    Span1 = cur.GetComponentSpan<T1>(),
+                    Span2 = cur.GetComponentSpan<T2>()
+                };
+            }
+        }
+
+        /// <summary>
+        ///     Indicates to the scene that this enumeration is finished; the scene might allow structual changes after this.
+        /// </summary>
+        public void Dispose()
+        {
+            _scene.ExitDisallowState(null);
+        }
+
+        /// <summary>
+        ///     Moves to the next chunk of components in this enumeration.
+        /// </summary>
+        /// <returns><see langword="true" /> when its possible to enumerate further, otherwise <see langword="false" />.</returns>
+        public bool MoveNext() => ++_archetypeIndex < _archetypes.Length;
+
+        /// <summary>
+        ///     Proxy type for foreach syntax
+        /// </summary>
+        /// <param name="query">The query to wrap.</param>
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public readonly struct QueryEnumerable(Query query)
+        {
+            /// <summary>
+            ///     Gets the enumerator over a query.
+            /// </summary>
+            public ChunkQueryEnumerator<T1, T2> GetEnumerator() => new ChunkQueryEnumerator<T1, T2>(query);
+        }
+    }
+      
+          /// <summary>
+    ///     The chunk query enumerator
+    /// </summary>
+    public ref struct ChunkQueryEnumerator<T1, T2, T3>
+    {
+        /// <summary>
+        ///     The scene
+        /// </summary>
+        private readonly Scene _scene;
+
+        /// <summary>
+        ///     The archetypes
+        /// </summary>
+        private readonly Span<Archetype> _archetypes;
+
+        /// <summary>
+        ///     The archetype index
+        /// </summary>
+        private int _archetypeIndex;
+
+        /// <summary>
+        ///     Initializes a new instance of the  class
+        /// </summary>
+        /// <param name="query">The query</param>
+        private ChunkQueryEnumerator(Query query)
+        {
+            _scene = query.Scene;
+            _scene.EnterDisallowState();
+            _archetypes = query.AsSpan();
+            _archetypeIndex = -1;
+        }
+
+        /// <summary>
+        ///     The current tuple of component chunks.
+        /// </summary>
+        public ChunkTuple<T1, T2, T3> Current
+        {
+            get
+            {
+                Archetype cur = _archetypes[_archetypeIndex];
+                return new ChunkTuple<T1, T2, T3>
+                {
+                    Span1 = cur.GetComponentSpan<T1>(),
+                    Span2 = cur.GetComponentSpan<T2>(),
+                    Span3 = cur.GetComponentSpan<T3>()
+                };
+            }
+        }
+
+        /// <summary>
+        ///     Indicates to the scene that this enumeration is finished; the scene might allow structual changes after this.
+        /// </summary>
+        public void Dispose()
+        {
+            _scene.ExitDisallowState(null);
+        }
+
+        /// <summary>
+        ///     Moves to the next chunk of components in this enumeration.
+        /// </summary>
+        /// <returns><see langword="true" /> when its possible to enumerate further, otherwise <see langword="false" />.</returns>
+        public bool MoveNext() => ++_archetypeIndex < _archetypes.Length;
+
+        /// <summary>
+        ///     Proxy type for foreach syntax
+        /// </summary>
+        /// <param name="query">The query to wrap.</param>
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public readonly struct QueryEnumerable(Query query)
+        {
+            /// <summary>
+            ///     Gets the enumerator over a query.
+            /// </summary>
+            public ChunkQueryEnumerator<T1, T2, T3> GetEnumerator() => new ChunkQueryEnumerator<T1, T2, T3>(query);
+        }
+    }
+          
+           /// <summary>
+    ///     The chunk query enumerator
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public ref struct ChunkQueryEnumerator<T1, T2, T3, T4>
+    {
+        /// <summary>
+        ///     The scene
+        /// </summary>
+        private readonly Scene _scene;
+
+        /// <summary>
+        ///     The archetypes
+        /// </summary>
+        private readonly Span<Archetype> _archetypes;
+
+        /// <summary>
+        ///     The archetype index
+        /// </summary>
+        private int _archetypeIndex;
+
+        /// <summary>
+        ///     Initializes a new instance of the  class
+        /// </summary>
+        /// <param name="query">The query</param>
+        private ChunkQueryEnumerator(Query query)
+        {
+            _scene = query.Scene;
+            _scene.EnterDisallowState();
+            _archetypes = query.AsSpan();
+            _archetypeIndex = -1;
+        }
+
+        /// <summary>
+        ///     The current tuple of component chunks.
+        /// </summary>
+        public ChunkTuple<T1, T2, T3, T4> Current
+        {
+            get
+            {
+                Archetype cur = _archetypes[_archetypeIndex];
+                return new ChunkTuple<T1, T2, T3, T4>
+                {
+                    Span1 = cur.GetComponentSpan<T1>(),
+                    Span2 = cur.GetComponentSpan<T2>(),
+                    Span3 = cur.GetComponentSpan<T3>(),
+                    Span4 = cur.GetComponentSpan<T4>()
+                };
+            }
+        }
+
+        /// <summary>
+        ///     Indicates to the scene that this enumeration is finished; the scene might allow structual changes after this.
+        /// </summary>
+        public void Dispose()
+        {
+            _scene.ExitDisallowState(null);
+        }
+
+        /// <summary>
+        ///     Moves to the next chunk of components in this enumeration.
+        /// </summary>
+        /// <returns><see langword="true" /> when its possible to enumerate further, otherwise <see langword="false" />.</returns>
+        public bool MoveNext() => ++_archetypeIndex < _archetypes.Length;
+
+        /// <summary>
+        ///     Proxy type for foreach syntax
+        /// </summary>
+        /// <param name="query">The query to wrap.</param>
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public readonly struct QueryEnumerable(Query query)
+        {
+            /// <summary>
+            ///     Gets the enumerator over a query.
+            /// </summary>
+            public ChunkQueryEnumerator<T1, T2, T3, T4> GetEnumerator() => new ChunkQueryEnumerator<T1, T2, T3, T4>(query);
+        }
+    }
+           
+               /// <summary>
+    ///     The chunk query enumerator
+    /// </summary>
+    public ref struct ChunkQueryEnumerator<T1, T2, T3, T4, T5>
+    {
+        /// <summary>
+        ///     The scene
+        /// </summary>
+        private readonly Scene _scene;
+
+        /// <summary>
+        ///     The archetypes
+        /// </summary>
+        private readonly Span<Archetype> _archetypes;
+
+        /// <summary>
+        ///     The archetype index
+        /// </summary>
+        private int _archetypeIndex;
+
+        /// <summary>
+        ///     Initializes a new instance of the  class
+        /// </summary>
+        /// <param name="query">The query</param>
+        private ChunkQueryEnumerator(Query query)
+        {
+            _scene = query.Scene;
+            _scene.EnterDisallowState();
+            _archetypes = query.AsSpan();
+            _archetypeIndex = -1;
+        }
+
+        /// <summary>
+        ///     The current tuple of component chunks.
+        /// </summary>
+        public ChunkTuple<T1, T2, T3, T4, T5> Current
+        {
+            get
+            {
+                Archetype cur = _archetypes[_archetypeIndex];
+                return new ChunkTuple<T1, T2, T3, T4, T5>
+                {
+                    Span1 = cur.GetComponentSpan<T1>(),
+                    Span2 = cur.GetComponentSpan<T2>(),
+                    Span3 = cur.GetComponentSpan<T3>(),
+                    Span4 = cur.GetComponentSpan<T4>(),
+                    Span5 = cur.GetComponentSpan<T5>()
+                };
+            }
+        }
+
+        /// <summary>
+        ///     Indicates to the scene that this enumeration is finished; the scene might allow structual changes after this.
+        /// </summary>
+        public void Dispose()
+        {
+            _scene.ExitDisallowState(null);
+        }
+
+        /// <summary>
+        ///     Moves to the next chunk of components in this enumeration.
+        /// </summary>
+        /// <returns><see langword="true" /> when its possible to enumerate further, otherwise <see langword="false" />.</returns>
+        public bool MoveNext() => ++_archetypeIndex < _archetypes.Length;
+
+        /// <summary>
+        ///     Proxy type for foreach syntax
+        /// </summary>
+        /// <param name="query">The query to wrap.</param>
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public readonly struct QueryEnumerable(Query query)
+        {
+            /// <summary>
+            ///     Gets the enumerator over a query.
+            /// </summary>
+            public ChunkQueryEnumerator<T1, T2, T3, T4, T5> GetEnumerator() => new ChunkQueryEnumerator<T1, T2, T3, T4, T5>(query);
+        }
+    }
+               
+                   /// <summary>
+    ///     The chunk query enumerator
+    /// </summary>
+    public ref struct ChunkQueryEnumerator<T1, T2, T3, T4, T5, T6>
+    {
+        /// <summary>
+        ///     The scene
+        /// </summary>
+        private readonly Scene _scene;
+
+        /// <summary>
+        ///     The archetypes
+        /// </summary>
+        private readonly Span<Archetype> _archetypes;
+
+        /// <summary>
+        ///     The archetype index
+        /// </summary>
+        private int _archetypeIndex;
+
+        /// <summary>
+        ///     Initializes a new instance of the  class
+        /// </summary>
+        /// <param name="query">The query</param>
+        private ChunkQueryEnumerator(Query query)
+        {
+            _scene = query.Scene;
+            _scene.EnterDisallowState();
+            _archetypes = query.AsSpan();
+            _archetypeIndex = -1;
+        }
+
+        /// <summary>
+        ///     The current tuple of component chunks.
+        /// </summary>
+        public ChunkTuple<T1, T2, T3, T4, T5, T6> Current
+        {
+            get
+            {
+                Archetype cur = _archetypes[_archetypeIndex];
+                return new ChunkTuple<T1, T2, T3, T4, T5, T6>
+                {
+                    Span1 = cur.GetComponentSpan<T1>(),
+                    Span2 = cur.GetComponentSpan<T2>(),
+                    Span3 = cur.GetComponentSpan<T3>(),
+                    Span4 = cur.GetComponentSpan<T4>(),
+                    Span5 = cur.GetComponentSpan<T5>(),
+                    Span6 = cur.GetComponentSpan<T6>()
+                };
+            }
+        }
+
+        /// <summary>
+        ///     Indicates to the scene that this enumeration is finished; the scene might allow structual changes after this.
+        /// </summary>
+        public void Dispose()
+        {
+            _scene.ExitDisallowState(null);
+        }
+
+        /// <summary>
+        ///     Moves to the next chunk of components in this enumeration.
+        /// </summary>
+        /// <returns><see langword="true" /> when its possible to enumerate further, otherwise <see langword="false" />.</returns>
+        public bool MoveNext() => ++_archetypeIndex < _archetypes.Length;
+
+        /// <summary>
+        ///     Proxy type for foreach syntax
+        /// </summary>
+        /// <param name="query">The query to wrap.</param>
+        public readonly struct QueryEnumerable(Query query)
+        {
+            /// <summary>
+            ///     Gets the enumerator over a query.
+            /// </summary>
+            public ChunkQueryEnumerator<T1, T2, T3, T4, T5, T6> GetEnumerator() => new ChunkQueryEnumerator<T1, T2, T3, T4, T5, T6>(query);
+        }
+    }
+                   
+                    /// <summary>
+    ///     The chunk query enumerator
+    /// </summary>
+    public ref struct ChunkQueryEnumerator<T1, T2, T3, T4, T5, T6, T7>
+    {
+        /// <summary>
+        ///     The scene
+        /// </summary>
+        private readonly Scene _scene;
+
+        /// <summary>
+        ///     The archetypes
+        /// </summary>
+        private readonly Span<Archetype> _archetypes;
+
+        /// <summary>
+        ///     The archetype index
+        /// </summary>
+        private int _archetypeIndex;
+
+        /// <summary>
+        ///     Initializes a new instance of the  class
+        /// </summary>
+        /// <param name="query">The query</param>
+        private ChunkQueryEnumerator(Query query)
+        {
+            _scene = query.Scene;
+            _scene.EnterDisallowState();
+            _archetypes = query.AsSpan();
+            _archetypeIndex = -1;
+        }
+
+        /// <summary>
+        ///     The current tuple of component chunks.
+        /// </summary>
+        public ChunkTuple<T1, T2, T3, T4, T5, T6, T7> Current
+        {
+            get
+            {
+                Archetype cur = _archetypes[_archetypeIndex];
+                return new ChunkTuple<T1, T2, T3, T4, T5, T6, T7>
+                {
+                    Span1 = cur.GetComponentSpan<T1>(),
+                    Span2 = cur.GetComponentSpan<T2>(),
+                    Span3 = cur.GetComponentSpan<T3>(),
+                    Span4 = cur.GetComponentSpan<T4>(),
+                    Span5 = cur.GetComponentSpan<T5>(),
+                    Span6 = cur.GetComponentSpan<T6>(),
+                    Span7 = cur.GetComponentSpan<T7>()
+                };
+            }
+        }
+
+        /// <summary>
+        ///     Indicates to the scene that this enumeration is finished; the scene might allow structual changes after this.
+        /// </summary>
+        public void Dispose()
+        {
+            _scene.ExitDisallowState(null);
+        }
+
+        /// <summary>
+        ///     Moves to the next chunk of components in this enumeration.
+        /// </summary>
+        /// <returns><see langword="true" /> when its possible to enumerate further, otherwise <see langword="false" />.</returns>
+        public bool MoveNext() => ++_archetypeIndex < _archetypes.Length;
+
+        /// <summary>
+        ///     Proxy type for foreach syntax
+        /// </summary>
+        /// <param name="query">The query to wrap.</param>
+        public readonly struct QueryEnumerable(Query query)
+        {
+            /// <summary>
+            ///     Gets the enumerator over a query.
+            /// </summary>
+            public ChunkQueryEnumerator<T1, T2, T3, T4, T5, T6, T7> GetEnumerator() => new ChunkQueryEnumerator<T1, T2, T3, T4, T5, T6, T7>(query);
+        }
+    }
+                    
+                     /// <summary>
+    ///     The chunk query enumerator
+    /// </summary>
+    public ref struct ChunkQueryEnumerator<T1, T2, T3, T4, T5, T6, T7, T8>
+    {
+        /// <summary>
+        ///     The scene
+        /// </summary>
+        private readonly Scene _scene;
+
+        /// <summary>
+        ///     The archetypes
+        /// </summary>
+        private readonly Span<Archetype> _archetypes;
+
+        /// <summary>
+        ///     The archetype index
+        /// </summary>
+        private int _archetypeIndex;
+
+        /// <summary>
+        ///     Initializes a new instance of the  class
+        /// </summary>
+        /// <param name="query">The query</param>
+        private ChunkQueryEnumerator(Query query)
+        {
+            _scene = query.Scene;
+            _scene.EnterDisallowState();
+            _archetypes = query.AsSpan();
+            _archetypeIndex = -1;
+        }
+
+        /// <summary>
+        ///     The current tuple of component chunks.
+        /// </summary>
+        public ChunkTuple<T1, T2, T3, T4, T5, T6, T7, T8> Current
+        {
+            get
+            {
+                Archetype cur = _archetypes[_archetypeIndex];
+                return new ChunkTuple<T1, T2, T3, T4, T5, T6, T7, T8>
+                {
+                    Span1 = cur.GetComponentSpan<T1>(),
+                    Span2 = cur.GetComponentSpan<T2>(),
+                    Span3 = cur.GetComponentSpan<T3>(),
+                    Span4 = cur.GetComponentSpan<T4>(),
+                    Span5 = cur.GetComponentSpan<T5>(),
+                    Span6 = cur.GetComponentSpan<T6>(),
+                    Span7 = cur.GetComponentSpan<T7>(),
+                    Span8 = cur.GetComponentSpan<T8>()
+                };
+            }
+        }
+
+        /// <summary>
+        ///     Indicates to the scene that this enumeration is finished; the scene might allow structual changes after this.
+        /// </summary>
+        public void Dispose()
+        {
+            _scene.ExitDisallowState(null);
+        }
+
+        /// <summary>
+        ///     Moves to the next chunk of components in this enumeration.
+        /// </summary>
+        /// <returns><see langword="true" /> when its possible to enumerate further, otherwise <see langword="false" />.</returns>
+        public bool MoveNext() => ++_archetypeIndex < _archetypes.Length;
+
+        /// <summary>
+        ///     Proxy type for foreach syntax
+        /// </summary>
+        /// <param name="query">The query to wrap.</param>
+        public readonly struct QueryEnumerable(Query query)
+        {
+            /// <summary>
+            ///     Gets the enumerator over a query.
+            /// </summary>
+            public ChunkQueryEnumerator<T1, T2, T3, T4, T5, T6, T7, T8> GetEnumerator() => new ChunkQueryEnumerator<T1, T2, T3, T4, T5, T6, T7, T8>(query);
         }
     }
 }
