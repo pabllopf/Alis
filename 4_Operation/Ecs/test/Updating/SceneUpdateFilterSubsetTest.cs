de14f476364ee5d7cafa1b5a13ee9fbb364468c0
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-03-16 21:17:35 +0000

    Update SceneUpdateFilterSubsetTest.cs

--- 4_Operation/Ecs/test/Updating/SceneUpdateFilterSubsetTest.cs
+++ 4_Operation/Ecs/test/Updating/SceneUpdateFilterSubsetTest.cs
@@ -1,34 +1,52 @@
 // --------------------------------------------------------------------------
-//
+// 
 //                               █▀▀█ ░█─── ▀█▀ ░█▀▀▀█
 //                              ░█▄▄█ ░█─── ░█─ ─▀▀▀▄▄
 //                              ░█─░█ ░█▄▄█ ▄█▄ ░█▄▄▄█
-//
+// 
 //  --------------------------------------------------------------------------
 //  File:SceneUpdateFilterSubsetTest.cs
-//
+// 
+//  Author:Pablo Perdomo Falcón
+//  Web:https://www.pabllopf.dev/
+// 
+//  Copyright (c) 2021 GNU General Public License v3.0
+// 
+//  This program is free software:you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.If not, see <http://www.gnu.org/licenses/>.
+// 
 //  --------------------------------------------------------------------------
 
+using System;
 using Alis.Core.Ecs.Test.Models;
 using Alis.Core.Ecs.Test.Updating.Runners;
 using Alis.Core.Ecs.Updating;
-using System;
 using Xunit;
 
 namespace Alis.Core.Ecs.Test.Updating
 {
     /// <summary>
-    /// Focused tests for SceneUpdateFilter.Update and SceneUpdateFilter.UpdateSubset paths.
+    ///     Focused tests for SceneUpdateFilter.Update and SceneUpdateFilter.UpdateSubset paths.
     /// </summary>
     public class SceneUpdateFilterSubsetTest
     {
         /// <summary>
-        /// The scene update filter subset attribute
+        ///     The scene update filter subset attribute
         /// </summary>
         private static readonly Type FilterMarkerType = typeof(SceneUpdateFilterSubsetAttribute);
 
         /// <summary>
-        /// Tests that update processes all matching archetypes
+        ///     Tests that update processes all matching archetypes
         /// </summary>
         [Fact]
         public void Update_ProcessesAllMatchingArchetypes()
@@ -48,7 +66,7 @@ namespace Alis.Core.Ecs.Test.Updating
         }
 
         /// <summary>
-        /// Tests that update subset when resolving deferred creation updates only new entities
+        ///     Tests that update subset when resolving deferred creation updates only new entities
         /// </summary>
         [Fact]
         public void UpdateSubset_WhenResolvingDeferredCreation_UpdatesOnlyNewEntities()
@@ -60,7 +78,7 @@ namespace Alis.Core.Ecs.Test.Updating
 
             scene.EnterDisallowState();
             GameObject deferred = scene.Create(new UpdateComponent {CallCount = 0});
-            scene.ExitDisallowState(filter, updateDeferredEntities: true);
+            scene.ExitDisallowState(filter, true);
 
             // ExitDisallowState(..., true) routes deferred records through SceneUpdateFilter.UpdateSubset.
             Assert.Equal(0, existing.Get<UpdateComponent>().CallCount);
@@ -68,13 +86,11 @@ namespace Alis.Core.Ecs.Test.Updating
         }
 
         /// <summary>
-        /// The scene update filter subset attribute class
+        ///     The scene update filter subset attribute class
         /// </summary>
-        /// <seealso cref="Attribute"/>
+        /// <seealso cref="Attribute" />
         private sealed class SceneUpdateFilterSubsetAttribute : Attribute
         {
         }
     }
 }
-
-
