e50b6e4d8873432eadee672c2455f32e18adf92e
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-03-16 20:56:32 +0000

    Update EnsureCapacitySample.cs

--- 4_Operation/Ecs/sample/Samples/EnsureCapacitySample.cs
+++ 4_Operation/Ecs/sample/Samples/EnsureCapacitySample.cs
@@ -1,30 +1,59 @@
+// --------------------------------------------------------------------------
+// 
+//                               █▀▀█ ░█─── ▀█▀ ░█▀▀▀█
+//                              ░█▄▄█ ░█─── ░█─ ─▀▀▀▄▄
+//                              ░█─░█ ░█▄▄█ ▄█▄ ░█▄▄▄█
+// 
+//  --------------------------------------------------------------------------
+//  File:EnsureCapacitySample.cs
+// 
+//  Author:Pablo Perdomo Falcón
+//  Web:https://www.pabllopf.dev/
+// 
+//  Copyright (c) 2021 GNU General Public License v3.0
+// 
+//  This program is free software:you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.If not, see <http://www.gnu.org/licenses/>.
+// 
+//  --------------------------------------------------------------------------
+
 using System;
 
 namespace Alis.Core.Ecs.Sample.Samples
 {
     /// <summary>
-    /// The ensure capacity sample class
+    ///     The ensure capacity sample class
     /// </summary>
-    /// <seealso cref="IEcsSample"/>
+    /// <seealso cref="IEcsSample" />
     internal sealed class EnsureCapacitySample : IEcsSample
     {
         /// <summary>
-        /// Gets the value of the key
+        ///     Gets the value of the key
         /// </summary>
         public string Key => "ensure-capacity";
 
         /// <summary>
-        /// Gets the value of the title
+        ///     Gets the value of the title
         /// </summary>
         public string Title => "Ensure Capacity";
 
         /// <summary>
-        /// Gets the value of the description
+        ///     Gets the value of the description
         /// </summary>
         public string Description => "Preallocates archetype capacity before bulk creation.";
 
         /// <summary>
-        /// Runs this instance
+        ///     Runs this instance
         /// </summary>
         public void Run()
         {
@@ -43,4 +72,3 @@ namespace Alis.Core.Ecs.Sample.Samples
         }
     }
 }
-
