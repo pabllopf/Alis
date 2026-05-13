8612731e6157bcd54d8e23d5c01cb3495ff3e260
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-05-13 13:47:29 +0100

    Revert "Update OpenGL delegates - comments & interop"
    This reverts commit ab6036c70eb86d369bb78ccb921d6c3801eb715b.

--- 4_Operation/Graphic/src/OpenGL/Delegates/LinkProgram.cs
+++ 4_Operation/Graphic/src/OpenGL/Delegates/LinkProgram.cs
@@ -12,14 +12,28 @@
 // 
 //  Copyright (c) 2021 GNU General Public License v3.0
 // 
+//  This program is free software:you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.If not, see <http://www.gnu.org/licenses/>.
+// 
 //  --------------------------------------------------------------------------
+
+using System.Runtime.InteropServices;
 
 namespace Alis.Core.Graphic.OpenGL.Delegates
 {
     /// <summary>
-    /// Represents the unmanaged function pointer for the OpenGL glLinkProgram command.
-    /// Links a program object, creating executable shaders for the programmable pipeline.
+    ///     The link program
     /// </summary>
-    /// <param name="program">The program object to link.</param>
+    [UnmanagedFunctionPointer(CallingConvention.StdCall)]
     public delegate void LinkProgram(uint program);
 }
