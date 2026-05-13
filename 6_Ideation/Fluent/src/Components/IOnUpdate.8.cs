88704b54d4499dc3d80cb60fd608ac0a641aec45
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-05-13 13:27:03 +0100

    docs: Improve XML docs and format code

--- 6_Ideation/Fluent/src/Components/IOnUpdate.8.cs
+++ 6_Ideation/Fluent/src/Components/IOnUpdate.8.cs
@@ -31,29 +31,7 @@ namespace Alis.Core.Aspect.Fluent.Components
 {
     /// <summary>
     ///     Lifecycle hook invoked each frame during the update loop, providing the owning entity and
-    ///     8 additional component references of types <typeparamref name="TArg1"/>, <typeparamref name="TArg2"/>, <typeparamref name="TArg3"/>, <typeparamref name="TArg4"/>, <typeparamref name="TArg5"/>, <typeparamref name="TArg6"/>, <typeparamref name="TArg7"/>, <typeparamref name="TArg8"/>.
-    /// </summary>
-    /// <typeparam name="TArg1">The type of the 1st additional component or data argument passed to the update method.</typeparam>
-    /// <typeparam name="TArg2">The type of the 2nd additional component or data argument passed to the update method.</typeparam>
-    /// <typeparam name="TArg3">The type of the 3rd additional component or data argument passed to the update method.</typeparam>
-    /// <typeparam name="TArg4">The type of the 4th additional component or data argument passed to the update method.</typeparam>
-    /// <typeparam name="TArg5">The type of the 5th additional component or data argument passed to the update method.</typeparam>
-    /// <typeparam name="TArg6">The type of the 6th additional component or data argument passed to the update method.</typeparam>
-    /// <typeparam name="TArg7">The type of the 7th additional component or data argument passed to the update method.</typeparam>
-    /// <typeparam name="TArg8">The type of the 8th additional component or data argument passed to the update method.</typeparam>
-    /// <typeparam name="TArg2">The type of the 2nd additional component or data argument passed to the update method.</typeparam>
-    /// <typeparam name="TArg3">The type of the 3rd additional component or data argument passed to the update method.</typeparam>
-    /// <typeparam name="TArg4">The type of the 4th additional component or data argument passed to the update method.</typeparam>
-    /// <typeparam name="TArg5">The type of the 5th additional component or data argument passed to the update method.</typeparam>
-    /// <typeparam name="TArg6">The type of the 6th additional component or data argument passed to the update method.</typeparam>
-    /// <typeparam name="TArg7">The type of the 7th additional component or data argument passed to the update method.</typeparam>
-    /// <typeparam name="TArg8">The type of the 8th additional component or data argument passed to the update method.</typeparam>
-
-    /// <seealso cref="IComponentBase" />
-    /// <seealso cref="IComponentBase" />
-    /// <summary>
-    ///     Lifecycle hook invoked each frame during the update loop, providing the owning entity and
-    ///     8 additional component references of types <typeparamref name="TArg1"/>, <typeparamref name="TArg2"/>, <typeparamref name="TArg3"/>, <typeparamref name="TArg4"/>, <typeparamref name="TArg5"/>, <typeparamref name="TArg6"/>, <typeparamref name="TArg7"/>, <typeparamref name="TArg8"/>.
+    ///     8 additional component references of types <typeparamref name="TArg1"/>, <typeparamref name="TArg2"/>, <typeparamref name="TArg3"/>, <typeparamref name="TArg4"/>, <typeparamref name="TArg5"/>, <typeparamref name="TArg6"/>, <typeparamref name="TArg7"/>, and <typeparamref name="TArg8"/>.
     /// </summary>
     /// <typeparam name="TArg1">The type of the 1st additional component or data argument passed to the update method.</typeparam>
     /// <typeparam name="TArg2">The type of the 2nd additional component or data argument passed to the update method.</typeparam>
@@ -78,6 +56,7 @@ namespace Alis.Core.Aspect.Fluent.Components
         /// <param name="arg6">The 6th additional component reference of type <typeparamref name="TArg6"/>.</param>
         /// <param name="arg7">The 7th additional component reference of type <typeparamref name="TArg7"/>.</param>
         /// <param name="arg8">The 8th additional component reference of type <typeparamref name="TArg8"/>.</param>
-        void Update(IGameObject self, ref TArg1 arg1, ref TArg2 arg2, ref TArg3 arg3, ref TArg4 arg4, ref TArg5 arg5, ref TArg6 arg6, ref TArg7 arg7, ref TArg8 arg8);
+        void Update(IGameObject self, ref TArg1 arg1, ref TArg2 arg2, ref TArg3 arg3, ref TArg4 arg4, ref TArg5 arg5,
+            ref TArg6 arg6, ref TArg7 arg7, ref TArg8 arg8);
     }
 }
