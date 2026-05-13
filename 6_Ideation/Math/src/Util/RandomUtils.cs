850256d88d0de4846692e0a7c551d74c3d1aa09c
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-05-13 13:27:38 +0100

    docs: Improve XML docs and format code

--- 6_Ideation/Math/src/Util/RandomUtils.cs
+++ 6_Ideation/Math/src/Util/RandomUtils.cs
@@ -33,22 +33,22 @@ using System.Security.Cryptography;
 namespace Alis.Core.Aspect.Math.Util
 {
     /// <summary>
-    ///     The random utils class
+    ///     Provides cryptographically secure random number generation for 32-bit signed integers.
     /// </summary>
     public static class RandomUtils
     {
         /// <summary>
-        ///     The create
+        ///     The cryptographic random number generator instance used for all operations.
         /// </summary>
         private static readonly RandomNumberGenerator Rng = RandomNumberGenerator.Create();
 
         /// <summary>
-        ///     Gets the int 32 using the specified min value
+        ///     Generates a cryptographically random 32-bit signed integer within the specified inclusive range.
         /// </summary>
-        /// <param name="minValue">The min value</param>
-        /// <param name="maxValue">The max value</param>
-        /// <exception cref="ArgumentException">minValue must be less than or equal to maxValue.</exception>
-        /// <returns>The int</returns>
+        /// <param name="minValue">The inclusive lower bound of the random number returned.</param>
+        /// <param name="maxValue">The inclusive upper bound of the random number returned. Must be greater than or equal to <paramref name="minValue" />.</param>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="minValue" /> is greater than <paramref name="maxValue" />.</exception>
+        /// <returns>A random 32-bit signed integer between <paramref name="minValue" /> and <paramref name="maxValue" />, inclusive.</returns>
         public static int GetInt32(int minValue, int maxValue)
         {
             if (minValue > maxValue)
@@ -64,11 +64,11 @@ namespace Alis.Core.Aspect.Math.Util
         }
 
         /// <summary>
-        ///     Gets the int 32 using the specified value
+        ///     Generates a cryptographically random 32-bit signed integer between 0 (inclusive) and the specified upper bound (inclusive).
         /// </summary>
-        /// <param name="value">The value</param>
-        /// <exception cref="ArgumentException">value must be greater than or equal to 0.</exception>
-        /// <returns>The int</returns>
+        /// <param name="value">The inclusive upper bound. Must be greater than or equal to 0.</param>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="value" /> is less than 0.</exception>
+        /// <returns>A random 32-bit signed integer between 0 and <paramref name="value" />, inclusive.</returns>
         public static int GetInt32(int value)
         {
             if (value < 0)
