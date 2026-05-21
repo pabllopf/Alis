0cb56513e05d7881b5c5a66e9002251e3c7ea6a3
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-05-21 09:56:19 +0100

    Update FilePickerValidator.cs

--- 1_Presentation/Extension/Io/FileDialog/src/FilePickerValidator.cs
+++ 1_Presentation/Extension/Io/FileDialog/src/FilePickerValidator.cs
@@ -28,6 +28,7 @@
 //  --------------------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
