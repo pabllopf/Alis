c070560006d6f86deddbde4b5e2c1e23bf8d92d5
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-05-23 11:22:39 +0100

    Update Project.cs

--- 1_Presentation/Hub/src/Entity/Project.cs
+++ 1_Presentation/Hub/src/Entity/Project.cs
@@ -34,19 +34,19 @@ using Alis.Core.Aspect.Data.Json;
 namespace Alis.App.Hub.Entity
 {
     /// <summary>
-    ///     The project class
+    ///     Represents a project metadata record containing name, path, synchronization status, and version information.
     /// </summary>
     [Serializable, StructLayout(LayoutKind.Sequential, Pack = 1)]
     public partial struct Project
     {
         /// <summary>
-        ///     Initializes a new instance of the <see cref="Project" /> class
+        ///     Initializes a new instance of the <see cref="Project" /> struct with the specified project metadata.
         /// </summary>
-        /// <param name="name">The name</param>
-        /// <param name="path">The path</param>
-        /// <param name="cloudStatus">The cloud status</param>
-        /// <param name="modifiedDate">The modified date</param>
-        /// <param name="editorVersion">The editor version</param>
+        /// <param name="name">The project display name.</param>
+        /// <param name="path">The file system path to the project directory.</param>
+        /// <param name="cloudStatus">The current cloud synchronization status.</param>
+        /// <param name="modifiedDate">The last modification date string.</param>
+        /// <param name="editorVersion">The editor version associated with the project.</param>
         public Project(string name, string path, string cloudStatus, string modifiedDate, string editorVersion)
         {
             Name = name;
@@ -57,43 +57,43 @@ namespace Alis.App.Hub.Entity
         }
 
         /// <summary>
-        ///     Gets the value of the name
+        ///     Gets or sets the project display name. Defaults to "Not Set".
         /// </summary>
         [JsonNativePropertyName("_name_")]
         public string Name { get; set; } = "Not Set";
 
         /// <summary>
-        ///     Gets the value of the path
+        ///     Gets or sets the file system path to the project directory. Defaults to "Not Set".
         /// </summary>
         [JsonNativePropertyName("_path_")]
         public string Path { get; set; } = "Not Set";
 
         /// <summary>
-        ///     Gets the value of the cloud status
+        ///     Gets or sets the current cloud synchronization status. Defaults to "Not Synced".
         /// </summary>
         [JsonNativePropertyName("_cloudStatus_")]
         public string CloudStatus { get; set; } = "Not Synced";
 
         /// <summary>
-        ///     Gets the value of the modified date
+        ///     Gets or sets the last modification date string. Defaults to "Never".
         /// </summary>
         [JsonNativePropertyName("_modifiedDate_")]
         public string ModifiedDate { get; set; } = "Never";
 
         /// <summary>
-        ///     Gets the value of the editor version
+        ///     Gets or sets the editor version associated with the project. Defaults to "2021.1.0".
         /// </summary>
         [JsonNativePropertyName("_editorVersion_")]
         public string EditorVersion { get; set; } = "2021.1.0";
 
         /// <summary>
-        ///     Gets or sets the value of the version
+        ///     Gets or sets the project version string. Defaults to "1.0.0".
         /// </summary>
         [JsonNativePropertyName("_version_")]
         public string Version { get; set; } = "1.0.0";
 
         /// <summary>
-        ///     Gets or sets the value of the last modified
+        ///     Gets or sets the last modified timestamp string. Defaults to "Never".
         /// </summary>
         [JsonNativePropertyName("_lastModified_")]
         public string LastModified { get; set; } = "Never";
