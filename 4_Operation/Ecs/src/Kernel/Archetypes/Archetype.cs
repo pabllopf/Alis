5accc97cfe3f04825d558d9bfbbc9dfd14832133
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-05-24 00:07:09 +0100

    refactor(Ecs): fix sonar csharpsquid:S2223 - reduce visibility of NextArchetypeId to private

--- 4_Operation/Ecs/src/Kernel/Archetypes/Archetype.cs
+++ 4_Operation/Ecs/src/Kernel/Archetypes/Archetype.cs
@@ -58,7 +58,7 @@ namespace Alis.Core.Ecs.Kernel.Archetypes
         /// <summary>
         ///     The next archetype id
         /// </summary>
-        internal static int NextArchetypeId = -1;
+        private static int NextArchetypeId = -1;
 
         /// <summary>
         ///     The existing archetypes
@@ -740,7 +740,7 @@ namespace Alis.Core.Ecs.Kernel.Archetypes
         /// <summary>
         ///     The next archetype id
         /// </summary>
-        internal static int NextArchetypeId = -1;
+        private static int NextArchetypeId = -1;
 
         /// <summary>
         ///     The existing archetypes
