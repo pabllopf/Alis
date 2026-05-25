ccda2da653912f2fdc1d28aa4152cbab7f1df71d
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-05-25 14:32:16 +0100

    refactor(math): fix sonar S107 reduce constructor params from 16 to 4 using ValueTuples

--- 6_Ideation/Math/src/Matrix/Matrix4X4.cs
+++ 6_Ideation/Math/src/Matrix/Matrix4X4.cs
@@ -95,46 +95,35 @@ namespace Alis.Core.Aspect.Math.Matrix
         public float M44 { get; set; }
 
         /// <summary>Creates a 4x4 matrix from the specified components.</summary>
-        /// <param name="m11">The value to assign to the first element in the first row.</param>
-        /// <param name="m12">The value to assign to the second element in the first row.</param>
-        /// <param name="m13">The value to assign to the third element in the first row.</param>
-        /// <param name="m14">The value to assign to the fourth element in the first row.</param>
-        /// <param name="m21">The value to assign to the first element in the second row.</param>
-        /// <param name="m22">The value to assign to the second element in the second row.</param>
-        /// <param name="m23">The value to assign to the third element in the second row.</param>
-        /// <param name="m24">The value to assign to the fourth element in the second row.</param>
-        /// <param name="m31">The value to assign to the first element in the third row.</param>
-        /// <param name="m32">The value to assign to the second element in the third row.</param>
-        /// <param name="m33">The value to assign to the third element in the third row.</param>
-        /// <param name="m34">The value to assign to the fourth element in the third row.</param>
-        /// <param name="m41">The value to assign to the first element in the fourth row.</param>
-        /// <param name="m42">The value to assign to the second element in the fourth row.</param>
-        /// <param name="m43">The value to assign to the third element in the fourth row.</param>
-        /// <param name="m44">The value to assign to the fourth element in the fourth row.</param>
-        public Matrix4X4(float m11, float m12, float m13, float m14,
-            float m21, float m22, float m23, float m24,
-            float m31, float m32, float m33, float m34,
-            float m41, float m42, float m43, float m44)
+        /// <param name="row1">The first row (m11, m12, m13, m14).</param>
+        /// <param name="row2">The second row (m21, m22, m23, m24).</param>
+        /// <param name="row3">The third row (m31, m32, m33, m34).</param>
+        /// <param name="row4">The fourth row (m41, m42, m43, m44).</param>
+        public Matrix4X4(
+            (float m11, float m12, float m13, float m14) row1,
+            (float m21, float m22, float m23, float m24) row2,
+            (float m31, float m32, float m33, float m34) row3,
+            (float m41, float m42, float m43, float m44) row4)
         {
-            M11 = m11;
-            M12 = m12;
-            M13 = m13;
-            M14 = m14;
+            M11 = row1.m11;
+            M12 = row1.m12;
+            M13 = row1.m13;
+            M14 = row1.m14;
 
-            M21 = m21;
-            M22 = m22;
-            M23 = m23;
-            M24 = m24;
+            M21 = row2.m21;
+            M22 = row2.m22;
+            M23 = row2.m23;
+            M24 = row2.m24;
 
-            M31 = m31;
-            M32 = m32;
-            M33 = m33;
-            M34 = m34;
+            M31 = row3.m31;
+            M32 = row3.m32;
+            M33 = row3.m33;
+            M34 = row3.m34;
 
-            M41 = m41;
-            M42 = m42;
-            M43 = m43;
-            M44 = m44;
+            M41 = row4.m41;
+            M42 = row4.m42;
+            M43 = row4.m43;
+            M44 = row4.m44;
 
             HashCode hash = new HashCode();
 
