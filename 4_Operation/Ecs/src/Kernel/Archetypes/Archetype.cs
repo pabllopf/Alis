27a360f4e303f29a0849df323f756c08964d1b70
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-05-25 11:43:53 +0100

    refactor(ecs): fix sonar S1940 use > instead of negated <=

--- 4_Operation/Ecs/src/Kernel/Archetypes/Archetype.cs
+++ 4_Operation/Ecs/src/Kernel/Archetypes/Archetype.cs
@@ -264,7 +264,7 @@ namespace Alis.Core.Ecs.Kernel.Archetypes
                 -(_entities.Length - (NextComponentIndex + deferredCreationArchetype.DeferredEntityCount));
             int previousComponentCount = NextComponentIndex;
 
-            if (!(deltaFromMaxDeferredInPlace <= 0))
+            if (deltaFromMaxDeferredInPlace > 0)
             {
                 //components overflowed into temp storage
 
