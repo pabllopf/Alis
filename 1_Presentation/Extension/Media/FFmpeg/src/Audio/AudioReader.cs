3a4a35711fef7e276f1bc8383175e4e26a1eb84b
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-05-26 22:59:03 +0100

    refactor(audio): fix sonar S3881 implement dispose pattern in AudioReader

--- 1_Presentation/Extension/Media/FFmpeg/src/Audio/AudioReader.cs
+++ 1_Presentation/Extension/Media/FFmpeg/src/Audio/AudioReader.cs
@@ -98,8 +98,20 @@ namespace Alis.Extension.Media.FFmpeg.Audio
         /// </summary>
         public void Dispose()
         {
-            DataStream?.Dispose();
+            Dispose(true);
             GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        ///     Releases managed and unmanaged resources
+        /// </summary>
+        /// <param name="disposing">Whether to release managed resources</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                DataStream?.Dispose();
+            }
         }
 
         /// <summary>
