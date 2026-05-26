7539f25950ba7e89fca0a036012f9266e7764e16
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-05-26 22:59:04 +0100

    refactor(video): fix sonar S3881 implement dispose pattern in VideoFrame

--- 1_Presentation/Extension/Media/FFmpeg/src/Video/VideoFrame.cs
+++ 1_Presentation/Extension/Media/FFmpeg/src/Video/VideoFrame.cs
@@ -84,8 +84,20 @@ namespace Alis.Extension.Media.FFmpeg.Video
         /// </summary>
         public void Dispose()
         {
-            frameBuffer = null;
+            Dispose(true);
             GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        ///     Releases managed and unmanaged resources
+        /// </summary>
+        /// <param name="disposing">Whether to release managed resources</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                frameBuffer = null;
+            }
         }
 
         /// <summary>
