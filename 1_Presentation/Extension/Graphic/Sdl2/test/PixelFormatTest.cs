0873a5c0712acdbb2838eea85ed9c3acdad7e968
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-06-02 20:48:52 +0100

    test: ShouldDefaultToZero PixelFormatTest.cs Sdl2

--- 1_Presentation/Extension/Graphic/Sdl2/test/PixelFormatTest.cs
+++ 1_Presentation/Extension/Graphic/Sdl2/test/PixelFormatTest.cs
@@ -1,38 +1,25 @@
-// --------------------------------------------------------------------------
-// 
-//                               █▀▀█ ░█─── ▀█▀ ░█▀▀▀█
-//                              ░█▄▄█ ░█─── ░█─ ─▀▀▀▄▄
-//                              ░█─░█ ░█▄▄█ ▄█▄ ░█▄▄▄█
-// 
-//  --------------------------------------------------------------------------
-//  File:PixelFormatTest.cs
-// 
-//  Author:Pablo Perdomo Falcón
-//  Web:https://www.pabllopf.dev/
-// 
-//  Copyright (c) 2021 GNU General Public License v3.0
-// 
-//  This program is free software:you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-// 
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-//  GNU General Public License for more details.
-// 
-//  You should have received a copy of the GNU General Public License
-//  along with this program.If not, see <http://www.gnu.org/licenses/>.
-// 
-//  --------------------------------------------------------------------------
+using System;
+using Xunit;
+using Alis.Extension.Graphic.Sdl2.Structs;
 
 namespace Alis.Extension.Graphic.Sdl2.Test
 {
-    /// <summary>
-    ///     Unit tests for the PixelFormat struct.
-    /// </summary>
     public class PixelFormatTest
     {
+        [Fact]
+        public void ShouldDefaultToZero()
+        {
+            var fmt = new PixelFormat();
+            Assert.Equal(0u, fmt.format);
+            Assert.Equal(IntPtr.Zero, fmt.Palette);
+            Assert.Equal(0, fmt.BitsPerPixel);
+            Assert.Equal(0, fmt.BytesPerPixel);
+            Assert.Equal(0u, fmt.RMask);
+            Assert.Equal(0u, fmt.GMask);
+            Assert.Equal(0u, fmt.BMask);
+            Assert.Equal(0u, fmt.AMask);
+            Assert.Equal(0, fmt.refCount);
+            Assert.Equal(IntPtr.Zero, fmt.Next);
+        }
     }
 }
