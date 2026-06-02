e3a2cf4215c147fb4d1a0f81e4e3fd1223854893
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-06-02 20:47:44 +0100

    test: ShouldAssignAndRetrieveFields KeySymTest.cs Sdl2

--- 1_Presentation/Extension/Graphic/Sdl2/test/KeySymTest.cs
+++ 1_Presentation/Extension/Graphic/Sdl2/test/KeySymTest.cs
@@ -12,5 +12,19 @@ namespace Alis.Extension.Graphic.Sdl2.Test
             var sym = new KeySym();
             Assert.Equal(0u, sym.unicode);
         }
+
+        [Fact]
+        public void ShouldAssignAndRetrieveFields()
+        {
+            var sym = new KeySym
+            {
+                scancode = SdlScancode.SdlScancodeA,
+                sym = KeyCodes.Return,
+                unicode = 65u
+            };
+            Assert.Equal(SdlScancode.SdlScancodeA, sym.scancode);
+            Assert.Equal(KeyCodes.Return, sym.sym);
+            Assert.Equal(65u, sym.unicode);
+        }
     }
 }
