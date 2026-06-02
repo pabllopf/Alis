ccbfc7fbda075f131fedadba7d62baa9269bd720
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-06-02 20:58:27 +0100

    test: ShouldDefaultToZero InternalAndroidWmInfoTest.cs Sdl2

---
+++ 1_Presentation/Extension/Graphic/Sdl2/test/InternalAndroidWmInfoTest.cs
@@ -0,0 +1,58 @@
+// --------------------------------------------------------------------------
+// 
+//                               █▀▀█ ░█─── ▀█▀ ░█▀▀▀█
+//                              ░█▄▄█ ░█─── ░█─ ─▀▀▀▄▄
+//                              ░█─░█ ░█▄▄█ ▄█▄ ░█▄▄▄█
+// 
+//  --------------------------------------------------------------------------
+//  File:InternalAndroidWmInfoTest.cs
+// 
+//  Author:Pablo Perdomo Falcón
+//  Web:https://www.pabllopf.dev/
+// 
+//  Copyright (c) 2021 GNU General Public License v3.0
+// 
+//  This program is free software:you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.If not, see <http://www.gnu.org/licenses/>.
+// 
+//  --------------------------------------------------------------------------
+
+using Xunit;
+using System;
+using Alis.Extension.Graphic.Sdl2.Structs;
+
+namespace Alis.Extension.Graphic.Sdl2.Test
+{
+    public class InternalAndroidWmInfoTest
+    {
+        [Fact]
+        public void ShouldDefaultToZero()
+        {
+            var obj = new InternalAndroidWmInfo();
+            Assert.Equal(IntPtr.Zero, obj.Window);
+            Assert.Equal(IntPtr.Zero, obj.Surface);
+        }
+
+        [Fact]
+        public void ShouldAssignAndRetrieveProperties()
+        {
+            var obj = new InternalAndroidWmInfo();
+            var testWindow = IntPtr.Zero + 1;
+            var testSurface = IntPtr.Zero + 2;
+            obj.Window = testWindow;
+            obj.Surface = testSurface;
+            Assert.Equal(testWindow, obj.Window);
+            Assert.Equal(testSurface, obj.Surface);
+        }
+    }
+}
