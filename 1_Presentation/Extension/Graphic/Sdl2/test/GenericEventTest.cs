b7c8bc46623e0cc0946034e7e4e95030755e2ef4
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-06-02 20:50:10 +0100

    test: ShouldAssignTypeAndTimestamp GenericEventTest.cs Sdl2

--- 1_Presentation/Extension/Graphic/Sdl2/test/GenericEventTest.cs
+++ 1_Presentation/Extension/Graphic/Sdl2/test/GenericEventTest.cs
@@ -1,5 +1,6 @@
 using Xunit;
 using Alis.Extension.Graphic.Sdl2.Structs;
+using Alis.Extension.Graphic.Sdl2.Enums;
 
 namespace Alis.Extension.Graphic.Sdl2.Test
 {
@@ -10,6 +11,14 @@ namespace Alis.Extension.Graphic.Sdl2.Test
         {
             var evt = new GenericEvent();
             Assert.Equal(0u, evt.timestamp);
+        }
+
+        [Fact]
+        public void ShouldAssignTypeAndTimestamp()
+        {
+            var evt = new GenericEvent { type = EventType.MouseButtonDown, timestamp = 42u };
+            Assert.Equal(EventType.MouseButtonDown, evt.type);
+            Assert.Equal(42u, evt.timestamp);
         }
     }
 }
