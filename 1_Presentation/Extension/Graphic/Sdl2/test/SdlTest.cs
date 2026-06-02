4f98a9fd61caf121b6bec8ddb94aa6ca0df532ad
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-06-02 20:53:30 +0100

    test: ShouldHaveCorrectConstantValues SdlTest.cs Sdl2

--- 1_Presentation/Extension/Graphic/Sdl2/test/SdlTest.cs
+++ 1_Presentation/Extension/Graphic/Sdl2/test/SdlTest.cs
@@ -1,18 +1,29 @@
-
-using Alis.Extension.Graphic.Sdl2.Structs;
 using Xunit;
+using Alis.Extension.Graphic.Sdl2;
 
 namespace Alis.Extension.Graphic.Sdl2.Test
 {
     public class SdlTest
     {
-        /// <summary>
-        ///     Tests that constant TextEditingEventTextSize has correct value.
-        /// </summary>
         [Fact]
-        public void TextEditingEventTextSize_HasCorrectValue()
+        public void ShouldHaveCorrectConstantValues()
         {
             Assert.Equal(32, Sdl.TextEditingEventTextSize);
+            Assert.Equal(32, Sdl.TextInputEventTextSize);
+            Assert.Equal(-1, Sdl.Query);
+            Assert.Equal(0, Sdl.Ignore);
+            Assert.Equal(0, Sdl.Disable);
+            Assert.Equal(1, Sdl.Enable);
+            Assert.Equal(1 << 30, Sdl.KScancodeMask);
+            Assert.Equal(1u, Sdl.ButtonLeft);
+            Assert.Equal(2u, Sdl.ButtonMiddle);
+            Assert.Equal(3u, Sdl.ButtonRight);
+            Assert.Equal(4u, Sdl.ButtonX1);
+            Assert.Equal(5u, Sdl.ButtonX2);
+            Assert.Equal((ushort)0xFF, Sdl.AudioMaskBitSize);
+            Assert.Equal((ushort)(1 << 8), Sdl.AudioMaskDatatype);
+            Assert.Equal((ushort)(1 << 12), Sdl.AudioMaskEndian);
+            Assert.Equal((ushort)(1 << 15), Sdl.AudioMaskSigned);
         }
-}
+    }
 }
