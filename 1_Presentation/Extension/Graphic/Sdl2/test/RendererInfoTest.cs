d18534052f696c463055e4fc6dc6096de0a3f425
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-06-02 20:47:52 +0100

    test: ShouldDefaultToZero RendererInfoTest.cs Sdl2

--- 1_Presentation/Extension/Graphic/Sdl2/test/RendererInfoTest.cs
+++ 1_Presentation/Extension/Graphic/Sdl2/test/RendererInfoTest.cs
@@ -1,38 +1,21 @@
-// --------------------------------------------------------------------------
-// 
-//                               █▀▀█ ░█─── ▀█▀ ░█▀▀▀█
-//                              ░█▄▄█ ░█─── ░█─ ─▀▀▀▄▄
-//                              ░█─░█ ░█▄▄█ ▄█▄ ░█▄▄▄█
-// 
-//  --------------------------------------------------------------------------
-//  File:RendererInfoTest.cs
-// 
-//  Author:Pablo Perdomo Falcón
-//  Web:https://www.pabllopf.dev/
-// 
-//  Copyright (c) 2021 GNU General Public License v3.0
-// 
-//  This program is free software:you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-// 
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-//  GNU General Public License for more details.
-// 
-//  You should have received a copy of the GNU General Public License
-//  along with this program.If not, see <http://www.gnu.org/licenses/>.
-// 
-//  --------------------------------------------------------------------------
+using System;
+using System.Runtime.InteropServices;
+using Xunit;
+using Alis.Extension.Graphic.Sdl2.Structs;
 
 namespace Alis.Extension.Graphic.Sdl2.Test
 {
-    /// <summary>
-    ///     Unit tests for the RendererInfo struct.
-    /// </summary>
     public class RendererInfoTest
     {
+        [Fact]
+        public void ShouldDefaultToZero()
+        {
+            var info = new RendererInfo();
+            Assert.Equal(IntPtr.Zero, info.Name);
+            Assert.Equal(0u, info.flags);
+            Assert.Equal(0u, info.num_texture_formats);
+            Assert.Equal(0, info.maxTextureWidth);
+            Assert.Equal(0, info.maxTextureHeight);
+        }
     }
 }
