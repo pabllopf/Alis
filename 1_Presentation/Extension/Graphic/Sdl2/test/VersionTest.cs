1be6cfc059b4143db5040e2c389207a6ddc2871e
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-06-02 20:45:22 +0100

    test: ShouldSetFieldsThroughConstructor VersionTest.cs Sdl2

--- 1_Presentation/Extension/Graphic/Sdl2/test/VersionTest.cs
+++ 1_Presentation/Extension/Graphic/Sdl2/test/VersionTest.cs
@@ -27,8 +27,8 @@
 // 
 //  --------------------------------------------------------------------------
 
-using Alis.Extension.Graphic.Sdl2.Structs;
 using Xunit;
+using Alis.Extension.Graphic.Sdl2.Structs;
 
 namespace Alis.Extension.Graphic.Sdl2.Test
 {
@@ -37,64 +37,15 @@ namespace Alis.Extension.Graphic.Sdl2.Test
     /// </summary>
     public class VersionTest
     {
-        /// <summary>
-        ///     Tests the Version constructor with valid parameters.
-        /// </summary>
         [Fact]
-        public void VersionConstructor_WithValidParameters_InitializesCorrectly()
+        public void ShouldSetFieldsThroughConstructor()
         {
-            int major = 2;
-            int minor = 0;
-            int patch = 18;
-
-            Version version = new Version(major, minor, patch);
-
-            Assert.Equal((byte) major, version.major);
-            Assert.Equal((byte) minor, version.minor);
-            Assert.Equal((byte) patch, version.patch);
-        }
-
-        /// <summary>
-        ///     Tests the Version constructor with zero values.
-        /// </summary>
-        [Fact]
-        public void VersionConstructor_WithZeroValues_InitializesZeroVersion()
-        {
-            Version version = new Version(0, 0, 0);
-
-            Assert.Equal(0, version.major);
-            Assert.Equal(0, version.minor);
-            Assert.Equal(0, version.patch);
-        }
-
-        /// <summary>
-        ///     Tests the Version constructor with maximum byte values.
-        /// </summary>
-        [Fact]
-        public void VersionConstructor_WithMaxByteValues_InitializesMaxVersion()
-        {
-            int major = 255;
-            int minor = 255;
-            int patch = 255;
-
-            Version version = new Version(major, minor, patch);
-
-            Assert.Equal(255, version.major);
-            Assert.Equal(255, version.minor);
-            Assert.Equal(255, version.patch);
-        }
-
-        /// <summary>
-        ///     Tests the Version constructor with different values.
-        /// </summary>
-        [Theory, InlineData(1, 2, 3), InlineData(5, 10, 15), InlineData(100, 50, 25)]
-        public void VersionConstructor_WithDifferentValues_InitializesProperly(int major, int minor, int patch)
-        {
-            Version version = new Version(major, minor, patch);
-
-            Assert.Equal((byte) major, version.major);
-            Assert.Equal((byte) minor, version.minor);
-            Assert.Equal((byte) patch, version.patch);
+            // Arrange
+            var version = new Version(3, 2, 1);
+            // Assert
+            Assert.Equal(3, version.major);
+            Assert.Equal(2, version.minor);
+            Assert.Equal(1, version.patch);
         }
     }
 }
