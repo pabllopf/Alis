068d8ec14a228a1ae877888529601da99a2216d4
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-06-02 20:37:00 +0100

    test: ShouldReturnTextBytesFromStruct TextInputEventTest.cs Sdl2

--- 1_Presentation/Extension/Graphic/Sdl2/test/TextInputEventTest.cs
+++ 1_Presentation/Extension/Graphic/Sdl2/test/TextInputEventTest.cs
@@ -27,6 +27,38 @@
 // 
 //  --------------------------------------------------------------------------
 
+// --------------------------------------------------------------------------
+// 
+//                               █▀▀█ ░█─── ▀█▀ ░█▀▀▀█
+//                              ░█▄▄█ ░█─── ░█─ ─▀▀▀▄▄
+//                              ░█─░█ ░█▄▄█ ▄█▄ ░█▄▄▄█
+// 
+//  --------------------------------------------------------------------------
+//  File:TextInputEventTest.cs
+// 
+//  Author:Pablo Perdomo Falcón
+//  Web:https://www.pabllopf.dev/
+// 
+//  Copyright (c) 2021 GNU General Public License v3.0
+// 
+//  This program is free software:you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.If not, see <http://www.gnu.org/licenses/>.
+// 
+//  --------------------------------------------------------------------------
+
+using Xunit;
+using Alis.Extension.Graphic.Sdl2.Structs;
+
 namespace Alis.Extension.Graphic.Sdl2.Test
 {
     /// <summary>
@@ -34,5 +66,24 @@ namespace Alis.Extension.Graphic.Sdl2.Test
     /// </summary>
     public class TextInputEventTest
     {
+        [Fact]
+        public void ShouldReturnTextBytesFromStruct()
+        {
+            // Arrange: Fill each byte field with unique values
+            var evt = new TextInputEvent();
+            for (int i = 0; i < 32; ++i)
+            {
+                typeof(TextInputEvent).GetField($"byte{i}", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic)
+                    .SetValueDirect(__makeref(evt), (byte)(i + 1));
+            }
+            // Act
+            var bytes = evt.Text;
+            // Assert
+            Assert.Equal(32, bytes.Length);
+            for (int i = 0; i < 32; ++i)
+            {
+                Assert.Equal((byte)(i + 1), bytes[i]);
+            }
+        }
     }
 }
