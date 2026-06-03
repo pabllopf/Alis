faf80b8c0eb31409178f9520f06d1f0952e9743a
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-06-03 20:22:45 +0100

    fix(physic): resolve sonar BUG S3923 conditional operation

--- 4_Operation/Physic/src/Collisions/Collision.cs
+++ 4_Operation/Physic/src/Collisions/Collision.cs
@@ -1237,14 +1237,14 @@ namespace Alis.Core.Physic.Collisions
             private static Vector2F SelectFrontUpperLimit(CollisionNormalInputs i)
             {
                 if (i.HasVertex0 && i.HasVertex3) return i.Convex1 && !i.Convex2 ? i.Normal1 : i.Normal2;
-                if (i.HasVertex0) return i.Convex1 ? -i.Normal1 : -i.Normal1;
+                if (i.HasVertex0) return i.Convex1 ? -i.Normal1 : i.Normal2;
                 return i.Normal2;
             }
 
             private static Vector2F SelectBackLowerLimit(CollisionNormalInputs i, Vector2F neg)
             {
                 if (i.HasVertex0 && i.HasVertex3) return i.Convex1 && !i.Convex2 ? -i.Normal2 : neg;
-                if (i.HasVertex0) return i.Convex1 ? -neg : -neg;
+                if (i.HasVertex0) return i.Convex1 ? -neg : neg;
                 return -i.Normal2;
             }
 
