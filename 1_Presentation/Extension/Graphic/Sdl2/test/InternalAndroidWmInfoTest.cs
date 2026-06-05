3e0e9ae8fd01b9a6a3686c3e1f242d3885cf337f
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-06-05 14:22:38 +0100

    test: ShouldDefaultInitialization InternalAndroidWmInfoTest.cs Graphic.Sdl2

--- 1_Presentation/Extension/Graphic/Sdl2/test/InternalAndroidWmInfoTest.cs
+++ 1_Presentation/Extension/Graphic/Sdl2/test/InternalAndroidWmInfoTest.cs
@@ -1,58 +1,43 @@
-// --------------------------------------------------------------------------
-// 
-//                               █▀▀█ ░█─── ▀█▀ ░█▀▀▀█
-//                              ░█▄▄█ ░█─── ░█─ ─▀▀▀▄▄
-//                              ░█─░█ ░█▄▄█ ▄█▄ ░█▄▄▄█
-// 
-//  --------------------------------------------------------------------------
-//  File:InternalAndroidWmInfoTest.cs
-// 
-//  Author:Pablo Perdomo Falcón
-//  Web:https://www.pabllopf.dev/
-// 
-//  Copyright (c) 2021 GNU General Public License v3.0
-// 
-//  This program is free software:you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-// 
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-//  GNU General Public License for more details.
-// 
-//  You should have received a copy of the GNU General Public License
-//  along with this program.If not, see <http://www.gnu.org/licenses/>.
-// 
-//  --------------------------------------------------------------------------
-
-using Xunit;
 using System;
 using Alis.Extension.Graphic.Sdl2.Structs;
+using Xunit;
 
 namespace Alis.Extension.Graphic.Sdl2.Test
 {
     public class InternalAndroidWmInfoTest
     {
         [Fact]
-        public void ShouldDefaultToZero()
+        public void InternalAndroidWmInfo_DefaultInitialization_PropertiesHaveDefaultValues()
         {
-            InternalAndroidWmInfo obj = new InternalAndroidWmInfo();
-            Assert.Equal(IntPtr.Zero, obj.Window);
-            Assert.Equal(IntPtr.Zero, obj.Surface);
+            InternalAndroidWmInfo info = new InternalAndroidWmInfo();
+
+            Assert.Equal(IntPtr.Zero, info.Window);
+            Assert.Equal(IntPtr.Zero, info.Surface);
         }
 
         [Fact]
-        public void ShouldAssignAndRetrieveProperties()
+        public void InternalAndroidWmInfo_SetProperties_StoresValuesCorrectly()
         {
-            InternalAndroidWmInfo obj = new InternalAndroidWmInfo();
-            IntPtr testWindow = IntPtr.Zero + 1;
-            IntPtr testSurface = IntPtr.Zero + 2;
-            obj.Window = testWindow;
-            obj.Surface = testSurface;
-            Assert.Equal(testWindow, obj.Window);
-            Assert.Equal(testSurface, obj.Surface);
+            InternalAndroidWmInfo info = new InternalAndroidWmInfo
+            {
+                Window = new IntPtr(123),
+                Surface = new IntPtr(456)
+            };
+
+            Assert.Equal(new IntPtr(123), info.Window);
+            Assert.Equal(new IntPtr(456), info.Surface);
+        }
+
+        [Fact]
+        public void InternalAndroidWmInfo_IsValueType_CopyIsIndependent()
+        {
+            InternalAndroidWmInfo original = new InternalAndroidWmInfo { Window = new IntPtr(100) };
+            InternalAndroidWmInfo copy = original;
+
+            copy.Window = new IntPtr(200);
+
+            Assert.Equal(new IntPtr(100), original.Window);
+            Assert.Equal(new IntPtr(200), copy.Window);
         }
     }
 }
