492b0e71fa6828b2514b501bfab8c608b6cefb17
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-06-05 12:50:24 +0100

    test: Constructor_AssignsFields Bvec3Test.cs Graphic.Sfml

---
+++ 1_Presentation/Extension/Graphic/Sfml/test/Render/Bvec3Test.cs
@@ -0,0 +1,52 @@
+// --------------------------------------------------------------------------
+// 
+//                               █▀▀█ ░█─── ▀█▀ ░█▀▀▀█
+//                              ░█▄▄█ ░█─── ░█─ ─▀▀▀▄▄
+//                              ░█─░█ ░█▄▄█ ▄█▄ ░█▄▄▄█
+// 
+//  --------------------------------------------------------------------------
+//  File:Bvec3Test.cs
+// 
+//  Author:Pablo Perdomo Falcón
+//  Web:https://www.pabllopf.dev/
+// 
+//  Copyright (c) 2021 GNU General Public License v3.0
+// 
+//  This program is free software:you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.If not, see <http://www.gnu.org/licenses/>.
+// 
+//  --------------------------------------------------------------------------
+
+using Alis.Extension.Graphic.Sfml.Render;
+using Xunit;
+
+namespace Alis.Extension.Graphic.Sfml.Test.Render
+{
+    /// <summary>
+    ///     Unit tests for the Bvec3 struct.
+    /// </summary>
+    public class Bvec3Test
+    {
+        /// <summary>
+        ///     Tests the constructor and field assignment.
+        /// </summary>
+        [Fact]
+        public void Constructor_AssignsFields()
+        {
+            Bvec3 v = new Bvec3(true, false, true);
+            Assert.True(v.X);
+            Assert.False(v.Y);
+            Assert.True(v.Z);
+        }
+    }
+}
