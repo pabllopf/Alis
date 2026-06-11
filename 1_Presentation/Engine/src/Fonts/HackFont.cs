39fa7efc6dee4a62c2bd1321f0a537aad2dd4012
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-06-11 14:28:51 +0100

    fix: sonarAZ6OPa_Ksynw1OJ1vi8A HackFont.cs

--- 1_Presentation/Engine/src/Fonts/HackFont.cs
+++ 1_Presentation/Engine/src/Fonts/HackFont.cs
@@ -32,7 +32,7 @@ namespace Alis.App.Engine.Fonts
     /// <summary>
     ///     The hack font class
     /// </summary>
-    public class HackFont
+    public sealed class HackFont
     {
     }
 }
--- 1_Presentation/Engine/src/Fonts/JetbrainFont.cs
+++ 1_Presentation/Engine/src/Fonts/JetbrainFont.cs
@@ -32,7 +32,7 @@ namespace Alis.App.Engine.Fonts
     /// <summary>
     ///     The jetbrain font class
     /// </summary>
-    public class JetbrainFont
+    public sealed class JetbrainFont
     {
     }
 }
