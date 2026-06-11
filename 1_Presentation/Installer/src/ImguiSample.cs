ae8f92390b4a7a385394554d7d01a121e1feb038
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-06-11 09:48:04 +0100

    fix: sonarAZ6OPbA2synw1OJ1vi8M ImguiSample.cs

--- 1_Presentation/Installer/src/ImguiSample.cs
+++ 1_Presentation/Installer/src/ImguiSample.cs
@@ -465,10 +465,7 @@ namespace Alis.App.Installer
                 for (int cmdi = 0; cmdi < cmdList.CmdBuffer.Size; cmdi++)
                 {
                     ImDrawCmd pcmd = cmdList.CmdBuffer[cmdi];
-                    if (pcmd.UserCallback != IntPtr.Zero)
-                    {
-                    }
-                    else
+                    if (pcmd.UserCallback == IntPtr.Zero)
                     {
                         IntPtr texIdPtr = pcmd.GetTexId();
                         uint texId = texIdPtr == IntPtr.Zero ? _fontTexture : (uint) texIdPtr.ToInt64();
