69d8c2edc51036958d421cce8e498ba6c55206b3
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-06-12 17:54:39 +0100

    test: ImPlotHistogramFlags_FlagsShouldBeDistinct ImPlotHistogramFlagsTest.cs ImPlot

---
+++ 1_Presentation/Extension/Graphic/Ui/test/Extras/Plot/ImPlotPieChartFlagsTest.cs
@@ -0,0 +1,58 @@
+// --------------------------------------------------------------------------
+// 
+//                               █▀▀█ ░█─── ▀█▀ ░█▀▀▀█
+//                              ░█▄▄█ ░█─── ░█─ ─▀▀▀▄▄
+//                              ░█─░█ ░█▄▄█ ▄█▄ ░█▄▄▄█
+// 
+//  --------------------------------------------------------------------------
+//  File:ImPlotPieChartFlagsTest.cs
+// 
+//  Author:Pablo Perdomo Falcón
+//  Web:https://www.pabllopf.dev/
+// 
+//  Copyright (c) 2021 GNU General Public License v3.0
+// 
+//  This program is free software:you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.If not, see <http://www.gnu.org/licenses/>.
+// 
+//  --------------------------------------------------------------------------
+
+using Alis.Extension.Graphic.Ui.Extras.Plot;
+using Xunit;
+
+namespace Alis.Extension.Graphic.Ui.Test.Extras.Plot
+{
+    /// <summary>
+    ///     Provides unit coverage for <see cref="ImPlotPieChartFlags" /> values.
+    /// </summary>
+    public class ImPlotPieChartFlagsTest
+    {
+        /// <summary>
+        ///     Verifies that None is zero.
+        /// </summary>
+        [Fact]
+        public void None_ShouldBeZero()
+        {
+            Assert.Equal(0, (int) ImPlotPieChartFlags.None);
+        }
+
+        /// <summary>
+        ///     Verifies that Normalize is non-zero.
+        /// </summary>
+        [Fact]
+        public void Normalize_ShouldBeNonZero()
+        {
+            Assert.NotEqual(0, (int) ImPlotPieChartFlags.Normalize);
+        }
+    }
+}
