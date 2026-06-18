75869a49127d3a7a09e74a4050aed8de15a0f6f2
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-06-18 19:51:07 +0100

    test: coverage RefTuple.cs
    Add RefTuple_DeconstructSingle to cover the Deconstruct method in
    RefTuple<T> which was previously untested. Higher-arity overloads
    (RefTuple<T1,T2> through T1..T8) remain uncovered.

---
+++ 4_Operation/Ecs/test/Systems/RefTupleDeconstructTest.cs
@@ -0,0 +1,56 @@
+// --------------------------------------------------------------------------
+// 
+//                               █▀▀█ ░█─── ▀█▀ ░█▀▀▀█
+//                              ░█▄▄█ ░█─── ░█─ ─▀▀▀▄▄
+//                              ░█─░█ ░█▄▄█ ▄█▄ ░█▄▄▄█
+// 
+//  --------------------------------------------------------------------------
+//  File:RefTupleDeconstructTest.cs
+// 
+//  Author:Pablo Perdomo Falcón
+//  Web:https://www.pabllopf.dev/
+// 
+//  Copyright (c) 2021 GNU General Public License v3.0
+// 
+//  This program is free software:you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.If not, see <http://www.gnu.org/licenses/>.
+// 
+//  --------------------------------------------------------------------------
+
+using Alis.Core.Ecs.Kernel;
+using Alis.Core.Ecs.Systems;
+using Xunit;
+
+namespace Alis.Core.Ecs.Test.Systems
+{
+    /// <summary>
+    ///     Tests deconstruction of <see cref="RefTuple{T}" /> types.
+    /// </summary>
+    public class RefTupleDeconstructTest
+    {
+        /// <summary>
+        ///     Tests deconstruction of <see cref="RefTuple{T}" /> with a single reference.
+        /// </summary>
+        [Fact]
+        public void RefTuple_DeconstructSingle()
+        {
+            int[] data = [42];
+            Ref<int> refItem = new Ref<int>(data, 0);
+            RefTuple<int> tuple = new RefTuple<int> { Item1 = refItem };
+
+            tuple.Deconstruct(out Ref<int> result);
+
+            Assert.Equal(42, result.Value);
+        }
+    }
+}
