4c59cec6926fa341e2f5fcca1d7f19fb09589e20
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-07-09 13:35:14 +0100

    test: coverage CallbackDialogAction.cs
    - Add test for null-callback no-op branch in Execute method (_callback?.Invoke() when _callback is null)

---
+++ 1_Presentation/Extension/Language/Dialogue/test/CallbackDialogActionCoverageTest.cs
@@ -0,0 +1,53 @@
+// --------------------------------------------------------------------------
+// 
+//                               █▀▀█ ░█─── ▀█▀ ░█▀▀▀█
+//                              ░█▄▄█ ░█─── ░█─ ─▀▀▀▄▄
+//                              ░█─░█ ░█▄▄█ ▄█▄ ░█▄▄▄█
+// 
+//  --------------------------------------------------------------------------
+//  File:CallbackDialogActionCoverageTest.cs
+// 
+//  Author:Pablo Perdomo Falcón
+//  Web:https://www.pabllopf.dev/
+// 
+//  Copyright (c) 2021 GNU General Public License v3.0
+// 
+//  This program is free software:you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.If not, see <http://www.gnu.org/licenses/>.
+// 
+//  --------------------------------------------------------------------------
+
+using Alis.Extension.Language.Dialogue.Core;
+using Xunit;
+
+namespace Alis.Extension.Language.Dialogue.Test
+{
+    /// <summary>
+    ///     Coverage tests for CallbackDialogAction
+    /// </summary>
+    public class CallbackDialogActionCoverageTest
+    {
+        /// <summary>
+        ///     Tests that Execute does not throw when no callback is set.
+        ///     This covers the null branch of _callback?.Invoke() in Execute.
+        /// </summary>
+        [Fact]
+        public void Execute_WithoutCallback_ShouldNotThrow()
+        {
+            CallbackDialogAction action = new CallbackDialogAction("testAction");
+            DialogContext context = new DialogContext("testDialog");
+
+            action.Execute(context);
+        }
+    }
+}
