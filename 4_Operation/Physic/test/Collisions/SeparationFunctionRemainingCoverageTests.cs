7b78ad1a4c2cd6da9af74bc053ef474f2634f498
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-07-10 17:38:15 +0100

    Add SeparationFunction remaining coverage tests (FaceA flip, default switch cases)

---
+++ 4_Operation/Physic/test/Collisions/SeparationFunctionRemainingCoverageTests.cs
@@ -0,0 +1,55 @@
+using System.Reflection;
+using Alis.Core.Aspect.Math.Vector;
+using Alis.Core.Physic.Collisions;
+using Alis.Core.Physic.Collisions.Shapes;
+using Alis.Core.Physic.Common;
+using Xunit;
+
+namespace Alis.Core.Physic.Test.Collisions
+{
+    public class SeparationFunctionRemainingCoverageTests
+    {
+        [Fact]
+        public void Set_FaceA_FlipAxis_WhenPointBAboveFace()
+        {
+            PolygonShape shapeA = new PolygonShape(PolygonTools.CreateRectangle(1.0f, 1.0f), 1.0f);
+            PolygonShape shapeB = new PolygonShape(PolygonTools.CreateRectangle(1.0f, 1.0f), 1.0f);
+            DistanceProxy proxyA = new DistanceProxy(shapeA, 0);
+            DistanceProxy proxyB = new DistanceProxy(shapeB, 0);
+            Sweep sweepA = new Sweep { C0 = Vector2F.Zero, C = Vector2F.Zero, LocalCenter = Vector2F.Zero };
+            Sweep sweepB = new Sweep { C0 = new Vector2F(0.0f, 3.0f), C = new Vector2F(0.0f, 3.0f), LocalCenter = Vector2F.Zero };
+
+            SimplexCache cache = new SimplexCache { Count = 2 };
+            cache.IndexA[0] = 3;
+            cache.IndexA[1] = 0;
+            cache.IndexB[0] = 0;
+            cache.IndexB[1] = 0;
+
+            SeparationFunction.Set(ref cache, ref proxyA, ref sweepA, ref proxyB, ref sweepB, 0.0f);
+            float separation = SeparationFunction.FindMinSeparation(out int indexA, out int indexB, 0.0f);
+
+            Assert.False(float.IsNaN(separation));
+            Assert.False(float.IsInfinity(separation));
+        }
+
+        [Fact]
+        public void FindMinSeparation_DefaultCase_ReturnsZero()
+        {
+            var typeField = typeof(SeparationFunction).GetField("_type", BindingFlags.Static | BindingFlags.NonPublic);
+            typeField.SetValue(null, (SeparationFunctionType)99);
+            float sep = SeparationFunction.FindMinSeparation(out int idxA, out int idxB, 0.0f);
+            Assert.Equal(0.0f, sep);
+            Assert.Equal(-1, idxA);
+            Assert.Equal(-1, idxB);
+        }
+
+        [Fact]
+        public void Evaluate_DefaultCase_ReturnsZero()
+        {
+            var typeField = typeof(SeparationFunction).GetField("_type", BindingFlags.Static | BindingFlags.NonPublic);
+            typeField.SetValue(null, (SeparationFunctionType)99);
+            float sep = SeparationFunction.Evaluate(0, 0, 0.0f);
+            Assert.Equal(0.0f, sep);
+        }
+    }
+}
