2814a88e104d3c439eec252e6606e96bc6b6bc82
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-07-10 18:03:04 +0100

    test: add VideoGameBuilderRemainingCoverageTests for Run() uncovered line

---
+++ 2_Application/Alis/test/Builder/Core/Ecs/System/VideoGameBuilderRemainingCoverageTests.cs
@@ -0,0 +1,27 @@
+using System.Reflection;
+using Alis.Builder.Core.Ecs.System;
+using Alis.Core.Ecs;
+using Alis.Core.Ecs.Systems.Configuration.Graphic;
+using Alis.Core.Ecs.Systems.Scope;
+using Xunit;
+
+namespace Alis.Test.Builder.Core.Ecs.System
+{
+    public class VideoGameBuilderRemainingCoverageTests
+    {
+        [Fact]
+        public void Run_CallsBuildAndRun_CompletesWhenIsRunningIsFalse()
+        {
+            VideoGameBuilder builder = new VideoGameBuilder();
+
+            builder.World(sb => sb.Add<Scene>(_ => { }));
+
+            FieldInfo contextField = typeof(VideoGameBuilder).GetField("Context", BindingFlags.Instance | BindingFlags.NonPublic);
+            Context context = (Context)contextField.GetValue(builder);
+            context.Setting.Graphic = context.Setting.Graphic with { PreviewMode = true };
+            context.Exit();
+
+            builder.Run();
+        }
+    }
+}
