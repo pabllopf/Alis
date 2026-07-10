8aecd68dd451fcce04c79ccbf35208fe9a68e92a
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-07-10 17:52:59 +0100

    Add tests to reach 100% coverage on GameObjectBuilder.cs

---
+++ 2_Application/Alis/test/Builder/Core/Ecs/Entity/GameObjectBuilderRemainingCoverageTests.cs
@@ -0,0 +1,55 @@
+using Alis.Builder.Core.Ecs.Components.Audio;
+using Alis.Builder.Core.Ecs.Entity;
+using Alis.Core.Ecs;
+using Alis.Core.Ecs.Components;
+using Alis.Core.Ecs.Components.Audio;
+using Alis.Core.Ecs.Systems.Scope;
+using Xunit;
+
+namespace Alis.Test.Builder.Core.Ecs.Entity
+{
+    public class GameObjectBuilderRemainingCoverageTests
+    {
+        [Fact]
+        public void WithComponent_AudioSourceConfig_ReturnsBuilder()
+        {
+            Context context = new Context();
+            Scene scene = new Scene();
+            GameObjectBuilder builder = new GameObjectBuilder(scene, context);
+            AudioSourceConfig<AudioSource> config = b => b.File("test.wav").Volume(50).Mute(false);
+            GameObjectBuilder result = builder.WithComponent(config);
+            Assert.Same(builder, result);
+        }
+
+        [Fact]
+        public void WithComponent_WithInstance_WithoutIHasContext_ReturnsBuilder()
+        {
+            Context context = new Context();
+            Scene scene = new Scene();
+            GameObjectBuilder builder = new GameObjectBuilder(scene, context);
+            Info info = new Info();
+            GameObjectBuilder result = builder.WithComponent(info);
+            Assert.Same(builder, result);
+        }
+
+        [Fact]
+        public void Name_WithoutExistingInfo_UpdatesInfo()
+        {
+            Context context = new Context();
+            Scene scene = new Scene();
+            GameObjectBuilder builder = new GameObjectBuilder(scene, context);
+            GameObjectBuilder result = builder.Name("TestObject");
+            Assert.Same(builder, result);
+        }
+
+        [Fact]
+        public void IsActive_NoArgs_WithoutExistingInfo_ReturnsBuilder()
+        {
+            Context context = new Context();
+            Scene scene = new Scene();
+            GameObjectBuilder builder = new GameObjectBuilder(scene, context);
+            GameObjectBuilder result = builder.IsActive();
+            Assert.Same(builder, result);
+        }
+    }
+}
