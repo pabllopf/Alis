8d80fc784eb32f484ee4fa533d481384c7a89a0c
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-07-26 22:31:27 +0100

    test: ImColor.cs

--- 1_Presentation/Extension/Graphic/Ui/test/ImColorTest.cs
+++ 1_Presentation/Extension/Graphic/Ui/test/ImColorTest.cs
@@ -66,13 +66,8 @@ namespace Alis.Extension.Graphic.Ui.Test
         [RequireCImguiSystemFact]
         public void Value_ShouldBeInitializedCorrectly()
         {
-            // Arrange
             ImColor color = new ImColor {Value = new Vector4F(1.0f, 0.5f, 0.25f, 1.0f)};
-
-            // Act
             Vector4F value = color.Value;
-
-            // Assert
             Assert.Equal(new Vector4F(1.0f, 0.5f, 0.25f, 1.0f), value);
         }
 
@@ -82,13 +77,8 @@ namespace Alis.Extension.Graphic.Ui.Test
         [RequireCImguiSystemFact]
         public void SetHsv_ShouldSetCorrectValues()
         {
-            // Arrange
             ImColor color = new ImColor();
-
-            // Act
             color.SetHsv(0, 1, 1);
-
-            // Assert
             Assert.NotEqual(default, color.Value);
         }
 
@@ -98,13 +88,8 @@ namespace Alis.Extension.Graphic.Ui.Test
         [RequireCImguiSystemFact]
         public void SetHsv_WithAlpha_ShouldSetCorrectValues()
         {
-            // Arrange
             ImColor color = new ImColor();
-
-            // Act
             color.SetHsv(0, 1, 1, 0.5f);
-
-            // Assert
             Assert.Equal(0.5f, color.Value.W);
         }
 
@@ -118,6 +103,41 @@ namespace Alis.Extension.Graphic.Ui.Test
             Vector4F value = new Vector4F(1, 2, 3, 4);
             color.Value = value;
             Assert.Equal(value, color.Value);
+        }
+
+        /// <summary>
+        ///     Tests that default value is zero
+        /// </summary>
+        [Fact]
+        public void Value_Default_ShouldBeZero()
+        {
+            ImColor color = new ImColor();
+            Assert.Equal(default, color.Value);
+        }
+
+        /// <summary>
+        ///     Tests that value can be set and read back
+        /// </summary>
+        [Fact]
+        public void Value_SetAndGet_ShouldMatch()
+        {
+            ImColor color = new ImColor();
+            Vector4F expected = new Vector4F(0.5f, 0.25f, 0.75f, 1.0f);
+            color.Value = expected;
+            Assert.Equal(expected, color.Value);
+        }
+
+        /// <summary>
+        ///     Tests that value can be overwritten
+        /// </summary>
+        [Fact]
+        public void Value_Overwrite_ShouldStoreLatest()
+        {
+            ImColor color = new ImColor();
+            color.Value = new Vector4F(1, 2, 3, 4);
+            Vector4F expected = new Vector4F(5, 6, 7, 8);
+            color.Value = expected;
+            Assert.Equal(expected, color.Value);
         }
 
         /// <summary>
