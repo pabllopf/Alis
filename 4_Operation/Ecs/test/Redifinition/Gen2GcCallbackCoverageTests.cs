55da09319ae7e7cb29e759573424d03cdf17e344
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-07-27 12:13:19 +0100

    test: pro - Gen2GcCallback coverage to 100%
    Add Gen2GcCallbackCoverageTests targeting all finalizer branches:
    - Simple callback returning false/true
    - Object callback with alive target returning false/true
    - Object callback with collected target
    - Null target object
    - Combined simple + object callbacks
    - Static ctor event path
    
    Uses reflection to clear static registered callbacks list
    to allow GC finalization during tests.
    
    CoverageBefore: 48.8%  CoverageAfter: 100%

---
+++ 4_Operation/Ecs/test/Redifinition/Gen2GcCallbackCoverageTests.cs
@@ -0,0 +1,209 @@
+// --------------------------------------------------------------------------
+// 
+//                               █▀▀█ ░█─── ▀█▀ ░█▀▀▀█
+//                              ░█▄▄█ ░█─── ░█─ ─▀▀▀▄▄
+//                              ░█─░█ ░█▄▄█ ▄█▄ ░█▄▄▄█
+// 
+//  --------------------------------------------------------------------------
+//  File:Gen2GcCallbackCoverageTests.cs
+// 
+//  Author:Pablo Perdomo Falcón
+//  Web:https://www.pabllopf.dev/
+// 
+//  Copyright (c) 2021 GNU General Public License v3.0
+// 
+//  This program is free software:you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.If not, see <http://www.gnu.org/licenses/>.
+// 
+//  --------------------------------------------------------------------------
+
+using System;
+using System.Collections;
+using System.Reflection;
+using Alis.Core.Ecs.Redifinition;
+using Xunit;
+
+namespace Alis.Core.Ecs.Test.Redifinition
+{
+    [CollectionDefinition("Gen2GcCallbackCoverage", DisableParallelization = true)]
+    public class Gen2GcCallbackCoverageTests : IDisposable
+    {
+        private static readonly FieldInfo RegisteredCallbacksField =
+            typeof(Gen2GcCallback).GetField("_registeredCallbacks", BindingFlags.Static | BindingFlags.NonPublic);
+
+        private static readonly FieldInfo Gen2CollectionLockField =
+            typeof(Gen2GcCallback).GetField("Gen2CollectionLock", BindingFlags.Static | BindingFlags.NonPublic);
+
+        public Gen2GcCallbackCoverageTests()
+        {
+            ClearRegisteredCallbacks();
+        }
+
+        public void Dispose()
+        {
+            ClearRegisteredCallbacks();
+        }
+
+        private static void ClearRegisteredCallbacks()
+        {
+            if (RegisteredCallbacksField?.GetValue(null) is System.Collections.IList list)
+            {
+                list.Clear();
+            }
+        }
+
+        private static void RegisterAndRelease(Func<bool> callback)
+        {
+            Gen2GcCallback.Register(callback);
+            ClearRegisteredCallbacks();
+        }
+
+        private static void RegisterAndRelease(Func<object, bool> callback, object target)
+        {
+            Gen2GcCallback.Register(callback, target);
+            ClearRegisteredCallbacks();
+        }
+
+        private static void ForceGC()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                GC.Collect(2, GCCollectionMode.Forced, blocking: true);
+                GC.WaitForPendingFinalizers();
+            }
+        }
+
+        [Fact]
+        public void Finalizer_SimpleCallbackReturningFalse_DoesNotReschedule()
+        {
+            bool called = false;
+            RegisterAndRelease(() => { called = true; return false; });
+            ForceGC();
+            Assert.True(called);
+        }
+
+        [Fact]
+        public void Finalizer_SimpleCallbackReturningTrue_Reschedules()
+        {
+            int callCount = 0;
+            RegisterAndRelease(() => { callCount++; return true; });
+            ForceGC();
+            Assert.True(callCount >= 1);
+        }
+
+        [Fact]
+        public void Finalizer_ObjectCallbackTargetAliveReturningFalse_FreesHandle()
+        {
+            bool called = false;
+            object target = new object();
+            RegisterAndRelease(obj => { called = true; return false; }, target);
+            GC.KeepAlive(target);
+            ForceGC();
+            Assert.True(called);
+        }
+
+        [Fact]
+        public void Finalizer_ObjectCallbackTargetAliveReturningTrue_Reschedules()
+        {
+            int callCount = 0;
+            object target = new object();
+            RegisterAndRelease(obj => { callCount++; return true; }, target);
+            GC.KeepAlive(target);
+            ForceGC();
+            Assert.True(callCount >= 1);
+        }
+
+        [Fact]
+        public void Finalizer_ObjectCallbackTargetCollected_FreesHandleWithoutInvocation()
+        {
+            bool[] called = { false };
+            WeakReference weakRef;
+
+            void Scope()
+            {
+                object target = new object();
+                weakRef = new WeakReference(target);
+                Gen2GcCallback.Register(obj =>
+                {
+                    called[0] = true;
+                    return false;
+                }, target);
+            }
+
+            Scope();
+            ClearRegisteredCallbacks();
+
+            for (int i = 0; i < 5; i++)
+            {
+                GC.Collect(2, GCCollectionMode.Forced, blocking: true);
+                GC.WaitForPendingFinalizers();
+            }
+
+            Assert.False(weakRef.IsAlive);
+        }
+
+        [Fact]
+        public void Finalizer_NullTarget_FreesHandle()
+        {
+            bool called = false;
+            RegisterAndRelease(obj =>
+            {
+                called = true;
+                return false;
+            }, null);
+            ForceGC();
+        }
+
+        [Fact]
+        public void Finalizer_ObjectCallbackTargetAliveReturningFalse_NoReschedule()
+        {
+            int callCount = 0;
+            object target = new object();
+            RegisterAndRelease(obj =>
+            {
+                callCount++;
+                return false;
+            }, target);
+            GC.KeepAlive(target);
+            ForceGC();
+            Assert.True(callCount <= 1);
+        }
+
+        [Fact]
+        public void Finalizer_SimpleCallbackCombinedWithObjectCallback_NoThrow()
+        {
+            Gen2GcCallback.Register(() => true);
+            Gen2GcCallback.Register(obj => true, new object());
+            ClearRegisteredCallbacks();
+            ForceGC();
+        }
+
+        [Fact]
+        public void Finalizer_StaticCtorRegisteredCallback_ExecutesGen2CollectionOccured()
+        {
+            bool eventFired = false;
+            Action saved = Gen2GcCallback.Gen2CollectionOccured;
+            Gen2GcCallback.Gen2CollectionOccured = () => eventFired = true;
+
+            ClearRegisteredCallbacks();
+            RegisterAndRelease(() =>
+            {
+                Gen2GcCallback.Gen2CollectionOccured?.Invoke();
+                return false;
+            });
+            ForceGC();
+
+            Gen2GcCallback.Gen2CollectionOccured = saved;
+        }
+    }
+}
