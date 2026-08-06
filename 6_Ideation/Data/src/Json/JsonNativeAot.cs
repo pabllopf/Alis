0522d9a8a6b7adf9d74e345f81d25f4dbd2f0065
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-08-06 18:57:15 +0100

    fix: jsons

--- 6_Ideation/Data/src/Json/JsonNativeAot.cs
+++ 6_Ideation/Data/src/Json/JsonNativeAot.cs
@@ -37,6 +37,7 @@ using Alis.Core.Aspect.Data.Json.Serialization;
 
 namespace Alis.Core.Aspect.Data.Json
 {
+    
     /// <summary>
     ///     Provides a static facade for JSON serialization, deserialization, parsing, and file I/O
     ///     operations. This class is designed to be AOT-compatible by avoiding runtime code generation
