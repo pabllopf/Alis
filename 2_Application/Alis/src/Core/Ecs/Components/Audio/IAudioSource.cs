ab20b93e02e3fc5e79ec9dc3e444d80473e7affe
Pablo Perdomo Falcón <pabloperdomofalcon@gmail.com>
2026-08-31 21:02:13 +0100

    release: new version

--- 2_Application/Alis/src/Core/Ecs/Components/Audio/IAudioSource.cs
+++ 2_Application/Alis/src/Core/Ecs/Components/Audio/IAudioSource.cs
@@ -31,6 +31,7 @@ using Alis.Core.Aspect.Fluent.Components;
 using Alis.Core.Aspect.Fluent.Words;
 using Alis.Core.Ecs.Systems.Scope;
 
+
 namespace Alis.Core.Ecs.Components.Audio
 {
     /// <summary>
